1fcf9384dbd8557160a589e367e028235f11e732
Stef Mallet <stephane.mallet@gmail.com>
2014-11-30 23:36:58 +0100

    TDP-2: first test in java

---
+++ recipe_processor/src/main/java/org/talend/recipeProcessor/RecipeProcessor.java
@@ -0,0 +1,10 @@
+package org.talend.recipeProcessor;
+
+import java.io.File;
+import java.io.IOException;
+
+public interface RecipeProcessor {
+
+	public void upperCase(File source, String column) throws IOException;
+	
+}
