894a06f40c83fd0ff2e61478f7781fa7152356dc
Stef Mallet <stephane.mallet@gmail.com>
2014-12-17 11:16:38 +0100

    TDP-10: extract grid to a custom directive dataset-grid

--- dataprep-webapp/src/main/webapp/app.js
+++ dataprep-webapp/src/main/webapp/app.js
@@ -3,7 +3,7 @@
 // Declare app level module which depends on views, and components
 var app = angular.module('people', []);
 
-app.controller('PeoplesCtrl', function($scope, $http){
+app.controller('PeoplesCtrl', function($scope, $http) {
 	
 	$scope.loadFile = function() {
 		$http.get('ranking').success(function(data) {
@@ -14,5 +14,12 @@ app.controller('PeoplesCtrl', function($scope, $http){
 		$http.get('customers_100.json').success(function(data) {
 			$scope.peoples = data;
 		});
+	};
+});
+
+app.directive('datasetGrid', function() {
+	return {
+		restrict: 'E',
+		templateUrl: 'partials/dataset-grid.html'
 	};
 });
