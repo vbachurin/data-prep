45563cd4a8406c328160b8b8769d5baa5fd1c1d7
Stef Mallet <stephane.mallet@gmail.com>
2015-01-15 14:22:04 +0100

    UI: fix width bug in dq bar

--- dataprep-webapp/src/main/webapp/js/app.js
+++ dataprep-webapp/src/main/webapp/js/app.js
@@ -24,12 +24,21 @@ app.controller('RecordsCtrl', function($scope, $http) {
 });
 
 app.controller('ColumnCtrl', ['$scope', function($scope) {
+	var MIN_PERCENT = 10;
+	
 	$scope.column.total = $scope.column.quality.valid + $scope.column.quality.empty + $scope.column.quality.invalid;
 	
+	// *_percent is the real % of empty/valid/invalid records, while *_percent_width is the width % of the bar.
+	// They can be differents if less than MIN_PERCENT are valid/invalid/empty, to assure a min width of each bar. To be usable by the user.
+	// TODO remove completely one bar if absolute zero records match (ie: if 0 invalid records, do not display invalid bar)
 	$scope.column.quality.empty_percent = Math.ceil($scope.column.quality.empty * 100 / $scope.column.total);
+	$scope.column.quality.empty_percent_width = Math.max($scope.column.quality.empty_percent, MIN_PERCENT);
+	
 	$scope.column.quality.invalid_percent = Math.ceil($scope.column.quality.invalid * 100 / $scope.column.total);
+	$scope.column.quality.invalid_percent_width = Math.max($scope.column.quality.invalid_percent, MIN_PERCENT);
 	
 	$scope.column.quality.valid_percent = 100 - $scope.column.quality.empty_percent - $scope.column.quality.invalid_percent;
+	$scope.column.quality.valid_percent_width = 100 - $scope.column.quality.empty_percent_width - $scope.column.quality.invalid_percent_width;
 }]);
 
 app.directive('datasetGrid', function() {
