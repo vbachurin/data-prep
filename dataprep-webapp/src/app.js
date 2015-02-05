71a0e6fb37e5c473dee88ab8942693f1d90e4e68
jsomsanith <jsomsanith@talend.com>
2015-02-05 09:54:40 +0100

    Create asset folder (images, font, ...) and unit test conf

--- dataprep-webapp/src/app.js
+++ dataprep-webapp/src/app.js
@@ -1,34 +1,35 @@
-'use strict';
+(function() {
+    'use strict';
 
-angular.module('data-prep',
-    [
-        'data-prep-utils', //utils, constants
-        'data-prep-dataset', //dataset getter, manipulation, etc
+    angular.module('data-prep',
+        [
+            'data-prep-utils', //utils, constants
+            'data-prep-dataset', //dataset getter, manipulation, etc
 
-        'ui.router',
-        'angularFileUpload' //file upload with progress support
-    ])
+            'ui.router',
+            'angularFileUpload' //file upload with progress support
+        ])
 
-    .config(['$compileProvider', 'disableDebug', function ($compileProvider, disableDebug) {
-        if(disableDebug) {
-            console.log('disable debug mode');
-            $compileProvider.debugInfoEnabled(false);
-        }
-    }])
+        .config(['$compileProvider', 'disableDebug', function ($compileProvider, disableDebug) {
+            if(disableDebug) {
+                console.log('disable debug mode');
+                $compileProvider.debugInfoEnabled(false);
+            }
+        }])
 
-    .config(function ($stateProvider, $urlRouterProvider) {
-        $stateProvider
-            .state('nav', {
-                abstract: true,
-                templateUrl: 'components/navbar/navbar.html'
-            })
-            .state('nav.home', {
-                url: '/home',
-                templateUrl: 'app/home/home.html',
-                controller: 'HomeCtrl',
-                controllerAs: 'homeCtrl'
-            });
+        .config(function ($stateProvider, $urlRouterProvider) {
+            $stateProvider
+                .state('nav', {
+                    abstract: true,
+                    templateUrl: 'components/navbar/navbar.html'
+                })
+                .state('nav.home', {
+                    url: '/home',
+                    templateUrl: 'app/home/home.html',
+                    controller: 'HomeCtrl',
+                    controllerAs: 'homeCtrl'
+                });
 
-        $urlRouterProvider.otherwise('/home');
-    })
-;
+            $urlRouterProvider.otherwise('/home');
+        });
+})();
---
+++ dataprep-webapp/src/app.spec.js
@@ -0,0 +1,11 @@
+describe('Dataprep app', function() {
+    'use strict';
+
+    it('should return true', function() {
+       expect(true).toBeTruthy();
+    });
+
+    it('should return false', function() {
+        expect(false).toBeFalsy();
+    });
+});
