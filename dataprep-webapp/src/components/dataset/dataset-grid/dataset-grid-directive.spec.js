1602c5a6fe20bd9d0d006c68f7d06afd35b57229
jsomsanith <jsomsanith@talend.com>
2015-02-10 14:28:18 +0100

    TDP-38 : dataset grid unit tests

--- dataprep-webapp/src/components/dataset/dataset-grid/dataset-grid-directive.spec.js
+++ dataprep-webapp/src/components/dataset/dataset-grid/dataset-grid-directive.spec.js
@@ -8,60 +8,94 @@ describe('Dataset grid directive', function() {
     beforeEach(inject(function($rootScope, $compile) {
         scope = $rootScope.$new();
         createElement = function(directiveScope) {
-            var element = angular.element('<dataset-grid column="column"></dataset-grid>');
+            var element = angular.element('<dataset-grid metadata="metadata" data="data"></dataset-grid>');
             $compile(element)(directiveScope);
             directiveScope.$digest();
             return element;
         };
     }));
 
-    it('should calculate column quality', function() {
+    it('should render dataset values', function() {
         //given
-        scope.column = {
-            'id': 'MostPopulousCity',
-            'quality': {
-                'empty': 5,
-                'invalid': 10,
-                'valid': 72
-            },
-            'type': 'string'
+        scope.metadata = {
+            'id': '12ce6c32-bf80-41c8-92e5-66d70f22ec1f',
+            'name': 'US States',
+            'author': 'anonymousUser',
+            'created': '02-03-2015 14:52'
+        };
+        scope.data = {
+            'columns': [
+                {
+                    'id': 'Postal',
+                    'quality': {
+                        'empty': 5,
+                        'invalid': 10,
+                        'valid': 72
+                    },
+                    'type': 'string'
+                },
+                {
+                    'id': 'State',
+                    'quality': {
+                        'empty': 5,
+                        'invalid': 10,
+                        'valid': 72
+                    },
+                    'type': 'string'
+                },
+                {
+                    'id': 'Capital',
+                    'quality': {
+                        'empty': 5,
+                        'invalid': 10,
+                        'valid': 72
+                    },
+                    'type': 'string'
+                },
+                {
+                    'id': 'MostPopulousCity',
+                    'quality': {
+                        'empty': 5,
+                        'invalid': 10,
+                        'valid': 72
+                    },
+                    'type': 'string'
+                }
+            ],
+            'records': [
+                {
+                    'Postal': 'AL',
+                    'State': 'Alabama',
+                    'Capital': 'Montgomery',
+                    'MostPopulousCity': 'Birmingham city'
+                },
+                {
+                    'Postal': 'AK',
+                    'State': 'Alaska',
+                    'Capital': 'Juneau',
+                    'MostPopulousCity': 'Anchorage'
+                }
+            ]
         };
 
         //when
         var element = createElement(scope);
-        //var ctrl = element.controller;
 
         //then
-        expect(scope.column.total).toBe(87);
-        expect(scope.column.quality.emptyPercent).toBe(6);
-        expect(scope.column.quality.emptyPercentWidth).toBe(10);
-        expect(scope.column.quality.invalidPercent).toBe(12);
-        expect(scope.column.quality.invalidPercentWidth).toBe(12);
-        expect(scope.column.quality.validPercent).toBe(82);
-        expect(scope.column.quality.validPercentWidth).toBe(78);
-    });
-
-    it('should display colum title, type and set quality bars width', function() {
-        //given
-        scope.column = {
-            'id': 'MostPopulousCity',
-            'quality': {
-                'empty': 5,
-                'invalid': 10,
-                'valid': 72
-            },
-            'type': 'string'
-        };
-
-        //when
-        var element = createElement(scope);
-
-        //then
-        expect(element.find('.grid-header-title').text()).toBe('MostPopulousCity');
-        expect(element.find('.grid-header-type').text()).toBe('string');
-        expect(element.find('.record-ok').css('width')).toBe('78%');
-        expect(element.find('.record-empty').css('width')).toBe('10%');
-        expect(element.find('.record-nok').css('width')).toBe('12%');
+        var firstRowCells = element.find('tbody').find('tr').eq(0).find('td');
+        var secondRowCells = element.find('tbody').find('tr').eq(1).find('td');
+        
+        expect(firstRowCells.length).toBe(4);
+        expect(firstRowCells.eq(0).text()).toBe('AL');
+        expect(firstRowCells.eq(1).text()).toBe('Alabama');
+        expect(firstRowCells.eq(2).text()).toBe('Montgomery');
+        expect(firstRowCells.eq(3).text()).toBe('Birmingham city');
+        
+        expect(secondRowCells.length).toBe(4);
+        expect(secondRowCells.eq(0).text()).toBe('AK');
+        expect(secondRowCells.eq(1).text()).toBe('Alaska');
+        expect(secondRowCells.eq(2).text()).toBe('Juneau');
+        expect(secondRowCells.eq(3).text()).toBe('Anchorage');
     });
     
 });
