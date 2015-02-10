072b4d6374e915600a55f65bdd72240e25e3e9ca
fhuaulme <fhuaulme@talend.com>
2015-02-10 16:58:14 +0100

    TDP-11: "Spark engine - jar allows a basic transformation" (Transformation service)
    [Platform tests] Updates stress test to include potential 202 response from data set service.
    (https://jira.talendforge.org/browse/TDP-11)

--- dataprep-platform-tests/src/test/scala/dataprep/DataSetCreation.scala
+++ dataprep-platform-tests/src/test/scala/dataprep/DataSetCreation.scala
@@ -7,16 +7,21 @@ import scala.concurrent.duration._
 
 class DataSetCreation extends Simulation {
   val httpConf = http
-    .baseURL("http://localhost:8888/")
+    .baseURL("http://api.tdp.talend.lan/")
     .acceptHeader("application/json,application/xml;q=0.9,*/*;q=0.8") // Here are the common headers
     .doNotTrackHeader("1")
   val scn = scenario("Create / Read / No Op Transform")
     .during(1 minute) {
     group("actions") {
-      exec(http("creation").post("api/datasets/").body(RawFileBody("data/test1.csv")).check(bodyString.find.saveAs("dataset"))).exec(http("read").get("api/datasets/${dataset}"))
+      exec(http("creation").post("api/datasets/").body(RawFileBody("data/test1.csv")).check(bodyString.saveAs("dataset")))
+        .exec(http("read_1").get("api/datasets/${dataset}").check(status.saveAs("dataset_get_status")))
+        .asLongAs(session => session.get("dataset_get_status").as[Integer].equals(202)) {
+        exec(http("read_2").get("api/datasets/${dataset}").check(status.saveAs("dataset_get_status")))
+          .pause(500 millis)
+      }
         .exec(http("transform").post("api/transform/${dataset}/"))
         .pause(500 millis)
     }
   }
-  setUp(scn.inject(rampUsers(30) over (30 seconds))).protocols(httpConf)
+  setUp(scn.inject(rampUsers(100) over (30 seconds))).protocols(httpConf)
 }
