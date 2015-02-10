bf4102a688af392697a10f9ca9ff6456af405a77
jsomsanith <jsomsanith@talend.com>
2015-02-10 10:28:38 +0100

    TDP-28 : bindToController in all directives

--- dataprep-webapp/src/components/dataset/dataset-grid/dataset-column-header-directive.js
+++ dataprep-webapp/src/components/dataset/dataset-grid/dataset-column-header-directive.js
@@ -8,22 +8,24 @@
             scope:{
                 column : '='
             },
-            link: function(scope) {
+            bindToController: true,
+            controllerAs: 'datasetHeaderCtrl',
+            controller: function() {
                 var MIN_PERCENT = 10;
 
-                scope.column.total = scope.column.quality.valid + scope.column.quality.empty + scope.column.quality.invalid;
+                this.column.total = this.column.quality.valid + this.column.quality.empty + this.column.quality.invalid;
 
                 // *_percent is the real % of empty/valid/invalid records, while *_percent_width is the width % of the bar.
                 // They can be differents if less than MIN_PERCENT are valid/invalid/empty, to assure a min width of each bar. To be usable by the user.
                 // TODO remove completely one bar if absolute zero records match (ie: if 0 invalid records, do not display invalid bar)
-                scope.column.quality.emptyPercent = Math.ceil(scope.column.quality.empty * 100 / scope.column.total);
-                scope.column.quality.emptyPercentWidth = Math.max(scope.column.quality.emptyPercent, MIN_PERCENT);
+                this.column.quality.emptyPercent = Math.ceil(this.column.quality.empty * 100 / this.column.total);
+                this.column.quality.emptyPercentWidth = Math.max(this.column.quality.emptyPercent, MIN_PERCENT);
 
-                scope.column.quality.invalidPercent = Math.ceil(scope.column.quality.invalid * 100 / scope.column.total);
-                scope.column.quality.invalidPercentWidth = Math.max(scope.column.quality.invalidPercent, MIN_PERCENT);
+                this.column.quality.invalidPercent = Math.ceil(this.column.quality.invalid * 100 / this.column.total);
+                this.column.quality.invalidPercentWidth = Math.max(this.column.quality.invalidPercent, MIN_PERCENT);
 
-                scope.column.quality.validPercent = 100 - scope.column.quality.emptyPercent - scope.column.quality.invalidPercent;
-                scope.column.quality.validPercentWidth = 100 - scope.column.quality.emptyPercentWidth - scope.column.quality.invalidPercentWidth;
+                this.column.quality.validPercent = 100 - this.column.quality.emptyPercent - this.column.quality.invalidPercent;
+                this.column.quality.validPercentWidth = 100 - this.column.quality.emptyPercentWidth - this.column.quality.invalidPercentWidth;
             }
         };
     }
