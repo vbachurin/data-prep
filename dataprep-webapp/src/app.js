c81bd61cee6a9e8cd46474d110d2b2e219649019
jsomsanith <jsomsanith@talend.com>
2015-02-23 11:05:07 +0100

    TDP-41 : angularjs-toaster lib

--- dataprep-webapp/src/app.js
+++ dataprep-webapp/src/app.js
@@ -9,7 +9,8 @@
 
             'talend.widget', //components widget built on bourbon (modal, dropdown, ...)
 
-            'ui.router'
+            'ui.router', //most advanced router
+            'toaster' //toaster popup
         ])
 
         .config(['$httpProvider', '$compileProvider', 'disableDebug', function ($httpProvider, $compileProvider, disableDebug) {
