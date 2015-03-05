22d1170a4ba0047063d7a576bb7b5b6eb5a8955c
Stef Mallet <stephane.mallet@gmail.com>
2015-03-05 16:40:24 +0100

    TDP-47: add 'cut' action

---
+++ dataprep-transformation/src/main/java/org/talend/dataprep/transformation/api/action/metadata/Cut.java
@@ -0,0 +1,90 @@
+package org.talend.dataprep.transformation.api.action.metadata;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.function.Consumer;
+
+import org.apache.commons.lang.StringUtils;
+import org.codehaus.jackson.JsonNode;
+import org.talend.dataprep.api.DataSetRow;
+import org.talend.dataprep.api.type.Types;
+import org.talend.dataprep.transformation.api.action.ActionParser;
+
+public class Cut implements ActionMetadata {
+
+    public static final String         COLUMN_NAME_PARAMETER = "column_name";                                    //$NON-NLS-1$
+
+    public static final String         PATTERN_PARAMETER     = "pattern";                                        //$NON-NLS-1$
+
+    public static final String         CUT_ACTION_NAME       = "cut";                                            //$NON-NLS-1$
+
+    public static final String         CUT_ACTION_DESC       = "remove specified text from cells in this column"; //$NON-NLS-1$
+
+    public static final ActionMetadata INSTANCE              = new Cut();
+
+    private Cut() {
+    }
+
+    @Override
+    public String getName() {
+        return CUT_ACTION_NAME;
+    }
+
+    @Override
+    public String getDescription() {
+        return CUT_ACTION_DESC;
+    }
+
+    @Override
+    public Type getType() {
+        return Type.OPERATION;
+    }
+
+    @Override
+    public String getCategory() {
+        return "repair";
+    }
+
+    @Override
+    public Item[] getItems() {
+        return new Item[0];
+    }
+
+    @Override
+    public String getValue() {
+        return StringUtils.EMPTY;
+    }
+
+    @Override
+    public Parameter[] getParameters() {
+        return new Parameter[] { new Parameter(COLUMN_NAME_PARAMETER, Types.STRING.getName(), StringUtils.EMPTY),
+                new Parameter(PATTERN_PARAMETER, Types.STRING.getName(), StringUtils.EMPTY) };
+    }
+
+    @Override
+    public Consumer<DataSetRow> create(Iterator<Map.Entry<String, JsonNode>> parameters) {
+        Map<String, String> parsedParameters = new HashMap<>();
+        while (parameters.hasNext()) {
+            Map.Entry<String, JsonNode> currentParameter = parameters.next();
+            switch (currentParameter.getKey()) {
+            case COLUMN_NAME_PARAMETER:
+                parsedParameters.put(COLUMN_NAME_PARAMETER, currentParameter.getValue().getTextValue());
+                break;
+            case PATTERN_PARAMETER:
+                parsedParameters.put(PATTERN_PARAMETER, currentParameter.getValue().getTextValue());
+                break;
+            default:
+                ActionParser.LOGGER
+                        .warn("Parameter '" + currentParameter.getKey() + "' is not recognized for " + this.getClass());
+            }
+        }
+        return row -> {
+            String columnName = parsedParameters.get(COLUMN_NAME_PARAMETER);
+            String value = row.get(columnName);
+            if (value != null) {
+                row.set(columnName, value.replace(parsedParameters.get(PATTERN_PARAMETER), ""));
+            }
+        };
+    }
+}
