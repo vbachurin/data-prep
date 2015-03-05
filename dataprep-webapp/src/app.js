e42443b82df9fc2d881caf273a1b41c74d10b903
jsomsanith <jsomsanith@talend.com>
2015-03-05 11:54:12 +0100

    TDP-30 : merge with master

--- dataprep-webapp/src/app.js
+++ dataprep-webapp/src/app.js
@@ -54,6 +54,5 @@
         .run(function ($window, $translate) {
             var language = ($window.navigator.language === 'fr') ? 'fr' : 'en';
             $translate.use(language);
-            $translate.use('fr');
         });
 })();
