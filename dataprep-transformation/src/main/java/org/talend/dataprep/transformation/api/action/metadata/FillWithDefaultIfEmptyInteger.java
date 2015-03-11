a4a5b84384c45440643092b49bffd2053aeebad8
Stef Mallet <stephane.mallet@gmail.com>
2015-03-11 18:47:12 +0100

    TDP-52: fix previous commit (fingers crossed)

---
+++ dataprep-transformation/src/main/java/org/talend/dataprep/transformation/api/action/metadata/FillWithDefaultIfEmptyInteger.java
@@ -0,0 +1,24 @@
+package org.talend.dataprep.transformation.api.action.metadata;
+
+import org.apache.commons.lang.StringUtils;
+import org.talend.dataprep.api.type.Types;
+import org.talend.dataprep.transformation.api.action.metadata.Item.Value;
+
+public class FillWithDefaultIfEmptyInteger extends FillWithDefaultIfEmpty {
+
+    public static final String         FILL_EMPTY_ACTION_NAME = "fillemptywithdefaultinteger";      //$NON-NLS-1$
+
+    public static final ActionMetadata INSTANCE               = new FillWithDefaultIfEmptyInteger();
+
+    @Override
+    public String getName() {
+        return FILL_EMPTY_ACTION_NAME;
+    }
+
+    @Override
+    public Parameter[] getParameters() {
+        return new Parameter[] { new Parameter(COLUMN_NAME_PARAMETER, Types.STRING.getName(), StringUtils.EMPTY),
+                new Parameter(DEFAULT_VALUE_PARAMETER, Types.INTEGER.getName(), "0") };
+    }
+
+}
