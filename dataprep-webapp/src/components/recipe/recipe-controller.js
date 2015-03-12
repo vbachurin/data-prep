3b26148a6b85d7539f663ab43f2c95a0ee696116
jsomsanith <jsomsanith@talend.com>
2015-03-12 14:16:26 +0100

    TDP-49 : Add recipe items in recipe menu (simple items, with no param)

--- dataprep-webapp/src/components/recipe/recipe-controller.js
+++ dataprep-webapp/src/components/recipe/recipe-controller.js
@@ -1,8 +1,19 @@
 (function() {
     'use strict';
 
-    function RecipeCtrl() {
+    function RecipeCtrl(RecipeService) {
+        var vm = this;
+        vm.recipeService = RecipeService;
     }
+
+    Object.defineProperty(RecipeCtrl.prototype,
+        'recipe', {
+            enumerable: true,
+            configurable: false,
+            get: function () {
+                return this.recipeService.getRecipe();
+            }
+        });
 
     angular.module('data-prep.recipe')
         .controller('RecipeCtrl', RecipeCtrl);
--- dataprep-webapp/src/services/recipe/recipe-service.js
+++ dataprep-webapp/src/services/recipe/recipe-service.js
@@ -2,7 +2,22 @@
     'use strict';
 
     function RecipeService() {
+        var recipe = [];
 
+        this.getRecipe = function() {
+            return recipe;
+        };
+
+        var createRecipeItem = function(column, menu) {
+            return {
+                transformation: angular.copy(menu),
+                column: column.id
+            };
+        };
+
+        this.add = function(column, menu) {
+            recipe.push(createRecipeItem(column, menu));
+        };
     }
 
     angular.module('data-prep.services.recipe')
