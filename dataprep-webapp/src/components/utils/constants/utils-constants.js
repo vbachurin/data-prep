33f7faa509901365326d9014663b00baffaf1e3a
Marc Maillart <mmaillart@talend.com>
2015-03-27 17:48:15 +0100

    intro.js workflow demonstration

---
+++ dataprep-webapp/src/components/utils/constants/utils-constants.js
@@ -0,0 +1,9 @@
+(function() {
+    'use strict';
+
+    angular.module('data-prep-utils')
+        //.constant('apiUrl', 'http://10.42.40.33:8081') // JIMMY
+        .constant('apiUrl', 'http://10.42.10.99:8081') // VM1
+        //.constant('apiUrl', 'http://10.42.10.40:8081') // VM2
+        .constant('disableDebug', false);
+})();
