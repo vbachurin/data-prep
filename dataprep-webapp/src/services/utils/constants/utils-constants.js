90b427aa8cd6c36fc537fe87108277c616e5bfed
Marc Maillart <mmaillart@talend.com>
2015-03-30 17:40:19 +0200

    data grid UI design

--- dataprep-webapp/src/services/utils/constants/utils-constants.js
+++ dataprep-webapp/src/services/utils/constants/utils-constants.js
@@ -2,6 +2,7 @@
     'use strict';
 
     angular.module('data-prep.services.utils')
-        .constant('apiUrl', 'http://10.42.10.99:8081')
+      //.constant('apiUrl', 'http://10.42.10.99:8081')
+      .constant('apiUrl', 'http://10.42.10.44:8081')
         .constant('disableDebug', false);
 })();
