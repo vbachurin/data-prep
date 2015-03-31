8a6f2930dde4cd6917c4891dc8b262925573616f
jsomsanith <jsomsanith@talend.com>
2015-03-31 14:58:29 +0200

    TDP-62 : slidable with resizable option, inventory data panel resizable

--- dataprep-webapp/src/components/widgets/slidable/widget-slidable-directive.js
+++ dataprep-webapp/src/components/widgets/slidable/widget-slidable-directive.js
@@ -1,7 +1,7 @@
 (function() {
     'use strict';
 
-    function TalendSlidable() {
+    function TalendSlidable($window) {
         return {
             restrict: 'E',
             transclude: true,
@@ -9,7 +9,8 @@
             templateUrl: 'components/widgets/slidable/slidable.html',
             scope: {
                 side: '@',
-                visible: '='
+                visible: '=',
+                resizable: '@'
             },
             bindToController: true,
             controllerAs: 'slidableCtrl',
@@ -19,6 +20,45 @@
                 vm.toggle = function() {
                     vm.visible = ! vm.visible;
                 };
+            },
+            link: function(scope, iElement, iAttrs, ctrl) {
+                if(ctrl.resizable) {
+                    var localStorageWidthKey = 'data-prep-' + ctrl.resizable + '-width';
+                    var width = $window.localStorage.getItem(localStorageWidthKey) || iElement.width() + 'px';
+
+                    iElement.resizable({
+                        handles: ctrl.side === 'right' ? 'w' : 'e',
+                        start: function() {
+                            iElement.addClass('no-transition');
+                        },
+                        stop: function(event, ui) {
+                            iElement.removeClass('no-transition');
+                            width = ui.size.width + 'px';
+                            $window.localStorage.setItem(localStorageWidthKey, width);
+                        },
+                        resize: function(event, ui) {
+                            iElement.css('left', 'auto');
+                            iElement.css('right', 'auto');
+                            iElement.css('flex', '0 ' + ui.size.width + 'px');
+                        }
+                    });
+
+                    scope.$watch(
+                        function() {
+                            return ctrl.visible;
+                        },
+                        function(visible) {
+                            if(visible) {
+                                iElement.css('flex', '0 ' + width);
+                                iElement.resizable('option', 'disabled', false);
+                            }
+                            else {
+                                iElement.css('flex', '');
+                                iElement.resizable('option', 'disabled', true);
+                            }
+                        }
+                    );
+                }
             }
         };
     }
