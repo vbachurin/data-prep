cc4b5e7aab9e433832c0ff58863ffa458d572aa2
fhuaulme <fhuaulme@talend.com>
2015-04-01 22:37:22 +0200

    Initial refactoring: prepare serialization configuration to use @Autowired fields.

--- dataprep-backend-common/src/main/java/org/talend/dataprep/api/preparation/json/PreparationMetadataModule.java
+++ dataprep-backend-common/src/main/java/org/talend/dataprep/api/preparation/json/PreparationMetadataModule.java
@@ -1,18 +1,16 @@
 package org.talend.dataprep.api.preparation.json;
 
-import org.talend.dataprep.api.dataset.DataSetMetadata;
+import org.springframework.stereotype.Component;
 import org.talend.dataprep.api.preparation.Preparation;
 
 import com.fasterxml.jackson.core.Version;
-import com.fasterxml.jackson.databind.Module;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 
+@Component
 public class PreparationMetadataModule extends SimpleModule {
 
-    public static final Module DEFAULT = new PreparationMetadataModule();
-
-    private PreparationMetadataModule() {
-        super(DataSetMetadata.class.getName(), new Version(1, 0, 0, null, null, null));
+    public PreparationMetadataModule() {
+        super(Preparation.class.getName(), new Version(1, 0, 0, null, null, null));
         // We can omit deserialization as deserialization is straight forward.
         addSerializer(Preparation.class, new PreparationJsonSerializer());
     }
