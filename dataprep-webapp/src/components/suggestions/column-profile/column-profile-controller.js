80e52c8847325dd70d5a4a240b6d32e94a8fc874
jsomsanith <jsomsanith@talend.com>
2015-04-02 10:09:29 +0200

    TDP-33 : chart experiment, front only

--- dataprep-webapp/src/components/suggestions/column-profile/column-profile-controller.js
+++ dataprep-webapp/src/components/suggestions/column-profile/column-profile-controller.js
@@ -1,9 +1,56 @@
 (function() {
     'use strict';
 
-    function ColumnProfileCtrl(DatasetGridService) {
+    function ColumnProfileCtrl($scope, DatasetGridService, StatisticsService) {
         var vm = this;
         vm.datasetGridService = DatasetGridService;
+
+        $scope.$watch(
+            function() {
+                return DatasetGridService.selectedColumnId;
+            },
+            function(colId) {
+                if(colId) {
+                    vm.distribution = StatisticsService.getDistribution(colId);
+                    console.log(vm.distribution.length * 20);
+                    vm.chartConfig = {
+                        options: {
+                            chart: {
+                                type: 'bar'
+                            }
+                        },
+                        xAxis: {
+                            categories: _.map(vm.distribution, function(item) {return item.colVal}),
+                            labels: {
+                                enabled: true
+                            }
+                        },
+                        series: [{
+                            name: 'Number of item',
+                            data: _.map(vm.distribution, function(item) {return item.nb})
+                        }],
+                        title: {
+                            text: colId
+                        },
+                        yAxis: {
+                            title: {
+                                text: ""
+                            }
+                        },
+
+                        size: {
+                            height: vm.distribution.length * 20 + 150,
+                            width: 220
+                        },
+                        loading: false
+                    }
+
+                }
+                else {
+                    vm.distribution = null;
+                }
+            }
+        );
     }
 
     Object.defineProperty(ColumnProfileCtrl.prototype,
