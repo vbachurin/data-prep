2d0b051e177e19545f64e694816b1bbb34940a35
fhuaulme <fhuaulme@talend.com>
2015-04-02 09:39:05 +0200

    Fix preparation serializer registration issue (wait for all beans to be initialized before registration).

--- dataprep-backend-common/src/main/java/org/talend/dataprep/api/preparation/json/PreparationJsonSerializer.java
+++ dataprep-backend-common/src/main/java/org/talend/dataprep/api/preparation/json/PreparationJsonSerializer.java
@@ -2,13 +2,11 @@ package org.talend.dataprep.api.preparation.json;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 
-import org.springframework.beans.BeansException;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.talend.dataprep.api.preparation.*;
 import org.talend.dataprep.transformation.api.action.metadata.ActionMetadata;
@@ -18,9 +16,15 @@ import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 
 @Component
-class PreparationJsonSerializer extends JsonSerializer<Preparation> implements ApplicationContextAware {
+public class PreparationJsonSerializer extends JsonSerializer<Preparation> {
 
-    private static ApplicationContext applicationContext;
+    public static final Logger LOGGER = LoggerFactory.getLogger(PreparationJsonSerializer.class);
+
+    @Autowired(required = false)
+    PreparationRepository versionRepository;
+
+    @Autowired(required = false)
+    ActionMetadata[] actionMetadata;
 
     @Override
     public void serialize(Preparation preparation, JsonGenerator generator, SerializerProvider serializerProvider)
@@ -33,54 +37,39 @@ class PreparationJsonSerializer extends JsonSerializer<Preparation> implements A
             generator.writeStringField("name", preparation.getName()); //$NON-NLS-1$
             generator.writeNumberField("creationDate", preparation.getCreationDate()); //$NON-NLS-1$
             generator.writeNumberField("lastModificationDate", preparation.getLastModificationDate()); //$NON-NLS-1$
-            PreparationRepository versionRepository = getRepository();
             if (versionRepository != null && preparation.getStep() != null) {
                 // Steps
                 final List<String> steps = PreparationUtils.listSteps(preparation.getStep(), versionRepository);
-                generator.writeObjectField("steps", steps);
+                generator.writeObjectField("steps", steps); //$NON-NLS-1$
 
                 // Actions
                 Step step = versionRepository.get(preparation.getStep().id(), Step.class);
                 PreparationActions prepActions = versionRepository.get(step.getContent(), PreparationActions.class);
                 List<Action> actions = prepActions.getActions();
                 generator.writeObjectField("actions", actions); //$NON-NLS-1$
+
                 // Actions metadata
-                Collection<ActionMetadata> actionMetadata = getActionMetadata();
-                List<ActionMetadata> metadataList = new ArrayList<>(actions.size());
-                for (Action action : actions) {
-                    String actionName = action.getAction();
-                    for (ActionMetadata metadata : actionMetadata) {
-                        if (metadata.getName().equals(actionName)) {
-                            metadataList.add(metadata);
-                            break;
+                if (actionMetadata != null) {
+                    List<ActionMetadata> metadataList = new ArrayList<>(actions.size());
+                    for (Action action : actions) {
+                        String actionName = action.getAction();
+                        for (ActionMetadata metadata : actionMetadata) {
+                            if (metadata.getName().equals(actionName)) {
+                                metadataList.add(metadata);
+                                break;
+                            }
                         }
                     }
+                    generator.writeObjectField("metadata", metadataList); //$NON-NLS-1$
+                } else {
+                    LOGGER.debug("No action metadata available, unable to serialize action metadata for preparation {}.",
+                            preparation.id());
                 }
-                generator.writeObjectField("metadata", metadataList); //$NON-NLS-1$
+            } else {
+                LOGGER.debug("No version repository available, unable to serialize steps for preparation {}.", preparation.id());
             }
         }
         generator.writeEndObject();
         generator.flush();
-    }
-
-    private Collection<ActionMetadata> getActionMetadata() {
-        if (applicationContext.getBeanNamesForType(PreparationRepository.class).length > 0) {
-            return applicationContext.getBeansOfType(ActionMetadata.class).values();
-        } else {
-            return Collections.emptyList();
-        }
-    }
-
-    private PreparationRepository getRepository() {
-        if (applicationContext.getBeanNamesForType(PreparationRepository.class).length > 0) {
-            return applicationContext.getBean(PreparationRepository.class);
-        } else {
-            return null;
-        }
-    }
-
-    @Override
-    public void setApplicationContext(ApplicationContext context) throws BeansException {
-        applicationContext = context;
     }
 }
--- dataprep-backend-common/src/main/java/org/talend/dataprep/api/preparation/json/PreparationMetadataModule.java
+++ dataprep-backend-common/src/main/java/org/talend/dataprep/api/preparation/json/PreparationMetadataModule.java
@@ -1,5 +1,8 @@
 package org.talend.dataprep.api.preparation.json;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.talend.dataprep.api.preparation.Preparation;
 
@@ -9,10 +12,17 @@ import com.fasterxml.jackson.databind.module.SimpleModule;
 @Component
 public class PreparationMetadataModule extends SimpleModule {
 
+    @Autowired
+    PreparationJsonSerializer preparationJsonSerializer;
+
     public PreparationMetadataModule() {
         super(Preparation.class.getName(), new Version(1, 0, 0, null, null, null));
+    }
+
+    @PostConstruct
+    public void init() {
         // We can omit deserialization as deserialization is straight forward.
-        addSerializer(Preparation.class, new PreparationJsonSerializer());
+        addSerializer(Preparation.class, preparationJsonSerializer);
     }
 
 }
