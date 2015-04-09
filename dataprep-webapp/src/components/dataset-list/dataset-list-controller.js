81cf24a339e03c09d2ba4a2a454429565d6d44cd
Stef Mallet <stephane.mallet@gmail.com>
2015-04-09 15:56:39 +0200

    TDP-58: fix js unit test

--- dataprep-webapp/src/components/dataset-list/dataset-list-controller.js
+++ dataprep-webapp/src/components/dataset-list/dataset-list-controller.js
@@ -24,7 +24,7 @@
                     return DatasetService.deleteDataset(dataset);
                 })
                 .then(function() {
-                    MessageService.success('REMOVE_SUCCESS_TITLE', 'REMOVE_SUCCESS', {type:'preparation', name: dataset.name});
+                    MessageService.success('REMOVE_SUCCESS_TITLE', 'REMOVE_SUCCESS', {type: 'dataset', name: dataset.name});
                     DatasetListService.refreshDatasets();
                 });
         };
