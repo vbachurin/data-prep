7ae03c86cddda8a9fa1793740e6f4789694bc9d9
jsomsanith <jsomsanith@talend.com>
2015-04-13 17:50:50 +0200

    TDP-72 : fix sync between preparations and datasets while getting preparations list

--- dataprep-webapp/src/services/preparation/list/preparation-list-service.js
+++ dataprep-webapp/src/services/preparation/list/preparation-list-service.js
@@ -6,34 +6,37 @@
         self.preparations = [];
 
         /**
-         * Wait for datasets, and bind the corresponding dataset to every preparation
+         * Bind the corresponding dataset to every preparation
          * @param preparations
          */
-        var adaptMetadataInfos = function(preparations) {
-            DatasetListService.getDatasetsPromise()
-                .then(function(datasets) {
-                    _.forEach(preparations, function(prep) {
-                        var correspondingDataset = _.find(datasets, function(dataset) {
-                            return dataset.id === prep.dataSetId;
-                        });
-                        prep.dataset = correspondingDataset;
-                    });
+        var adaptMetadataInfos = function(preparations, datasets) {
+            _.forEach(preparations, function(prep) {
+                var correspondingDataset = _.find(datasets, function(dataset) {
+                    return dataset.id === prep.dataSetId;
                 });
+                prep.dataset = correspondingDataset;
+            });
         };
 
         /**
          * Refresh preparation list
          */
         self.refreshPreparations = function() {
-            return PreparationService.getPreparations()
-                .then(function(result) {
-                    adaptMetadataInfos(result.data);
-                    self.preparations = result.data;
+            return $q.all([PreparationService.getPreparations(), DatasetListService.getDatasetsPromise()])
+                .then(function(results) {
+                    var preparationResult = results[0];
+                    var datasets = results[1];
+                    adaptMetadataInfos(preparationResult.data, datasets);
+                    self.preparations = preparationResult.data;
 
                     return self.preparations;
                 });
         };
 
+        /**
+         * Return preparation promise that resolve current preparation list if not empty, or call GET service
+         * @returns Promise
+         */
         self.getPreparationsPromise = function() {
             if(self.preparations.length) {
                 return $q.when(self.preparations);
