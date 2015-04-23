c1e8abb6e306291893f852b81b78c0ad421fb14c
Vincent Jaouën <vjaouen@talend.com>
2015-04-23 10:53:59 +0200

    merge conflict with branch master

--- dataprep-webapp/src/services/utils/constants/utils-constants.js
+++ dataprep-webapp/src/services/utils/constants/utils-constants.js
@@ -2,6 +2,16 @@
     'use strict';
 
     angular.module('data-prep.services.utils')
+        /**
+         * @ngdoc object
+         * @name data-prep.services.utils.service:apiUrl
+         * @description The REST api base url
+         */
         .constant('apiUrl', 'http://10.42.10.99:8888')
+        /**
+         * @ngdoc object
+         * @name data-prep.services.utils.service:disableDebug
+         * @description Application option. Disable debug mode (ex: in production) for performance
+         */
         .constant('disableDebug', false);
 })();
