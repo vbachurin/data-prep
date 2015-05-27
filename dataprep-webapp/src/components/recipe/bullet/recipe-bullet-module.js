030aef5570ce882267006022b2346624e1dd1fee
amaalej <amaalej@talend.com>
2015-05-27 17:46:15 +0200

    refactoring recipe-bullet directive, remaining when the user resizes the playground and test

---
+++ dataprep-webapp/src/components/recipe/bullet/recipe-bullet-module.js
@@ -0,0 +1,10 @@
+(function() {
+    'use strict';
+
+    /**
+     * @ngdoc object
+     * @name data-prep.recipeBullet
+     * @description This module contains the controller and directives to manage the recipe Bullets
+     */
+    angular.module('data-prep.recipeBullet', []);
+})();
--- dataprep-webapp/src/components/recipe/recipe-module.js
+++ dataprep-webapp/src/components/recipe/recipe-module.js
@@ -7,6 +7,7 @@
      * @description This module contains the controller and directives to manage the recipe
      * @requires data-prep.services.playground
      * @requires data-prep.services.recipe
+     * @requires data-prep.services.recipeBullet
      * @requires data-prep.services.preparation
      */
     angular.module('data-prep.recipe', [
@@ -14,6 +15,6 @@
         'data-prep.services.playground',
         'data-prep.services.recipe',
         'data-prep.services.preparation',
-        'data-prep.chainLink'
+        'data-prep.recipeBullet'
     ]);
 })();
