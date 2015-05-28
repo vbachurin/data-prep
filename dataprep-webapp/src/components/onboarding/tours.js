5f0ffc9b518151f45f047268cca17728c10d0097
Stef Mallet <stephane.mallet@gmail.com>
2015-05-28 16:31:51 +0200

    TDP-155: cleaning this - first step

--- dataprep-webapp/src/components/onboarding/tours.js
+++ dataprep-webapp/src/components/onboarding/tours.js
@@ -1,44 +1,29 @@
-function startTourNavBar(){
-  var navBarItems = introJs();
-  navBarItems.setOptions({
+function startTourIntro(){
+  var introTourItems = introJs();
+    introTourItems.setOptions({
     steps: [
         {
-          element: '#help-logo',
-          title: 'Logo',
-          intro: 'This is Talend logo!',
+          element: '#help-import-local',
+          title: 'Import local file button',
+          intro: 'Click here to import your new Dataset (csv, xls, etc.) and start play with it. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mollis augue a neque cursus ac blandit orci faucibus. Phasellus nec metus purus.',
           position: 'right'
         },
         {
-          element: '#help-mkt',
-          title: 'Market place',
-          intro: 'Click this to open market place.',
-          position: 'bottom'
-        }
-    ]
-  });
-
-  navBarItems.start();
-}
-
-function startTourFooter(){
-  var footerItems = introJs();
-  footerItems.setOptions({
-    steps: [
-        {
-          element: '#help-mail',
-          title: 'A nice icon',
-          intro: 'This is a nice mail icon',
-          position: 'bottom'
+          element: '#btn-toggle-right-panel',
+          title: 'Show / Hide Left Hand Side Panel',
+          intro: 'Show / Hide interesting information (owner, creation date, size, etc.) about the dataset you have selected. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mollis augue a neque cursus ac blandit orci faucibus. Phasellus nec metus purus.',
+          position: 'left'
         },
         {
-          element: '#help-version',
-          title: 'Version',
-          intro: 'This is the current version',
-          position: 'top'
+            element: '#help-dataset-nav',
+            title: 'Quick Data Access',
+            intro: 'This is the place you can quickly access the dataset you are working on.',
+            position: 'right'
         }
     ]
   });
 
-  footerItems.start();
+    introTourItems.start();
 }
+
 
