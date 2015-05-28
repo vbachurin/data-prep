fe01b902bca2b1e22dac28034a41fc9ef9b8421e
fhuaulme <fhuaulme@talend.com>
2015-05-28 16:08:54 +0200

    Reverts back end IP overridden with boot2docker IP.

--- dataprep-webapp/src/services/utils/constants/utils-constants.js
+++ dataprep-webapp/src/services/utils/constants/utils-constants.js
@@ -7,7 +7,7 @@
          * @name data-prep.services.utils.service:apiUrl
          * @description The REST api base url
          */
-        .constant('apiUrl', 'http://192.168.174.170:8888')
+        .constant('apiUrl', 'http://10.42.10.99:8888')
         /**
          * @ngdoc object
          * @name data-prep.services.utils.service:disableDebug
