c5ab7843efb33a7d35554e9bd431cd61b62c3f7c
Olivier Lamy <olamy@apache.org>
2015-05-28 11:23:56 +1000

    no need of static instance, can just cause a memmory leak

--- dataprep-backend-common/src/main/java/org/talend/dataprep/transformation/api/action/ActionParser.java
+++ dataprep-backend-common/src/main/java/org/talend/dataprep/transformation/api/action/ActionParser.java
@@ -31,7 +31,7 @@ public class ActionParser implements BeanFactoryAware {
     /** This class' logger. */
     public static final Logger LOGGER = LoggerFactory.getLogger(ActionParser.class);
 
-    private static BeanFactory beanFactory;
+    private BeanFactory beanFactory;
 
     /**
      * Return the parsed actions ready to be run.
@@ -110,6 +110,6 @@ public class ActionParser implements BeanFactoryAware {
 
     @Override
     public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
-        ActionParser.beanFactory = beanFactory;
+        this.beanFactory = beanFactory;
     }
 }
