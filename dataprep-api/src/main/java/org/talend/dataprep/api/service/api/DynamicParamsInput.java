f8dc4e8dc0a63c21c84bb9c15b81afc210f2b2ac
jsomsanith <jsomsanith@talend.com>
2015-05-28 17:30:02 +0200

    TDP-136 : get dynamic params for preparation at a specified step

--- dataprep-api/src/main/java/org/talend/dataprep/api/service/api/DynamicParamsInput.java
+++ dataprep-api/src/main/java/org/talend/dataprep/api/service/api/DynamicParamsInput.java
@@ -10,6 +10,10 @@ public class DynamicParamsInput {
      */
     private String preparationId;
     /**
+     * The step id. If not provided, this is considered as 'head' version
+     */
+    private String stepId = "head";
+    /**
      * The dataset id. If this is null, preparationId must be set
      */
     private String datasetId;
@@ -42,5 +46,13 @@ public class DynamicParamsInput {
 
     public void setColumnId(String columnId) {
         this.columnId = columnId;
+    }
+
+    public String getStepId() {
+        return stepId;
+    }
+
+    public void setStepId(String stepId) {
+        this.stepId = stepId;
     }
 }
