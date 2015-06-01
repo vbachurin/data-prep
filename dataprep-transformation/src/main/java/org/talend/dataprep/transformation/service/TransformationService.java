1464e027806c2522417c385e31d4d135acd241e2
Olivier Lamy <olamy@apache.org>
2015-06-01 21:22:29 +1000

    fix previous merge

--- dataprep-transformation/src/main/java/org/talend/dataprep/transformation/service/TransformationService.java
+++ dataprep-transformation/src/main/java/org/talend/dataprep/transformation/service/TransformationService.java
@@ -11,6 +11,7 @@ import java.util.Base64;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import javax.servlet.http.HttpServletResponse;
