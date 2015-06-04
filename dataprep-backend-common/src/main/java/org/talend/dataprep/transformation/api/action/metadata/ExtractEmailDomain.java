303a484628aeb2fe478a952f2632f0da335e1a8e
Stef Mallet <stephane.mallet@gmail.com>
2015-06-04 22:46:43 +0200

    TDP-180: implements action ExtractEmailDomain

---
+++ dataprep-backend-common/src/main/java/org/talend/dataprep/transformation/api/action/metadata/ExtractEmailDomain.java
@@ -0,0 +1,136 @@
+package org.talend.dataprep.transformation.api.action.metadata;
+
+import java.util.*;
+import java.util.function.Consumer;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.stereotype.Component;
+import org.talend.dataprep.api.dataset.ColumnMetadata;
+import org.talend.dataprep.api.dataset.DataSetRow;
+import org.talend.dataprep.api.dataset.RowMetadata;
+import org.talend.dataprep.api.type.Type;
+import org.talend.dataprep.transformation.api.action.parameters.Item;
+
+/**
+ * Split a cell value on a separator.
+ */
+@Component(ExtractEmailDomain.ACTION_BEAN_PREFIX + ExtractEmailDomain.EXTRACT_DOMAIN_ACTION_NAME)
+public class ExtractEmailDomain extends SingleColumnAction {
+
+    /** The action name. */
+    public static final String EXTRACT_DOMAIN_ACTION_NAME = "extractemaildomain"; //$NON-NLS-1$
+
+    /**
+     * Private constructor to ensure IoC use.
+     */
+    protected ExtractEmailDomain() {
+    }
+
+    /**
+     * @see ActionMetadata#getName()
+     */
+    @Override
+    public String getName() {
+        return EXTRACT_DOMAIN_ACTION_NAME;
+    }
+
+    /**
+     * @see ActionMetadata#getCategory()
+     */
+    @Override
+    public String getCategory() {
+        return "columns"; //$NON-NLS-1$
+    }
+
+    /**
+     * @see ActionMetadata#getCompatibleColumnTypes()
+     */
+    @Override
+    public Set<Type> getCompatibleColumnTypes() {
+        return Collections.singleton(Type.STRING);
+    }
+
+    /**
+     * @see ActionMetadata#getItems()@return
+     */
+    @Override
+    public Item[] getItems() {
+        return new Item[] {};
+    }
+
+    /**
+     * Split the column for each row.
+     *
+     * @see ActionMetadata#create(Map)
+     */
+    @Override
+    public Consumer<DataSetRow> create(Map<String, String> parameters) {
+        String columnName = parameters.get(COLUMN_ID);
+        String realSeparator = "@";
+
+        return row -> {
+            String originalValue = row.get(columnName);
+            if (originalValue != null) {
+                String[] split = originalValue.split(realSeparator, 2);
+
+                String local_part = (split.length >= 2 ? split[0] : StringUtils.EMPTY);
+                row.set(columnName + "_local", local_part);
+
+                String domain_part = (split.length >= 2 ? split[1] : StringUtils.EMPTY);
+                row.set(columnName + "_domain", domain_part);
+            }
+        };
+    }
+
+    /**
+     * Update row metadata.
+     *
+     * @see ActionMetadata#createMetadataClosure(Map)
+     */
+    @Override
+    public Consumer<RowMetadata> createMetadataClosure(Map<String, String> parameters) {
+
+        return rowMetadata -> {
+
+            String columnId = parameters.get(COLUMN_ID);
+
+            List<ColumnMetadata> newColumns = new ArrayList<>(rowMetadata.size() + 1);
+
+            for (ColumnMetadata column : rowMetadata.getColumns()) {
+                ColumnMetadata newColumnMetadata = ColumnMetadata.Builder.column().copy(column).build();
+                newColumns.add(newColumnMetadata);
+
+                // append the split column
+                if (StringUtils.equals(columnId, column.getId())) {
+                    newColumnMetadata = ColumnMetadata.Builder //
+                            .column() //
+                            .computedId(column.getId() + "_local") //
+                            .name(column.getName() + "_local") //
+                            .type(Type.get(column.getType())) //
+                            .empty(column.getQuality().getEmpty()) //
+                            .invalid(column.getQuality().getInvalid()) //
+                            .valid(column.getQuality().getValid()) //
+                            .headerSize(column.getHeaderSize()) //
+                            .build();
+                    newColumns.add(newColumnMetadata);
+
+                    newColumnMetadata = ColumnMetadata.Builder //
+                            .column() //
+                            .computedId(column.getId() + "_domain") //
+                            .name(column.getName() + "_domain") //
+                            .type(Type.get(column.getType())) //
+                            .empty(column.getQuality().getEmpty()) //
+                            .invalid(column.getQuality().getInvalid()) //
+                            .valid(column.getQuality().getValid()) //
+                            .headerSize(column.getHeaderSize()) //
+                            .build();
+                    newColumns.add(newColumnMetadata);
+                }
+
+            }
+
+            // apply the new columns to the row metadata
+            rowMetadata.setColumns(newColumns);
+        };
+    }
+}
---
+++ dataprep-transformation/src/test/java/org/talend/dataprep/transformation/api/action/metadata/ExtractEmailDomainTest.java
@@ -0,0 +1,149 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2014 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.dataprep.transformation.api.action.metadata;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
+
+import org.apache.commons.io.IOUtils;
+import org.codehaus.jackson.JsonFactory;
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.talend.dataprep.api.dataset.ColumnMetadata;
+import org.talend.dataprep.api.dataset.DataSetRow;
+import org.talend.dataprep.api.dataset.RowMetadata;
+import org.talend.dataprep.api.type.Type;
+import org.talend.dataprep.transformation.Application;
+import org.talend.dataprep.transformation.TransformationServiceTests;
+
+/**
+ * Test class for Split action. Creates one consumer, and test it.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@IntegrationTest
+@WebAppConfiguration
+public class ExtractEmailDomainTest {
+
+    /** The row consumer to test. */
+    private Consumer<DataSetRow> rowClosure;
+
+    /** The metadata consumer to test. */
+    private Consumer<RowMetadata> metadataClosure;
+
+    /** The action to test. */
+    @Autowired
+    private ExtractEmailDomain action;
+
+    /**
+     * Initialization before each test.
+     */
+    @Before
+    public void setUp() throws IOException {
+        String actions = IOUtils.toString(TransformationServiceTests.class.getResourceAsStream("extractDomainAction.json"));
+        ObjectMapper mapper = new ObjectMapper(new JsonFactory());
+        String content = actions.trim();
+        JsonNode node = mapper.readTree(content);
+        Map<String, String> parameters = action.parseParameters(node.get("actions").get(0).get("parameters").getFields());
+        rowClosure = action.create(parameters);
+        metadataClosure = action.createMetadataClosure(parameters);
+    }
+
+    /**
+     * @see Split#create(Map)
+     */
+    @Test
+    public void test_values() {
+        Map<String, String> values = new HashMap<>();
+        values.put("recipe", "lorem bacon");
+        values.put("email", "david.bowie@yopmail.com");
+        values.put("last update", "01/01/2015");
+        DataSetRow row = new DataSetRow(values);
+
+        Map<String, String> expectedValues = new HashMap<>();
+        expectedValues.put("recipe", "lorem bacon");
+        expectedValues.put("email", "david.bowie@yopmail.com");
+        expectedValues.put("email_local", "david.bowie");
+        expectedValues.put("email_domain", "yopmail.com");
+        expectedValues.put("last update", "01/01/2015");
+
+        rowClosure.accept(row);
+        assertEquals(expectedValues, row.values());
+    }
+
+    @Test
+    public void test_values_invalid() {
+        Map<String, String> values = new HashMap<>();
+        values.put("recipe", "lorem bacon");
+        values.put("email", "david.bowie");
+        values.put("last update", "01/01/2015");
+        DataSetRow row = new DataSetRow(values);
+
+        Map<String, String> expectedValues = new HashMap<>();
+        expectedValues.put("recipe", "lorem bacon");
+        expectedValues.put("email", "david.bowie");
+        expectedValues.put("email_local", "");
+        expectedValues.put("email_domain", "");
+        expectedValues.put("last update", "01/01/2015");
+
+        rowClosure.accept(row);
+        assertEquals(expectedValues, row.values());
+    }
+
+    /**
+     * @see Split#createMetadataClosure(Map)
+     */
+    @Test
+    public void test_metadata() {
+        List<ColumnMetadata> input = new ArrayList<>();
+        input.add(createMetadata("recipe", "recipe"));
+        input.add(createMetadata("email", "email"));
+        input.add(createMetadata("last update", "last update"));
+        RowMetadata rowMetadata = new RowMetadata(input);
+
+        metadataClosure.accept(rowMetadata);
+        List<ColumnMetadata> actual = rowMetadata.getColumns();
+
+        List<ColumnMetadata> expected = new ArrayList<>();
+        expected.add(createMetadata("recipe", "recipe"));
+        expected.add(createMetadata("email", "email"));
+        expected.add(createMetadata("email_local", "email_local"));
+        expected.add(createMetadata("email_domain", "email_domain"));
+        expected.add(createMetadata("last update", "last update"));
+
+        assertEquals(expected, actual);
+    }
+
+    /**
+     * @param name name of the column metadata to create.
+     * @return a new column metadata
+     */
+    private ColumnMetadata createMetadata(String id, String name) {
+        return ColumnMetadata.Builder.column().computedId(id).name(name).type(Type.STRING).headerSize(12).empty(0).invalid(2)
+                .valid(5).build();
+    }
+}
