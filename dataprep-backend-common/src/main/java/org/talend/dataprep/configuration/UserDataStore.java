32af33c7769ea144e35661f5bb0da33879375c13
fhuaulme <fhuaulme@talend.com>
2015-06-17 14:49:41 +0200

    Renames UserDataStore::getStore() to avoid bean id clash. Adds @ConditionalOnProperty to automatically disable configuration is no "user.data.store" is found in properties. Clean up common application.properties. Add user.data.store to application.properties in API tests.

--- dataprep-backend-common/src/main/java/org/talend/dataprep/configuration/UserDataStore.java
+++ dataprep-backend-common/src/main/java/org/talend/dataprep/configuration/UserDataStore.java
@@ -3,6 +3,7 @@ package org.talend.dataprep.configuration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.talend.dataprep.store.UserDataRepository;
@@ -10,6 +11,7 @@ import org.talend.dataprep.store.local.InMemoryUserDataRepository;
 import org.talend.dataprep.store.mongo.MongoUserDateRepository;
 
 @Configuration
+@ConditionalOnProperty(name = "user.data.store")
 public class UserDataStore {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(UserDataStore.class);
@@ -18,7 +20,7 @@ public class UserDataStore {
     private String userDataStoreConfiguration;
 
     @Bean
-    public UserDataRepository getStore() {
+    public UserDataRepository getUserDataRepository() {
         LOGGER.info("User data store: {}", userDataStoreConfiguration); //$NON-NLS-1$
         switch (userDataStoreConfiguration) {
         case "mongodb": //$NON-NLS-1$
