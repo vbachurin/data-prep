666860e1f35b933a3042e3395aeccf4d0a461ce7
Marc Maillart <mmaillart@talend.com>
2015-06-19 09:49:04 +0200

    M5 - first set of decoration items including: --- previews --- preparation badges and list --- distribution barchart

--- dataprep-webapp/src/services/utils/constants/utils-constants.js
+++ dataprep-webapp/src/services/utils/constants/utils-constants.js
@@ -7,7 +7,9 @@
          * @name data-prep.services.utils.service:apiUrl
          * @description The REST api base url
          */
-        .constant('apiUrl', 'http://10.42.10.99:8888')
+        .constant('apiUrl', 'http://10.42.10.99:8888')  /* VM1 */
+        // .constant('apiUrl', 'http://10.42.40.91:8888')  /* VINCENT */
+        //.constant('apiUrl', 'http://192.168.40.134:8888')  /* MARC VM DOCKER  */
         /**
          * @ngdoc object
          * @name data-prep.services.utils.service:disableDebug
