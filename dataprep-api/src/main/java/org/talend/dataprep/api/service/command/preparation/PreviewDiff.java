d028adcd39f257bcf45fa47e4565089c057afa4d
Vincent Jaouën <vjaouen@talend.com>
2015-07-04 15:29:47 +0200

    TDP-280: a bit of refactoring to factorize code

--- dataprep-api/src/main/java/org/talend/dataprep/api/service/command/preparation/PreviewDiff.java
+++ dataprep-api/src/main/java/org/talend/dataprep/api/service/command/preparation/PreviewDiff.java
@@ -1,33 +1,24 @@
 package org.talend.dataprep.api.service.command.preparation;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.*;
 
-import org.apache.http.HttpEntity;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.ContentType;
-import org.apache.http.entity.mime.MultipartEntityBuilder;
-import org.apache.http.entity.mime.content.InputStreamBody;
-import org.apache.http.entity.mime.content.StringBody;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import org.talend.dataprep.api.preparation.Action;
 import org.talend.dataprep.api.preparation.Preparation;
-import org.talend.dataprep.api.service.APIService;
 import org.talend.dataprep.api.service.api.PreviewDiffInput;
-import org.talend.dataprep.api.service.command.ReleasableInputStream;
-import org.talend.dataprep.api.service.command.common.PreparationCommand;
 
 @Component
 @Scope("request")
-public class PreviewDiff extends PreparationCommand<InputStream> {
+public class PreviewDiff extends PreviewAbstract {
 
+    /** The all the preview parameters. */
     private final PreviewDiffInput input;
 
     public PreviewDiff(final HttpClient client, final PreviewDiffInput input) {
-        super(APIService.PREPARATION_GROUP, client);
+        super(client);
         this.input = input;
     }
 
@@ -63,30 +54,5 @@ public class PreviewDiff extends PreparationCommand<InputStream> {
         return previewTransformation(content, oldEncodedActions, newEncodedActions, encodedTdpIds);
     }
 
-    /**
-     * Call the transformation service to compute preview between old and new transformation
-     * 
-     * @param content - the dataset content
-     * @param oldEncodedActions - the old actions
-     * @param newEncodedActions - the preview actions
-     * @param encodedTdpIds - the TDP ids
-     * @throws java.io.IOException
-     */
-    private InputStream previewTransformation(final InputStream content, final String oldEncodedActions,
-            final String newEncodedActions, final String encodedTdpIds) throws IOException {
 
-        final String uri = this.transformationServiceUrl + "/transform/preview";
-        HttpPost transformationCall = new HttpPost(uri);
-
-        HttpEntity reqEntity = MultipartEntityBuilder.create()
-                .addPart("oldActions", new StringBody(oldEncodedActions, ContentType.TEXT_PLAIN.withCharset("UTF-8"))) //$NON-NLS-1$ //$NON-NLS-2$
-                .addPart("newActions", new StringBody(newEncodedActions, ContentType.TEXT_PLAIN.withCharset("UTF-8"))) //$NON-NLS-1$ //$NON-NLS-2$
-                .addPart("indexes", new StringBody(encodedTdpIds, ContentType.TEXT_PLAIN.withCharset("UTF-8"))) //$NON-NLS-1$ //$NON-NLS-2$
-                .addPart("content", new InputStreamBody(content, ContentType.APPLICATION_JSON)) //$NON-NLS-1$
-                .build();
-        transformationCall.setEntity(reqEntity);
-
-        return new ReleasableInputStream(client.execute(transformationCall).getEntity().getContent(),
-                transformationCall::releaseConnection);
-    }
 }
