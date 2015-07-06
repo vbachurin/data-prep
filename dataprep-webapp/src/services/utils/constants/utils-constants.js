4952bfa5e678e2b2b89fdb9295d93cd8f82befcd
mmaillart <mmaillart@talend.com>
2015-07-06 10:39:55 +0200

    Reset utils-constants.js (moved configuration values to mine.js).

--- dataprep-webapp/src/services/utils/constants/utils-constants.js
+++ dataprep-webapp/src/services/utils/constants/utils-constants.js
@@ -7,10 +7,7 @@
          * @name data-prep.services.utils.service:apiUrl
          * @description The REST api base url
          */
-        //.constant('apiUrl', 'http://10.42.10.99:8888')  /* VM1 */
-        //.constant('apiUrl', 'http://10.42.40.91:8888')  /* VINCENT */
-        //.constant('apiUrl', 'http://10.42.40.50:8888')  /* JIMMY */
-        .constant('apiUrl', 'http://192.168.40.134:8888')  /* MARC VM DOCKER  */
+        .constant('apiUrl', 'http://10.42.10.99:8888')
         /**
          * @ngdoc object
          * @name data-prep.services.utils.service:disableDebug
