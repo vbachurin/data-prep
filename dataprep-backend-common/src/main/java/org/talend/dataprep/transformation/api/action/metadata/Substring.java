98835eaca6926a65f29719bf047ee07e33cadd97
Stef Mallet <stephane.mallet@gmail.com>
2015-07-15 14:18:36 +0200

    TDP-160: support incremental ids

--- dataprep-backend-common/src/main/java/org/talend/dataprep/transformation/api/action/metadata/Substring.java
+++ dataprep-backend-common/src/main/java/org/talend/dataprep/transformation/api/action/metadata/Substring.java
@@ -2,7 +2,10 @@ package org.talend.dataprep.transformation.api.action.metadata;
 
 import static org.talend.dataprep.api.preparation.Action.Builder.builder;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Component;
@@ -16,6 +19,9 @@ public class Substring extends SingleColumnAction {
 
     /** The action name. */
     public static final String SUBSTRING_ACTION_NAME = "substring"; //$NON-NLS-1$
+
+    /** The column appendix. */
+    public static final String APPENDIX = "_substring"; //$NON-NLS-1$
 
     public static final String FROM_PARAMETER = "from"; //$NON-NLS-1$
 
@@ -52,17 +58,65 @@ public class Substring extends SingleColumnAction {
      */
     @Override
     public Action create(Map<String, String> parameters) {
-        String columnName = parameters.get(COLUMN_ID);
+        String columnId = parameters.get(COLUMN_ID);
         int fromIndex = Integer.parseInt(parameters.get(FROM_PARAMETER));
         int toIndex = Integer.parseInt(parameters.get(TO_PARAMETER));
 
         return builder().withRow((row, context) -> {
-            String value = row.get(columnName);
+            String value = row.get(columnId);
             if (value != null) {
                 String newValue = value.substring(fromIndex, toIndex);
-                row.set(columnName, newValue);
+
+                List<String> rowIds = row.values().keySet().stream().collect(Collectors.toList());
+                Integer nextSplitIndex = getNextAvailableSplitIndex(rowIds, columnId);
+                row.set(columnId + APPENDIX + "_" + nextSplitIndex, newValue);
             }
-        }).build();
+        }).withMetadata((rowMetadata, context) -> {
+                // go through the columns to be able to 'insert' the new columns just after the one needed.
+                for (int i = 0; i < rowMetadata.getColumns().size(); i++) {
+                    ColumnMetadata column = rowMetadata.getColumns().get(i);
+                    if (!StringUtils.equals(column.getId(), columnId)) {
+                        continue;
+                    }
+                        // get the new column id
+                        List<String> columnIds = new ArrayList<>(rowMetadata.size());
+                        rowMetadata.getColumns().forEach(columnMetadata -> columnIds.add(columnMetadata.getId()));
+
+                        Integer nextAvailableSplitIndex = getNextAvailableSplitIndex(columnIds, column.getId());
+                        if (nextAvailableSplitIndex == null) {
+                            // if this happen, let's not break anything
+                            break;
+                        }
+                        // create the new column
+                        ColumnMetadata newColumnMetadata = ColumnMetadata.Builder //
+                                .column() //
+                            .computedId(column.getId() + APPENDIX + '_' + nextAvailableSplitIndex) //
+                            .name(column.getName() + APPENDIX + '_' + nextAvailableSplitIndex) //
+                                .type(Type.get(column.getType())) //
+                                .empty(column.getQuality().getEmpty()) //
+                                .invalid(column.getQuality().getInvalid()) //
+                                .valid(column.getQuality().getValid()) //
+                                .headerSize(column.getHeaderSize()) //
+                                .build();
+                        // add the new column after the current one
+                    rowMetadata.getColumns().add(i + 1, newColumnMetadata);
+                }
+            }).build();
+    }
+
+    /**
+     * Copied from Split TODO refactor to reduce code duplication
+     */
+    private Integer getNextAvailableSplitIndex(List<String> columnsId, String columnId) {
+
+        for (int i = 1; i < 1000; i++) {
+            String temp = columnId + APPENDIX + '_' + i;
+            if (!columnsId.contains(temp)) {
+                return i;
+            }
+        }
+
+        return null;
     }
 
     /**
--- dataprep-backend-common/src/test/java/org/talend/dataprep/transformation/api/action/metadata/SubstringTest.java
+++ dataprep-backend-common/src/test/java/org/talend/dataprep/transformation/api/action/metadata/SubstringTest.java
@@ -81,11 +81,142 @@ public class SubstringTest {
 
         Map<String, String> expectedValues = new HashMap<>();
         expectedValues.put("recipe", "lorem bacon");
-        expectedValues.put("steps", " ipsum ");
+        expectedValues.put("steps", "Bacon ipsum dolor amet swine leberkas pork belly");
+        expectedValues.put("steps_substring_1", " ipsum ");
         expectedValues.put("last update", "01/01/2015");
 
         rowClosure.accept(row, new TransformationContext());
         assertEquals(expectedValues, row.values());
+    }
+
+    /**
+     * @throws IOException
+     * @see Split#create(Map)
+     */
+    @Test
+    public void should_substring_twice() throws IOException {
+        Map<String, String> values = new HashMap<>();
+        values.put("recipe", "lorem bacon");
+        values.put("steps", "Bacon ipsum dolor amet swine leberkas pork belly");
+        values.put("last update", "01/01/2015");
+        DataSetRow row = new DataSetRow(values);
+
+        Map<String, String> expectedValues = new HashMap<>();
+        expectedValues.put("recipe", "lorem bacon");
+        expectedValues.put("steps", "Bacon ipsum dolor amet swine leberkas pork belly");
+        expectedValues.put("steps_substring_2", "acon ");
+        expectedValues.put("steps_substring_1", " ipsum ");
+        expectedValues.put("last update", "01/01/2015");
+
+        rowClosure.accept(row, new TransformationContext());
+
+        // =====================================================
+        // Create a new rowClosure with different params:
+        // =====================================================
+        Map<String, String> parameters = ActionMetadataTestUtils.parseParameters( //
+                action, //
+                SubstringTest.class.getResourceAsStream("substringAction.json"));
+
+        parameters.put(Substring.FROM_PARAMETER, "1");
+        parameters.put(Substring.TO_PARAMETER, "6");
+
+        Action alternativeAction = this.action.create(parameters);
+        BiConsumer<DataSetRow, TransformationContext> alternativeRowClosure = alternativeAction.getRowAction();
+        // =====================================================
+
+        alternativeRowClosure.accept(row, new TransformationContext());
+        assertEquals(expectedValues, row.values());
+    }
+
+    /**
+     * @throws IOException
+     * @see Split#create(Map)
+     */
+    @Test
+    public void should_substring_the_new_substring() throws IOException {
+        Map<String, String> values = new HashMap<>();
+        values.put("recipe", "lorem bacon");
+        values.put("steps", "Bacon ipsum dolor amet swine leberkas pork belly");
+        values.put("last update", "01/01/2015");
+        DataSetRow row = new DataSetRow(values);
+
+        Map<String, String> expectedValues = new HashMap<>();
+        expectedValues.put("recipe", "lorem bacon");
+        expectedValues.put("steps", "Bacon ipsum dolor amet swine leberkas pork belly");
+        expectedValues.put("steps_substring_1", " ipsum ");
+        expectedValues.put("steps_substring_1_substring_1", "ips");
+        expectedValues.put("last update", "01/01/2015");
+
+        rowClosure.accept(row, new TransformationContext());
+
+        // =====================================================
+        // Create a new rowClosure with different params:
+        // =====================================================
+        Map<String, String> parameters = ActionMetadataTestUtils.parseParameters( //
+                action, //
+                SubstringTest.class.getResourceAsStream("substringAction.json"));
+
+        parameters.put(Substring.COLUMN_ID, "steps_substring_1");
+        parameters.put(Substring.FROM_PARAMETER, "1");
+        parameters.put(Substring.TO_PARAMETER, "4");
+
+        Action alternativeAction = this.action.create(parameters);
+        BiConsumer<DataSetRow, TransformationContext> alternativeRowClosure = alternativeAction.getRowAction();
+        // =====================================================
+
+        alternativeRowClosure.accept(row, new TransformationContext());
+        assertEquals(expectedValues, row.values());
+    }
+
+    /**
+     * @see Action#getMetadataAction()
+     */
+    @Test
+    public void should_update_metadata() {
+
+        List<ColumnMetadata> input = new ArrayList<>();
+        input.add(createMetadata("recipe", "recipe"));
+        input.add(createMetadata("steps", "steps"));
+        input.add(createMetadata("last update", "last update"));
+        RowMetadata rowMetadata = new RowMetadata(input);
+
+        metadataClosure.accept(rowMetadata, new TransformationContext());
+        List<ColumnMetadata> actual = rowMetadata.getColumns();
+
+        List<ColumnMetadata> expected = new ArrayList<>();
+        expected.add(createMetadata("recipe", "recipe"));
+        expected.add(createMetadata("steps", "steps"));
+        expected.add(createMetadata("steps_substring_1", "steps_substring_1"));
+        expected.add(createMetadata("last update", "last update"));
+
+        assertEquals(expected, actual);
+    }
+
+    /**
+     * @see Action#getMetadataAction()
+     */
+    @Test
+    public void should_update_metadata_twice() {
+
+        List<ColumnMetadata> input = new ArrayList<>();
+        input.add(createMetadata("recipe", "recipe"));
+        input.add(createMetadata("steps", "steps"));
+        input.add(createMetadata("last update", "last update"));
+        RowMetadata rowMetadata = new RowMetadata(input);
+
+        metadataClosure.accept(rowMetadata, new TransformationContext());
+        metadataClosure.accept(rowMetadata, new TransformationContext());
+
+        List<ColumnMetadata> actual = rowMetadata.getColumns();
+
+        List<ColumnMetadata> expected = new ArrayList<>();
+        expected.add(createMetadata("recipe", "recipe"));
+        expected.add(createMetadata("steps", "steps"));
+        expected.add(createMetadata("steps_substring_2", "steps_substring_2"));
+        expected.add(createMetadata("steps_substring_1", "steps_substring_1"));
+        expected.add(createMetadata("last update", "last update"));
+
+        assertEquals(expected, actual);
     }
 
     @Test
