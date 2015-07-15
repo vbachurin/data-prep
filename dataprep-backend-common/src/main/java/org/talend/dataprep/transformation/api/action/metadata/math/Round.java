e3a848ee06a0a061242a9d84764dd105090aff45
Stef Mallet <stephane.mallet@gmail.com>
2015-07-15 10:26:43 +0200

    Sonar: remove duplication in Floor, Ceil & Round

--- dataprep-backend-common/src/main/java/org/talend/dataprep/transformation/api/action/metadata/math/Round.java
+++ dataprep-backend-common/src/main/java/org/talend/dataprep/transformation/api/action/metadata/math/Round.java
@@ -12,23 +12,14 @@
 // ============================================================================
 package org.talend.dataprep.transformation.api.action.metadata.math;
 
-import static org.talend.dataprep.api.preparation.Action.Builder.builder;
-
-import java.util.Map;
-
 import org.springframework.stereotype.Component;
-import org.talend.dataprep.api.dataset.ColumnMetadata;
-import org.talend.dataprep.api.preparation.Action;
-import org.talend.dataprep.api.type.Type;
-import org.talend.dataprep.transformation.api.action.metadata.ActionCategory;
 import org.talend.dataprep.transformation.api.action.metadata.ActionMetadata;
-import org.talend.dataprep.transformation.api.action.metadata.SingleColumnAction;
 
 /**
  * This will compute the absolute value for numerical columns.
  */
 @Component(Round.ACTION_BEAN_PREFIX + Round.ROUND_ACTION_NAME)
-public class Round extends SingleColumnAction {
+public class Round extends AbstractRound {
 
     /** The action name. */
     public static final String ROUND_ACTION_NAME = "round"; //$NON-NLS-1$
@@ -41,48 +32,7 @@ public class Round extends SingleColumnAction {
         return ROUND_ACTION_NAME;
     }
 
-    /**
-     * @see ActionMetadata#getCategory()
-     */
-    @Override
-    public String getCategory() {
-        return ActionCategory.MATH.getDisplayName();
-    }
-
-    /**
-     * @see ActionMetadata#create(Map)
-     */
-    @Override
-    public Action create(Map<String, String> parameters) {
-        return builder().withRow((row, context) -> {
-            String columnName = parameters.get(COLUMN_ID);
-            String value = row.get(columnName);
-            if (value == null) {
-                return;
-            }
-
-            String absValueStr = null;
-            try {
-                double doubleValue = Double.parseDouble(value);
-                long roundedValue = Math.round(doubleValue);
-                absValueStr = String.format("%s", roundedValue); //$NON-NLS-1$
-            } catch (NumberFormatException nfe2) {
-                // Nan: nothing to do, but fail silently (no change in value)
-            }
-
-            if (absValueStr != null) {
-                row.set(columnName, absValueStr);
-            }
-        }).build();
-    }
-
-    /**
-     * @see ActionMetadata#accept(ColumnMetadata)
-     */
-    @Override
-    public boolean accept(ColumnMetadata column) {
-        Type columnType = Type.get(column.getType());
-        // in order to 'clean' integer typed columns, this function needs to be allowed on any numeric types
-        return Type.NUMERIC.isAssignableFrom(columnType);
+    protected int compute(double from) {
+        return (int) Math.round(from);
     }
 }
