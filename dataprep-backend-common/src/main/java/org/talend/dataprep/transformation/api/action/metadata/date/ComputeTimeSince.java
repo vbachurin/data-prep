7db1be657051cbc9828c85376bfa41081f81f2b7
Stef Mallet <stephane.mallet@gmail.com>
2015-07-16 14:20:12 +0200

    TDP-168: first step

---
+++ dataprep-backend-common/src/main/java/org/talend/dataprep/transformation/api/action/metadata/date/ComputeTimeSince.java
@@ -0,0 +1,174 @@
+package org.talend.dataprep.transformation.api.action.metadata.date;
+
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.talend.dataprep.api.dataset.ColumnMetadata;
+import org.talend.dataprep.api.preparation.Action;
+import org.talend.dataprep.api.type.Type;
+import org.talend.dataprep.exception.CommonErrorCodes;
+import org.talend.dataprep.exception.TDPException;
+import org.talend.dataprep.transformation.api.action.metadata.ActionCategory;
+import org.talend.dataprep.transformation.api.action.metadata.ActionMetadata;
+import org.talend.dataprep.transformation.api.action.metadata.SingleColumnAction;
+
+import java.io.IOException;
+import java.text.ParsePosition;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.time.temporal.TemporalAccessor;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static org.talend.dataprep.api.preparation.Action.Builder.builder;
+
+@Component(ComputeTimeSince.ACTION_BEAN_PREFIX + ComputeTimeSince.TIME_SINCE_ACTION_NAME)
+public class ComputeTimeSince extends SingleColumnAction {
+
+    /**
+     * The action name.
+     */
+    public static final String TIME_SINCE_ACTION_NAME = "compute_time_since"; //$NON-NLS-1$
+
+    /**
+     * The column appendix.
+     */
+    public static final String APPENDIX = "_time"; //$NON-NLS-1$
+
+    /**
+     * Name of the date pattern.
+     */
+    protected static final String PATTERN = "date_pattern"; //$NON-NLS-1$
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ComputeTimeSince.class);
+
+    /**
+     * @see ActionMetadata#getName()
+     */
+    @Override
+    public String getName() {
+        return TIME_SINCE_ACTION_NAME;
+    }
+
+    /**
+     * @see ActionMetadata#getCategory()
+     */
+    @Override
+    public String getCategory() {
+        return ActionCategory.DATE.getDisplayName();
+    }
+
+    /**
+     * @see ActionMetadata#create(Map)
+     */
+    @Override
+    public Action create(Map<String, String> parameters) {
+
+        return builder().withRow((row, context) -> {
+            String columnId = getColumnIdParameter(parameters);
+            // sadly unable to do that outside of the closure since the context is not available
+            DateTimeFormatter dtf = DateTimeFormatter.ofPattern((String) context.get(PATTERN));
+
+            String value = row.get(columnId);
+
+            // parse the date
+            TemporalAccessor temporalAccessor = null;
+            try {
+                temporalAccessor = dtf.parse(value, new ParsePosition(0));
+            } catch (DateTimeParseException e) {
+                // Nothing to do: in this case, temporalAccessor is left null
+                LOGGER.debug("Unable to parse date {}.", value, e);
+            }
+
+            LocalDate now = LocalDate.now();
+            LocalDate valueAsDate = LocalDate.from(temporalAccessor);
+
+            Period period = Period.between(valueAsDate, now);
+
+            row.set(columnId + APPENDIX, period.getYears() + "");
+
+        }).withMetadata((rowMetadata, context) -> {
+
+            String columnId = parameters.get(COLUMN_ID);
+
+            JsonFactory jsonFactory = new JsonFactory();
+            ObjectMapper mapper = new ObjectMapper(jsonFactory);
+
+            List<String> columnIds = new ArrayList<>(rowMetadata.size());
+            rowMetadata.getColumns().forEach(columnMetadata -> columnIds.add(columnMetadata.getId()));
+            if (!columnIds.contains(columnId + APPENDIX)) {
+
+                // go through the columns to be able to 'insert' the new columns just after the one needed.
+                for (int i = 0; i < rowMetadata.getColumns().size(); i++) {
+                    ColumnMetadata column = rowMetadata.getColumns().get(i);
+                    if (!StringUtils.equals(column.getId(), columnId)) {
+                        continue;
+                    }
+
+                    // store the current pattern in the context
+                    JsonNode rootNode = getStatisticsNode(mapper, column);
+
+                    JsonNode mostUsedPatternNode = rootNode.get("patternFrequencyTable").get(0); //$NON-NLS-1$
+                    String datePattern = mostUsedPatternNode.get("pattern").asText(); //$NON-NLS-1$
+                    context.put(PATTERN, datePattern);
+
+                    // create the new column
+                    ColumnMetadata newColumnMetadata = ColumnMetadata.Builder //
+                            .column() //
+                            .computedId(column.getId() + APPENDIX) //
+                            .name(column.getName() + APPENDIX) //
+                            .type(Type.INTEGER) //
+                            .empty(column.getQuality().getEmpty()) //
+                            .invalid(column.getQuality().getInvalid()) //
+                            .valid(column.getQuality().getValid()) //
+                            .headerSize(column.getHeaderSize()) //
+                            .build();
+                    // add the new column after the current one
+                    rowMetadata.getColumns().add(i + 1, newColumnMetadata);
+                }
+            }
+        }).build();
+    }
+
+    /**
+     * Return the json statistics node.
+     *
+     * @param mapper jackson object mapper.
+     * @param column the column metadata to work on.
+     * @return the json statistics node.
+     */
+    private JsonNode getStatisticsNode(ObjectMapper mapper, ColumnMetadata column) {
+        try {
+            return mapper.readTree(column.getStatistics());
+        } catch (IOException e) {
+            throw new TDPException(CommonErrorCodes.UNABLE_TO_PARSE_JSON, e);
+        }
+    }
+
+    /**
+     * @param parameters the parameters.
+     * @return the column id parameter.
+     */
+    private String getColumnIdParameter(Map<String, String> parameters) {
+        String columnId = parameters.get(COLUMN_ID);
+        if (columnId == null) {
+            throw new IllegalArgumentException("Parameter '" + COLUMN_ID + "' is required for this action");
+        }
+        return columnId;
+    }
+
+    /**
+     * @see ActionMetadata#accept(ColumnMetadata)
+     */
+    @Override
+    public boolean accept(ColumnMetadata column) {
+        return Type.DATE.equals(Type.get(column.getType()));
+    }
+}
---
+++ dataprep-backend-common/src/test/java/org/talend/dataprep/transformation/api/action/metadata/date/ComputeTimeSinceTest.java
@@ -0,0 +1,150 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2014 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.dataprep.transformation.api.action.metadata.date;
+
+import org.junit.Test;
+import org.talend.dataprep.api.dataset.ColumnMetadata;
+import org.talend.dataprep.api.dataset.DataSetRow;
+import org.talend.dataprep.api.dataset.RowMetadata;
+import org.talend.dataprep.api.preparation.Action;
+import org.talend.dataprep.api.type.Type;
+import org.talend.dataprep.transformation.api.action.context.TransformationContext;
+import org.talend.dataprep.transformation.api.action.metadata.ActionMetadataTestUtils;
+import org.talend.dataprep.transformation.api.action.metadata.ComputeLength;
+import org.talend.dataprep.transformation.api.action.metadata.Split;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.BiConsumer;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+import static org.talend.dataprep.api.dataset.ColumnMetadata.Builder.column;
+import static org.talend.dataprep.transformation.api.action.metadata.ActionMetadataTestUtils.getColumn;
+
+/**
+ * Test class for Split action. Creates one consumer, and test it.
+ *
+ * @see Split
+ */
+public class ComputeTimeSinceTest {
+
+    /**
+     * The row consumer to test.
+     */
+    private BiConsumer<DataSetRow, TransformationContext> rowClosure;
+
+    /**
+     * The metadata consumer to test.
+     */
+    private BiConsumer<RowMetadata, TransformationContext> metadataClosure;
+
+    /**
+     * The action to test.
+     */
+    private ComputeTimeSince action;
+
+    /**
+     * Constructor.
+     */
+    public ComputeTimeSinceTest() throws IOException {
+        action = new ComputeTimeSince();
+        Map<String, String> parameters = ActionMetadataTestUtils.parseParameters( //
+                action, //
+                ComputeTimeSinceTest.class.getResourceAsStream("computeTimeSinceAction.json"));
+        final Action action = this.action.create(parameters);
+        rowClosure = action.getRowAction();
+        metadataClosure = action.getMetadataAction();
+    }
+
+    @Test
+    public void testAdapt() throws Exception {
+        assertThat(action.adapt(null), is(action));
+        ColumnMetadata column = column().name("myColumn").id(0).type(Type.STRING).build();
+        assertThat(action.adapt(column), is(action));
+    }
+
+    /**
+     * @see Split#create(Map)
+     */
+    @Test
+    public void should_compute() {
+        Map<String, String> values = new HashMap<>();
+        values.put("recipe", "lorem bacon");
+        values.put("last update", "01/01/2010");
+        values.put("steps", "Bacon");
+        DataSetRow row = new DataSetRow(values);
+
+        Map<String, String> expectedValues = new HashMap<>();
+        expectedValues.put("recipe", "lorem bacon");
+        expectedValues.put("last update", "01/01/2010");
+        expectedValues.put("last update_time", "5");
+        expectedValues.put("steps", "Bacon");
+
+        TransformationContext context = new TransformationContext();
+        context.put(ExtractDateTokens.PATTERN, "MM/dd/yyyy");
+
+        rowClosure.accept(row, context);
+        assertEquals(expectedValues, row.values());
+    }
+
+    /**
+     * @see Action#getMetadataAction()
+     */
+    @Test
+    public void should_update_metadata() {
+
+        List<ColumnMetadata> input = new ArrayList<>();
+        input.add(createMetadata("recipe", "recipe"));
+        input.add(createMetadata("steps", "steps"));
+        input.add(createMetadata("last update", "last update"));
+        RowMetadata rowMetadata = new RowMetadata(input);
+
+        metadataClosure.accept(rowMetadata, new TransformationContext());
+        List<ColumnMetadata> actual = rowMetadata.getColumns();
+
+        List<ColumnMetadata> expected = new ArrayList<>();
+        expected.add(createMetadata("recipe", "recipe"));
+        expected.add(createMetadata("steps", "steps"));
+        expected.add(createMetadata("steps_length", "steps_length", Type.INTEGER));
+        expected.add(createMetadata("last update", "last update"));
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void should_accept_column() {
+        assertTrue(action.accept(getColumn(Type.DATE)));
+    }
+
+    @Test
+    public void should_not_accept_column() {
+        assertFalse(action.accept(getColumn(Type.NUMERIC)));
+        assertFalse(action.accept(getColumn(Type.FLOAT)));
+        assertFalse(action.accept(getColumn(Type.STRING)));
+        assertFalse(action.accept(getColumn(Type.BOOLEAN)));
+    }
+
+    private ColumnMetadata createMetadata(String id, String name) {
+        return createMetadata(id, name, Type.STRING);
+    }
+
+    private ColumnMetadata createMetadata(String id, String name, Type type) {
+        return ColumnMetadata.Builder.column().computedId(id).name(name).type(type).headerSize(12).empty(0).invalid(2)
+                .valid(5).build();
+    }
+
+}
