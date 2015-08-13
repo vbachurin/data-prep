d0da37651948ace89217781ead1469d1ae0d1c86
Olivier Lamy <olamy@apache.org>
2015-08-13 22:22:18 +1000

    [TDP-378] fix unit tests

--- dataprep-backend-common/src/test/java/org/talend/dataprep/transformation/api/action/metadata/fillempty/FillEmptyTest.java
+++ dataprep-backend-common/src/test/java/org/talend/dataprep/transformation/api/action/metadata/fillempty/FillEmptyTest.java
@@ -1,6 +1,5 @@
 package org.talend.dataprep.transformation.api.action.metadata.fillempty;
 
-
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -14,46 +13,40 @@ import org.talend.dataprep.api.type.Type;
 import org.talend.dataprep.transformation.api.action.context.TransformationContext;
 import org.talend.dataprep.transformation.api.action.metadata.ActionMetadataTestUtils;
 
-import com.google.common.collect.Sets;
-
-public class FillEmptyTest
-{
+public class FillEmptyTest {
 
     @Test
-    public void should_fill_non_valid_integer() throws Exception {
+    public void should_fill_empty_integer() throws Exception {
         // given
         final Map<String, String> values = new HashMap<>();
         values.put("0001", "David Bowie");
         values.put("0002", "");
-        values.put( "0003", "Something" );
+        values.put("0003", "Something");
 
         final RowMetadata rowMetadata = new RowMetadata();
-        rowMetadata.setColumns( Arrays.asList( ColumnMetadata.Builder.column() //
-                                                   .type( Type.INTEGER ) //
-                                                   .computedId( "0002" ) //
-                                                   .build() ) );
+        rowMetadata.setColumns(Arrays.asList(ColumnMetadata.Builder.column() //
+                .type( Type.INTEGER ) //
+                .computedId( "0002" ) //
+                .build()));
 
         final DataSetRow row = new DataSetRow(values);
         row.setRowMetadata(rowMetadata);
 
+        FillWithIntegerIfEmpty fillWithIntegerIfEmpty = new FillWithIntegerIfEmpty();
 
-        FillWithIntegerIfEmpty fillWithIntegerIfEmpty  = new FillWithIntegerIfEmpty();
-
-        Map<String, String>  parameters = ActionMetadataTestUtils.parseParameters( fillWithIntegerIfEmpty, //
-                                                                                   FillEmptyTest.class.getResourceAsStream(
-                                                                                       "fillInvalidIntegerAction.json" ) );
-
+        Map<String, String> parameters = ActionMetadataTestUtils.parseParameters(fillWithIntegerIfEmpty, //
+                FillEmptyTest.class.getResourceAsStream("fillEmptyIntegerAction.json"));
 
         // when
         fillWithIntegerIfEmpty.applyOnColumn(row, new TransformationContext(), parameters, "0002");
 
         // then
-        Assert.assertEquals( "25", row.get( "0002" ) );
-        Assert.assertEquals( "David Bowie", row.get( "0001" ) );
+        Assert.assertEquals("25", row.get("0002"));
+        Assert.assertEquals("David Bowie", row.get("0001"));
     }
 
     @Test
-    public void should_fill_non_valid_string() throws Exception{
+    public void should_fill_empty_string() throws Exception {
         // given
         final Map<String, String> values = new HashMap<>();
         values.put("0001", "David Bowie");
@@ -61,29 +54,29 @@ public class FillEmptyTest
         values.put("0003", "100");
 
         final RowMetadata rowMetadata = new RowMetadata();
-        rowMetadata.setColumns( Arrays.asList( ColumnMetadata.Builder.column() //
-                                                   .type( Type.STRING ) //
-                                                   .computedId( "0003" ) //
-                                                   .build() ) );
+        rowMetadata.setColumns(Arrays.asList(ColumnMetadata.Builder.column() //
+                .type( Type.STRING ) //
+                .computedId( "0002" ) //
+                .build()));
 
         final DataSetRow row = new DataSetRow(values);
-        row.setRowMetadata( rowMetadata );
+        row.setRowMetadata(rowMetadata);
 
-        FillWithStringIfEmpty fillWithStringIfInvalid  = new FillWithStringIfInvalid();
+        FillWithStringIfEmpty fillWithStringIfInvalid = new FillWithStringIfEmpty();
 
-        Map<String, String>  parameters =  ActionMetadataTestUtils.parseParameters(fillWithStringIfInvalid, //
-                                                                                   FillInvalidTest.class.getResourceAsStream("fillInvalidStringAction.json"));
+        Map<String, String> parameters = ActionMetadataTestUtils.parseParameters(fillWithStringIfInvalid, //
+                FillEmptyTest.class.getResourceAsStream("fillEmptyStringAction.json"));
 
         // when
-        fillWithStringIfInvalid.applyOnColumn( row, new TransformationContext(), parameters, "0003" );
+        fillWithStringIfInvalid.applyOnColumn(row, new TransformationContext(), parameters, "0002");
 
         // then
-        Assert.assertEquals( "beer", row.get( "0003" ) );
-        Assert.assertEquals( "David Bowie", row.get( "0001" ) );
+        Assert.assertEquals("beer", row.get("0002"));
+        Assert.assertEquals("David Bowie", row.get("0001"));
     }
 
     @Test
-    public void should_fill_non_valid_boolean() throws Exception{
+    public void should_fill_empty_boolean() throws Exception {
         // given
         final Map<String, String> values = new HashMap<>();
         values.put("0001", "David Bowie");
@@ -91,25 +84,25 @@ public class FillEmptyTest
         values.put("0003", "100");
 
         final RowMetadata rowMetadata = new RowMetadata();
-        rowMetadata.setColumns( Arrays.asList( ColumnMetadata.Builder.column() //
-                                                   .type( Type.BOOLEAN ) //
-                                                   .computedId( "0003" ) //
-                                                   .build() ) );
+        rowMetadata.setColumns(Arrays.asList(ColumnMetadata.Builder.column() //
+                .type( Type.BOOLEAN ) //
+                .computedId( "0002" ) //
+                .build()));
 
         final DataSetRow row = new DataSetRow(values);
-        row.setRowMetadata( rowMetadata );
+        row.setRowMetadata(rowMetadata);
 
-        FillWithStringIfInvalid fillWithStringIfInvalid  = new FillWithStringIfInvalid();
+        FillWithBooleanIfEmpty fillWithBooleanIfEmpty = new FillWithBooleanIfEmpty();
 
-        Map<String, String>  parameters =  ActionMetadataTestUtils.parseParameters(fillWithStringIfInvalid, //
-                                                                                   FillInvalidTest.class.getResourceAsStream("fillInvalidBooleanAction.json"));
+        Map<String, String> parameters = ActionMetadataTestUtils.parseParameters(fillWithBooleanIfEmpty, //
+                FillEmptyTest.class.getResourceAsStream("fillEmptyBooleanAction.json"));
 
         // when
-        fillWithStringIfInvalid.applyOnColumn( row, new TransformationContext(), parameters, "0003" );
+        fillWithBooleanIfEmpty.applyOnColumn(row, new TransformationContext(), parameters, "0002");
 
         // then
-        Assert.assertEquals( "True", row.get( "0003" ) );
-        Assert.assertEquals( "David Bowie", row.get( "0001" ) );
+        Assert.assertEquals("True", row.get("0002"));
+        Assert.assertEquals("David Bowie", row.get("0001"));
     }
 
 }
