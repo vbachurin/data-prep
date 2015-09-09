8d17e1d896d3849d353088102645c19308fc9b88
Stef Mallet <stephane.mallet@gmail.com>
2015-09-09 18:30:40 +0200

    TDP-492: first implementation of new action TimestampToDate

---
+++ dataprep-backend-common/src/main/java/org/talend/dataprep/transformation/api/action/metadata/date/TimestampToDate.java
@@ -0,0 +1,96 @@
+package org.talend.dataprep.transformation.api.action.metadata.date;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.Map;
+
+import org.springframework.stereotype.Component;
+import org.talend.dataprep.api.dataset.ColumnMetadata;
+import org.talend.dataprep.api.dataset.DataSetRow;
+import org.talend.dataprep.api.dataset.RowMetadata;
+import org.talend.dataprep.api.type.Type;
+import org.talend.dataprep.transformation.api.action.context.TransformationContext;
+import org.talend.dataprep.transformation.api.action.metadata.category.ActionCategory;
+import org.talend.dataprep.transformation.api.action.metadata.common.AbstractActionMetadata;
+import org.talend.dataprep.transformation.api.action.metadata.common.ActionMetadata;
+import org.talend.dataprep.transformation.api.action.metadata.common.ColumnAction;
+
+@Component(TimestampToDate.ACTION_BEAN_PREFIX + TimestampToDate.ACTION_NAME)
+public class TimestampToDate extends AbstractActionMetadata implements ColumnAction {
+
+    /**
+     * The action name.
+     */
+    public static final String ACTION_NAME = "timestamp_to_date"; //$NON-NLS-1$
+
+    /**
+     * The column appendix.
+     */
+    public static final String APPENDIX = "_as_date"; //$NON-NLS-1$
+
+    /**
+     * @see ActionMetadata#getName()
+     */
+    @Override
+    public String getName() {
+        return ACTION_NAME;
+    }
+
+    /**
+     * @see ActionMetadata#acceptColumn(ColumnMetadata)
+     */
+    @Override
+    public boolean acceptColumn(ColumnMetadata column) {
+        return Type.INTEGER.equals(Type.get(column.getType()));
+    }
+
+    /**
+     * @see ActionMetadata#getCategory()
+     */
+    @Override
+    public String getCategory() {
+        return ActionCategory.DATE.getDisplayName();
+    }
+
+    /**
+     * @see ColumnAction#applyOnColumn(DataSetRow, TransformationContext, Map, String)
+     */
+    @Override
+    public void applyOnColumn(DataSetRow row, TransformationContext context, Map<String, String> parameters, String columnId) {
+        // create new column and append it after current column
+        final RowMetadata rowMetadata = row.getRowMetadata();
+        final ColumnMetadata column = rowMetadata.getById(columnId);
+        final String newColumn = rowMetadata.insertAfter(columnId, createNewColumn(column));
+
+        final String value = row.get(columnId);
+        row.set(newColumn, apply(value));
+    }
+
+    protected String apply(String from) {
+        try {
+            LocalDateTime date = LocalDateTime.ofEpochSecond(Long.parseLong(from), 0, ZoneOffset.UTC);
+            final String to = DateTimeFormatter.ISO_LOCAL_DATE.format(date);
+            return to;
+        } catch (NumberFormatException e) {
+            return "";
+        }
+    }
+
+    /**
+     * Create the new "string length" column
+     *
+     * @param column the current column metadata
+     * @return the new column metadata
+     */
+    private ColumnMetadata createNewColumn(final ColumnMetadata column) {
+        return ColumnMetadata.Builder //
+                .column() //
+                .name(column.getName() + APPENDIX) //
+                .type(Type.DATE) //
+                .headerSize(column.getHeaderSize()) //
+                .build();
+    }
+}
