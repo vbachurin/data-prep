280fae014084680ff7b0809a940aa40bb75a3285
Olivier Lamy <olamy@apache.org>
2015-09-09 14:21:05 +1000

    [TDP-442] Fill Empty if Date action add backend classes.

---
+++ dataprep-backend-common/src/test/java/org/talend/dataprep/transformation/api/action/metadata/fillempty/FillWithDateIfEmptyTest.java
@@ -0,0 +1,81 @@
+package org.talend.dataprep.transformation.api.action.metadata.fillempty;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.talend.dataprep.transformation.api.action.metadata.ActionMetadataTestUtils.getColumn;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.talend.dataprep.api.dataset.ColumnMetadata;
+import org.talend.dataprep.api.dataset.DataSetRow;
+import org.talend.dataprep.api.dataset.RowMetadata;
+import org.talend.dataprep.api.type.Type;
+import org.talend.dataprep.transformation.api.action.context.TransformationContext;
+import org.talend.dataprep.transformation.api.action.metadata.ActionMetadataTestUtils;
+
+/**
+ * Unit test for the FillWithStringIfEmpty action.
+ *
+ * @see FillWithStringIfEmpty
+ */
+public class FillWithDateIfEmptyTest {
+
+    /** The action to test. */
+    private FillWithDateIfEmpty action;
+
+    /**
+     * Default empty constructor.
+     */
+    public FillWithDateIfEmptyTest() {
+        action = new FillWithDateIfEmpty();
+    }
+
+    @Test
+    public void should_fill_empty_date() throws Exception {
+        // given
+        final Map<String, String> values = new HashMap<>();
+        values.put("0001", "David Bowie");
+        values.put("0002", "");
+        values.put("0003", "100");
+
+        final RowMetadata rowMetadata = new RowMetadata();
+        rowMetadata.setColumns(Arrays.asList(ColumnMetadata.Builder.column() //
+                .type(Type.DATE) //
+                .computedId("0002") //
+                .build()));
+
+        final DataSetRow row = new DataSetRow(values);
+        row.setRowMetadata(rowMetadata);
+
+        Map<String, String> parameters = ActionMetadataTestUtils.parseParameters(action, //
+                this.getClass().getResourceAsStream("fillEmptyDateAction.json"));
+
+        // when
+        action.applyOnColumn(row, new TransformationContext(), parameters, "0002");
+
+        // then
+        Assert.assertEquals("01/01/1970 10:00:00", row.get("0002"));
+        Assert.assertEquals("David Bowie", row.get("0001"));
+    }
+
+    @Test
+    public void should_accept_column() {
+        assertTrue(action.acceptColumn(getColumn(Type.DATE)));
+    }
+
+    @Test
+    public void should_not_accept_column() {
+        assertFalse(action.acceptColumn(getColumn(Type.STRING)));
+        assertFalse(action.acceptColumn(getColumn(Type.NUMERIC)));
+        assertFalse(action.acceptColumn(getColumn(Type.DOUBLE)));
+        assertFalse(action.acceptColumn(getColumn(Type.FLOAT)));
+        assertFalse(action.acceptColumn(getColumn(Type.INTEGER)));
+        assertFalse(action.acceptColumn(getColumn(Type.BOOLEAN)));
+        assertFalse(action.acceptColumn(getColumn(Type.ANY)));
+    }
+
+}
