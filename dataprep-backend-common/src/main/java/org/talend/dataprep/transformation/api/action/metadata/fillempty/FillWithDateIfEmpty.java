2f313c5df673d89ad22cc48266f278e4e21de064
fhuaulme <fhuaulme@talend.com>
2015-09-10 11:20:52 +0200

    TDP-442: Update Javadoc with warning for back end developers.

--- dataprep-backend-common/src/main/java/org/talend/dataprep/transformation/api/action/metadata/fillempty/FillWithDateIfEmpty.java
+++ dataprep-backend-common/src/main/java/org/talend/dataprep/transformation/api/action/metadata/fillempty/FillWithDateIfEmpty.java
@@ -46,7 +46,7 @@ public class FillWithDateIfEmpty extends AbstractFillIfEmpty {
     }
 
     /**
-     *
+     * Note: this method is used for JSON serialization and used in web UI, please consider this when making changes.
      * @return <code>true</code> if this action is date related so the datetimepicker can be display
      */
     public boolean isDate() {
