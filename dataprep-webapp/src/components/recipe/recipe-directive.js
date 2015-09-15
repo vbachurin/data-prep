a33a93823bce6038eb186b954ad2e1435d16f4df
jsomsanith <jsomsanith@talend.com>
2015-09-15 15:12:46 +0200

    TDP-363 : highlight steps to remove in recipe

--- dataprep-webapp/src/components/recipe/recipe-directive.js
+++ dataprep-webapp/src/components/recipe/recipe-directive.js
@@ -14,7 +14,50 @@
             restrict: 'E',
             templateUrl: 'components/recipe/recipe.html',
             controllerAs: 'recipeCtrl',
-            controller: 'RecipeCtrl'
+            controller: 'RecipeCtrl',
+            link: function(scope, iElement, iAttrs, ctrl) {
+                function attachDeleteMouseOver(recipe) {
+                    _.forEach(recipe, function(step) {
+                        var stepId = step.transformation.stepId;
+                        var hasDiff = step.diff && step.diff.createdColumns && step.diff.createdColumns.length;
+                        var createdColumns = step.diff.createdColumns;
+
+                        function shouldBeRemoved(stepToTest) {
+                            /*jshint camelcase: false */
+                            // current step
+                            return stepToTest.transformation.stepId === stepId ||
+                                    //step on a column that will be removed
+                                    (hasDiff && stepToTest.actionParameters &&
+                                        createdColumns.indexOf(stepToTest.actionParameters.parameters.column_id) > -1);
+                        }
+
+                        var stepsToRemove = _.chain(recipe)
+                            .filter(shouldBeRemoved)
+                            .map(function(step) {
+                                return iElement.find('#step-' + step.transformation.stepId);
+                            })
+                            .value();
+
+                        var removeElement = iElement.find('#step-' + stepId).find('#step-remove-' + stepId);
+                        removeElement.on('mouseover', function() {
+                            _.forEach(stepsToRemove, function(stepElement) {
+                                stepElement.addClass('remove');
+                            });
+                        });
+                        removeElement.on('mouseout', function() {
+                            _.forEach(stepsToRemove, function(stepElement) {
+                                stepElement.removeClass('remove');
+                            });
+                        });
+                    });
+                }
+
+                scope.$watch(function() {
+                    return ctrl.recipe;
+                }, function(recipe) {
+                    setTimeout(attachDeleteMouseOver.bind(null, recipe), 0);
+                });
+            }
         };
     }
 
