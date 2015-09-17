807e14ba20678496dce23db9116b680d9b8045b8
fhuaulme <fhuaulme@talend.com>
2015-09-17 14:45:05 +0200

    Migrate transformation commands to new API.

--- dataprep-api/src/main/java/org/talend/dataprep/api/service/command/transformation/SuggestActionParams.java
+++ dataprep-api/src/main/java/org/talend/dataprep/api/service/command/transformation/SuggestActionParams.java
@@ -1,15 +1,15 @@
 package org.talend.dataprep.api.service.command.transformation;
 
+import static org.talend.dataprep.api.service.command.common.GenericCommand.Defaults.pipeStream;
+
 import java.io.InputStream;
 
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.InputStreamEntity;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Scope;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
-import org.talend.dataprep.api.service.command.ReleasableInputStream;
 import org.talend.dataprep.api.service.command.common.ChainedCommand;
 
 import com.netflix.hystrix.HystrixCommand;
@@ -18,40 +18,17 @@ import com.netflix.hystrix.HystrixCommand;
 @Scope("request")
 public class SuggestActionParams extends ChainedCommand<InputStream, InputStream> {
 
-    /**
-     * This class' logger.
-     */
-    private static final Logger LOG = LoggerFactory.getLogger(SuggestActionParams.class);
-
-    /**
-     * The target action for this parameters getter operation
-     */
-    private final String action;
-
-    private String columnId;
-
     private SuggestActionParams(final HttpClient client, final HystrixCommand<InputStream> content, final String action,
             final String columnId) {
         super(client, content);
-        this.action = action;
-        this.columnId = columnId;
-    }
-
-    @Override
-    protected InputStream run() throws Exception {
-        try {
+        execute(() -> {
             final String uri = transformationServiceUrl + "/transform/suggest/" + action + "/params?columnId=" + columnId;
-            final InputStreamEntity content = new InputStreamEntity(getInput());
-
             final HttpPost getParametersCall = new HttpPost(uri);
-            getParametersCall.setEntity(content);
-
-            return new ReleasableInputStream(client.execute(getParametersCall).getEntity().getContent(),
-                    getParametersCall::releaseConnection);
-        } catch (Exception e) {
-            LOG.error("Exception while processing GET dynamic parameters : " + e.getMessage(), e);
-            throw e;
-        }
-
+            final InputStreamEntity entity = new InputStreamEntity(getInput());
+            getParametersCall.setEntity(entity);
+            return getParametersCall;
+        });
+        on(HttpStatus.OK).then(pipeStream());
     }
+
 }
--- dataprep-api/src/main/java/org/talend/dataprep/api/service/command/transformation/SuggestColumnActions.java
+++ dataprep-api/src/main/java/org/talend/dataprep/api/service/command/transformation/SuggestColumnActions.java
@@ -1,11 +1,11 @@
 package org.talend.dataprep.api.service.command.transformation;
 
-import java.io.ByteArrayInputStream;
+import static org.talend.dataprep.api.service.command.common.GenericCommand.Defaults.asNull;
+import static org.talend.dataprep.api.service.command.common.GenericCommand.Defaults.pipeStream;
+
 import java.io.InputStream;
 
 import org.apache.http.HttpHeaders;
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.InputStreamEntity;
@@ -14,21 +14,15 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.talend.dataprep.api.APIErrorCodes;
 import org.talend.dataprep.api.service.PreparationAPI;
-import org.talend.dataprep.api.service.command.ReleasableInputStream;
-import org.talend.dataprep.api.service.command.common.DataPrepCommand;
+import org.talend.dataprep.api.service.command.common.GenericCommand;
 import org.talend.dataprep.exception.TDPException;
-
-import com.netflix.hystrix.HystrixCommand;
 
 /**
  * Return the suggested actions to perform on a given column. So far, simple pass through to the transformation api.
  */
 @Component
 @Scope("request")
-public class SuggestColumnActions extends DataPrepCommand<InputStream> {
-
-    /** The column description to get the actions for (in json). */
-    private final InputStream input;
+public class SuggestColumnActions extends GenericCommand<InputStream> {
 
     /**
      * Constructor.
@@ -38,37 +32,16 @@ public class SuggestColumnActions extends DataPrepCommand<InputStream> {
      */
     private SuggestColumnActions(HttpClient client, InputStream input) {
         super(PreparationAPI.TRANSFORM_GROUP, client);
-        this.input = input;
+        execute(() -> {
+            HttpPost post = new HttpPost(transformationServiceUrl + "/suggest/column");
+            post.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+            post.setEntity(new InputStreamEntity(input));
+            return post;
+        });
+        onError((e) -> new TDPException(APIErrorCodes.UNABLE_TO_RETRIEVE_SUGGESTED_ACTIONS, e));
+        on(org.springframework.http.HttpStatus.NO_CONTENT).then(asNull());
+        on(org.springframework.http.HttpStatus.ACCEPTED).then(asNull());
+        on(org.springframework.http.HttpStatus.OK).then(pipeStream());
     }
 
-    /**
-     * @see HystrixCommand#run()
-     */
-    @Override
-    protected InputStream run() throws Exception {
-
-        // if there's no metadata, there's no actions to do...
-        if (input == null) {
-            // Column does not exist in data set metadata.
-            return new ByteArrayInputStream(new byte[0]);
-        }
-
-        HttpPost post = new HttpPost(transformationServiceUrl + "/suggest/column");
-        post.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
-
-        post.setEntity(new InputStreamEntity(input));
-
-        HttpResponse response = client.execute(post);
-        int statusCode = response.getStatusLine().getStatusCode();
-        if (statusCode >= 200) {
-            if (statusCode == HttpStatus.SC_NO_CONTENT || statusCode == HttpStatus.SC_ACCEPTED) {
-                // Immediately release connection
-                post.releaseConnection();
-                return null;
-            } else if (statusCode == HttpStatus.SC_OK) {
-                return new ReleasableInputStream(response.getEntity().getContent(), post::releaseConnection);
-            }
-        }
-        throw new TDPException(APIErrorCodes.UNABLE_TO_RETRIEVE_SUGGESTED_ACTIONS);
-    }
 }
