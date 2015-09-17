dbb2afc6d414c9cf6e05f8e38dc8244ad8cecffe
fhuaulme <fhuaulme@talend.com>
2015-09-17 16:52:04 +0200

    Migrate missing ExportType commands to new API. Remove now unused DataPrepCommand.

--- dataprep-api/src/main/java/org/talend/dataprep/api/service/command/export/ExportTypes.java
+++ dataprep-api/src/main/java/org/talend/dataprep/api/service/command/export/ExportTypes.java
@@ -1,31 +1,25 @@
 package org.talend.dataprep.api.service.command.export;
 
+import static org.talend.dataprep.api.service.command.common.GenericCommand.Defaults.pipeStream;
+
 import java.io.InputStream;
 
-import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.springframework.context.annotation.Scope;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.talend.dataprep.api.service.APIService;
-import org.talend.dataprep.api.service.command.ReleasableInputStream;
-import org.talend.dataprep.api.service.command.common.DataPrepCommand;
+import org.talend.dataprep.api.service.command.common.GenericCommand;
 
 @Component
 @Scope("request")
-public class ExportTypes extends DataPrepCommand<InputStream> {
+public class ExportTypes extends GenericCommand<InputStream> {
 
-    protected ExportTypes(final HttpClient client) {
+    private ExportTypes(final HttpClient client) {
         super(APIService.TRANSFORM_GROUP, client);
+        execute(() -> new HttpGet(this.transformationServiceUrl + "/export/types"));
+        on(HttpStatus.OK).then(pipeStream());
     }
 
-    @Override
-    protected InputStream run() throws Exception {
-        HttpGet get = new HttpGet(this.transformationServiceUrl + "/export/types");
-
-        HttpResponse response = client.execute(get);
-
-        return new ReleasableInputStream(response.getEntity().getContent(), get::releaseConnection);
-
-    }
 }
