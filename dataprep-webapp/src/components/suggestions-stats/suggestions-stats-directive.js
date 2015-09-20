d2d3ea01d7f4df6c61bb98f0c875c01894c72281
cnguyen <cnguyen@talend.com>
2015-09-20 16:59:39 +0200

    TDP-398: set  panel header fixed

--- dataprep-webapp/src/components/suggestions-stats/suggestions-stats-directive.js
+++ dataprep-webapp/src/components/suggestions-stats/suggestions-stats-directive.js
@@ -1,7 +1,7 @@
 (function() {
     'use strict';
 
-    function SuggestionsStats() {
+    function SuggestionsStats($timeout) {
         return {
             restrict: 'E',
             templateUrl: 'components/suggestions-stats/suggestions-stats.html',
@@ -10,7 +10,36 @@
             },
             bindToController: true,
             controllerAs: 'suggestionsStatsCtrl',
-            controller: 'SuggestionsStatsCtrl'
+            controller: 'SuggestionsStatsCtrl',
+            link: function(scope, iElement) {
+
+                //Resize action-suggestion and stat-detail panels
+                $timeout(function(){
+                    var handler = angular.element('.split-handler');
+                    var panel1 = angular.element('.split-pane1');
+                    var panel2 = angular.element('.split-pane2');
+
+                    var drag = false;
+
+                    iElement.bind('mousemove', function () {
+                        if (!drag) {
+                            return;
+                        }
+                        angular.element('.action-suggestion-tab-items').css('height', panel1.height()-100 + 'px');
+                        angular.element('.stat-detail-tab-items').css('height', panel2.height()-100 + 'px');
+                    });
+
+                    handler.bind('mousedown', function (ev) {
+                        ev.preventDefault();
+                        drag = true;
+                    });
+
+                    angular.element(document).bind('mouseup', function () {
+                        drag = false;
+                    });
+                }, 200);
+
+            }
         };
     }
 
