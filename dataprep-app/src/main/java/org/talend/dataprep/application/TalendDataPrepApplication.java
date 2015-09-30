35c65ab71f7ac7736a2344ca580e91a850fa28f1
fhuaulme <fhuaulme@talend.com>
2015-09-30 15:32:31 +0200

    TDP-643: Clean up log and storage directories.
    Redirect all info log to a file (except start up log info).
    Clean up storage directories (removes "target/test" in favor of "talend/dataprep").

--- dataprep-app/src/main/java/org/talend/dataprep/application/TalendDataPrepApplication.java
+++ dataprep-app/src/main/java/org/talend/dataprep/application/TalendDataPrepApplication.java
@@ -18,20 +18,20 @@ public class TalendDataPrepApplication {
 
     public static void main(String[] args) {
         // start all in one application
+        LOGGER.info("Starting Talend Data Preparation...");
         final SpringApplication application = new SpringApplication(TalendDataPrepApplication.class);
         final ConfigurableApplicationContext context = application.run(args);
         // log information about current configuration
         final ConfigurableEnvironment environment = context.getEnvironment();
-        LOGGER.info("Connection information:");
-        LOGGER.info("Dataset location: {}.", environment.getProperty("dataset.service.url"));
-        LOGGER.info("Transformation location: {}.", environment.getProperty("transformation.service.url"));
-        LOGGER.info("Preparation location: {}.", environment.getProperty("preparation.service.url"));
-
-        LOGGER.info("Store locations:");
-        LOGGER.info("Metadata location: {}.", environment.getProperty("dataset.metadata.store.file.location"));
-        LOGGER.info("Content location: {}.", environment.getProperty("dataset.content.store.file.location"));
-        LOGGER.info("Store location: {}.", environment.getProperty("preparation.store.file.location"));
-        LOGGER.info("User data location: {}.", environment.getProperty("user.data.store.file.location"));
+        LOGGER.debug("Dataset location: {}.", environment.getProperty("dataset.service.url"));
+        LOGGER.debug("Transformation location: {}.", environment.getProperty("transformation.service.url"));
+        LOGGER.debug("Preparation location: {}.", environment.getProperty("preparation.service.url"));
+        LOGGER.debug("Metadata location: {}.", environment.getProperty("dataset.metadata.store.file.location"));
+        LOGGER.debug("Content location: {}.", environment.getProperty("dataset.content.store.file.location"));
+        LOGGER.debug("Store location: {}.", environment.getProperty("preparation.store.file.location"));
+        LOGGER.debug("User data location: {}.", environment.getProperty("user.data.store.file.location"));
+        final String url = "http://127.0.0.1:" + environment.getProperty("server.port") + "/ui/index.html";
+        LOGGER.info("Talend Data Preparation started @ {}.", url);
     }
 
 }
