02f8113b8e2bd9000bdbe99aa18d8abdeaa2442f
amaalej <amaalej@talend.com>
2015-10-06 18:20:13 +0200

    TDP-655 got rid of an unused task

--- dataprep-webapp/gulp/watch.js
+++ dataprep-webapp/gulp/watch.js
@@ -4,7 +4,7 @@ var gulp = require('gulp');
 
 gulp.task('watch', ['wiredep', 'injector:css', 'injector:js'] ,function () {
   gulp.watch('src/**/*.scss', ['injector:css']);
-  gulp.watch('src/**/*.js', ['injector:js', 'copy-personnal-files']);
+  gulp.watch('src/**/*.js', ['injector:js']);
   gulp.watch('src/assets/config/**/*.json', ['copy-personnal-files']);
   gulp.watch('src/assets/images/**/*', ['images']);
   gulp.watch('bower.json', ['wiredep']);
