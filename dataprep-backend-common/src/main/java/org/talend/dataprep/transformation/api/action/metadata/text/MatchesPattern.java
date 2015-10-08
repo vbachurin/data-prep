fcb3177dbf2a43c4ebbbcf1dfef11c10d7c5657d
Boubacar Diouf <bdiouf@talend.com>
2015-10-08 18:59:28 +0200

    TDP-171: Add matches pattern action

---
+++ dataprep-backend-common/src/main/java/org/talend/dataprep/transformation/api/action/metadata/text/MatchesPattern.java
@@ -0,0 +1,156 @@
+package org.talend.dataprep.transformation.api.action.metadata.text;
+
+import static org.apache.commons.lang.StringUtils.EMPTY;
+import static org.apache.commons.lang.BooleanUtils.toStringTrueFalse;
+import static org.talend.dataprep.transformation.api.action.parameters.ParameterType.STRING;
+
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+import javax.annotation.Nonnull;
+
+import org.springframework.stereotype.Component;
+import org.talend.dataprep.api.dataset.ColumnMetadata;
+import org.talend.dataprep.api.dataset.DataSetRow;
+import org.talend.dataprep.api.dataset.RowMetadata;
+import org.talend.dataprep.api.type.Type;
+import org.talend.dataprep.transformation.api.action.context.TransformationContext;
+import org.talend.dataprep.transformation.api.action.metadata.category.ActionCategory;
+import org.talend.dataprep.transformation.api.action.metadata.common.AbstractActionMetadata;
+import org.talend.dataprep.transformation.api.action.metadata.common.ActionMetadata;
+import org.talend.dataprep.transformation.api.action.metadata.common.ColumnAction;
+import org.talend.dataprep.transformation.api.action.parameters.Parameter;
+import org.talend.dataprep.transformation.api.action.parameters.SelectParameter;
+
+@Component(MatchesPattern.ACTION_BEAN_PREFIX + MatchesPattern.MATCHES_PATTERN_ACTION_NAME)
+public class MatchesPattern extends AbstractActionMetadata implements ColumnAction {
+
+	/**
+	 * The action name.
+	 */
+	public static final String MATCHES_PATTERN_ACTION_NAME = "matches_pattern"; //$NON-NLS-1$
+
+	/**
+	 * The column appendix.
+	 */
+	public static final String APPENDIX = "_matching"; //$NON-NLS-1$
+
+	/**
+	 * @see ActionMetadata#getName()
+	 */
+	@Override
+	public String getName() {
+		return MATCHES_PATTERN_ACTION_NAME;
+	}
+
+	/**
+	 * The pattern shown to the user as a list. An item in this list is the
+	 * value 'other', which allow the user to manually enter his pattern.
+	 */
+	private static final String PATTERN_PARAMETER = "proposed_pattern"; //$NON-NLS-1$
+
+	/**
+	 * The pattern manually specified by the user. Should be used only if
+	 * PATTERN_PARAMETER value is 'other'.
+	 */
+	private static final String MANUAL_PATTERN_PARAMETER = "manual_pattern"; //$NON-NLS-1$
+
+
+	/**
+	 * @see ActionMetadata#acceptColumn(ColumnMetadata)
+	 */
+	@Override
+	public boolean acceptColumn(ColumnMetadata column) {
+		return Type.STRING.equals(Type.get(column.getType()));
+	}
+
+	/**
+	 * @see ActionMetadata#getCategory()
+	 */
+	@Override
+	public String getCategory() {
+		return ActionCategory.STRINGS.getDisplayName();
+	}
+
+	@Override
+	@Nonnull
+	public List<Parameter> getParameters() {
+		final List<Parameter> parameters = super.getParameters();
+		// @formatter:off
+		parameters.add(SelectParameter.Builder.builder()
+				.name(PATTERN_PARAMETER)
+				.item("[a-z]*")
+				.item("[A-Z]*")
+				.item("[0-9]*")
+				.item("[a-zA-Z]*")
+				.item("[a-zA-Z0-9]*")
+				.item(" *")
+				.item(".*")
+				.item("other", new Parameter(MANUAL_PATTERN_PARAMETER, STRING, EMPTY))
+				.defaultValue("[a-zA-Z]*")
+				.build());
+		// @formatter:on
+		return parameters;
+	}
+
+	/**
+     * @param parameters the action parameters.
+     * @return the pattern to use according to the given parameters.
+     */
+    private String getPattern(Map<String, String> parameters) {
+        return ("other").equals(parameters.get(PATTERN_PARAMETER)) ? parameters.get(MANUAL_PATTERN_PARAMETER) : parameters
+                .get(PATTERN_PARAMETER);
+    }
+    
+	/**
+	 * @see ColumnAction#applyOnColumn(DataSetRow, TransformationContext, Map,
+	 *      String)
+	 */
+	@Override
+	public void applyOnColumn(DataSetRow row, TransformationContext context, Map<String, String> parameters,
+			String columnId) {
+		// Retrieve the pattern to use
+        final String realPattern = getPattern(parameters);
+		try{
+			final Pattern pattern = Pattern.compile(realPattern);
+			
+			// create new column and append it after current column
+			final RowMetadata rowMetadata = row.getRowMetadata();
+			final ColumnMetadata column = rowMetadata.getById(columnId);
+			final ColumnMetadata newCol = createNewColumn(column);
+			final String matchingColumn = rowMetadata.insertAfter(columnId, newCol);
+			
+			// is the pattern matching
+			final String value = row.get(columnId);
+			final Matcher matcher = pattern.matcher(value == null ?"":value);
+			row.set(matchingColumn, toStringTrueFalse(matcher.matches()));
+		}
+		catch (PatternSyntaxException e){
+			return;
+		}
+		
+	}
+
+	/**
+	 * Create the new "string matching" column
+	 *
+	 * @param column
+	 *            the current column metadata
+	 * @return the new column metadata
+	 */
+	private ColumnMetadata createNewColumn(final ColumnMetadata column) {
+		return ColumnMetadata.Builder //
+				.column() //
+				.name(column.getName() + APPENDIX) //
+				.type(Type.BOOLEAN) //
+				.empty(column.getQuality().getEmpty()) //
+				.invalid(column.getQuality().getInvalid()) //
+				.valid(column.getQuality().getValid()) //
+				.headerSize(column.getHeaderSize()) //
+				.build();
+	}
+
+}
---
+++ dataprep-backend-common/src/test/java/org/talend/dataprep/transformation/api/action/metadata/text/MatchesPatternTest.java
@@ -0,0 +1,247 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2014 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.dataprep.transformation.api.action.metadata.text;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+import static org.talend.dataprep.api.dataset.ColumnMetadata.Builder.column;
+import static org.talend.dataprep.transformation.api.action.metadata.ActionMetadataTestUtils.getColumn;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.talend.dataprep.api.dataset.ColumnMetadata;
+import org.talend.dataprep.api.dataset.DataSetRow;
+import org.talend.dataprep.api.dataset.RowMetadata;
+import org.talend.dataprep.api.type.Type;
+import org.talend.dataprep.transformation.api.action.context.TransformationContext;
+import org.talend.dataprep.transformation.api.action.metadata.ActionMetadataTestUtils;
+import org.talend.dataprep.transformation.api.action.metadata.category.ActionCategory;
+
+/**
+ * Test class for Match Pattern action. Creates one consumer, and test it.
+ *
+ * @see Split
+ */
+public class MatchesPatternTest {
+	/**
+	 * The action to test.
+	 */
+	private MatchesPattern action;
+
+	private Map<String, String> parameters;
+
+	@Before
+	public void init() throws IOException {
+		action = new MatchesPattern();
+
+		parameters = ActionMetadataTestUtils.parseParameters( //
+				action, //
+				MatchesPatternTest.class.getResourceAsStream("matchesPattern.json"));
+	}
+
+	@Test
+	public void testAdapt() throws Exception {
+		assertThat(action.adapt(null), is(action));
+		ColumnMetadata column = column().name("myColumn").id(0).type(Type.STRING).build();
+		assertThat(action.adapt(column), is(action));
+	}
+
+	@Test
+	public void testCategory() throws Exception {
+		assertThat(action.getCategory(), is(ActionCategory.STRINGS.getDisplayName()));
+	}
+
+	/**
+	 * @see Split#create(Map)
+	 */
+	@Test
+	public void shouldMatchPattern() {
+		// given
+		final Map<String, String> values = new HashMap<>();
+		values.put("0000", "lorem bacon");
+		values.put("0001", "Bacon");
+		values.put("0002", "01/01/2015");
+		final DataSetRow row = new DataSetRow(values);
+
+		final Map<String, String> expectedValues = new HashMap<>();
+		expectedValues.put("0000", "lorem bacon");
+		expectedValues.put("0001", "Bacon");
+		expectedValues.put("0003", "true");
+		expectedValues.put("0002", "01/01/2015");
+
+		// when
+		action.applyOnColumn(row, new TransformationContext(), parameters, "0001");
+
+		// then
+		assertEquals(expectedValues, row.values());
+	}
+	
+	/**
+	 * @see Split#create(Map)
+	 */
+	@Test
+	public void shouldNotMatchPattern() {
+		// given
+		final Map<String, String> values = new HashMap<>();
+		values.put("0000", "lorem bacon");
+		values.put("0001", " ");
+		values.put("0002", "01/01/2015");
+		final DataSetRow row = new DataSetRow(values);
+
+		final Map<String, String> expectedValues = new HashMap<>();
+		expectedValues.put("0000", "lorem bacon");
+		expectedValues.put("0001", " ");
+		expectedValues.put("0003", "false");
+		expectedValues.put("0002", "01/01/2015");
+
+		// when
+		action.applyOnColumn(row, new TransformationContext(), parameters, "0001");
+
+		// then
+		assertEquals(expectedValues, row.values());
+	}
+
+
+	/**
+	 * @see Split#create(Map)
+	 */
+	@Test
+	public void shouldMatchEmptyString() {
+		// given
+		final Map<String, String> values = new HashMap<>();
+		values.put("0000", "lorem bacon");
+		values.put("0001", "");
+		values.put("0002", "01/01/2015");
+		final DataSetRow row = new DataSetRow(values);
+
+		final Map<String, String> expectedValues = new HashMap<>();
+		expectedValues.put("0000", "lorem bacon");
+		expectedValues.put("0001", "");
+		expectedValues.put("0003", "true");
+		expectedValues.put("0002", "01/01/2015");
+
+		// when
+		action.applyOnColumn(row, new TransformationContext(), parameters, "0001");
+
+		// then
+		assertEquals(expectedValues, row.values());
+	}
+
+	/**
+	 * @see Split#create(Map)
+	 */
+	@Test
+	public void shouldMatchPatternTwice() {
+		// given
+		final Map<String, String> values = new HashMap<>();
+		values.put("0000", "lorem bacon");
+		values.put("0001", "Bacon");
+		values.put("0002", "01/01/2015");
+		final DataSetRow row = new DataSetRow(values);
+
+		final Map<String, String> expectedValues = new HashMap<>();
+		expectedValues.put("0000", "lorem bacon");
+		expectedValues.put("0001", "Bacon");
+		expectedValues.put("0004", "true");
+		expectedValues.put("0003", "true");
+		expectedValues.put("0002", "01/01/2015");
+
+		// when
+		action.applyOnColumn(row, new TransformationContext(), parameters, "0001");
+		action.applyOnColumn(row, new TransformationContext(), parameters, "0001");
+
+		// then
+		assertEquals(expectedValues, row.values());
+	}
+
+	/**
+	 * @see ComputeLength#create(Map)
+	 */
+	@Test
+	public void shouldUpdateMetadata() {
+		// given
+		final List<ColumnMetadata> input = new ArrayList<>();
+		input.add(createMetadata("0000", "recipe"));
+		input.add(createMetadata("0001", "steps"));
+		input.add(createMetadata("0002", "last update"));
+		final RowMetadata rowMetadata = new RowMetadata(input);
+
+		final List<ColumnMetadata> expected = new ArrayList<>();
+		expected.add(createMetadata("0000", "recipe"));
+		expected.add(createMetadata("0001", "steps"));
+		expected.add(createMetadata("0003", "steps_matching", Type.BOOLEAN));
+		expected.add(createMetadata("0002", "last update"));
+
+		// when
+		action.applyOnColumn(new DataSetRow(rowMetadata), new TransformationContext(), parameters, "0001");
+
+		// then
+		assertEquals(expected, rowMetadata.getColumns());
+	}
+
+	/**
+	 * @see ComputeLength#create(Map)
+	 */
+	@Test
+	public void shouldUpdateMetadataTwice() {
+		// given
+		final List<ColumnMetadata> input = new ArrayList<>();
+		input.add(createMetadata("0000", "recipe"));
+		input.add(createMetadata("0001", "steps"));
+		input.add(createMetadata("0002", "last update"));
+		final RowMetadata rowMetadata = new RowMetadata(input);
+
+		final List<ColumnMetadata> expected = new ArrayList<>();
+		expected.add(createMetadata("0000", "recipe"));
+		expected.add(createMetadata("0001", "steps"));
+		expected.add(createMetadata("0004", "steps_matching", Type.BOOLEAN));
+		expected.add(createMetadata("0003", "steps_matching", Type.BOOLEAN));
+		expected.add(createMetadata("0002", "last update"));
+
+		// when
+		action.applyOnColumn(new DataSetRow(rowMetadata), new TransformationContext(), parameters, "0001");
+		action.applyOnColumn(new DataSetRow(rowMetadata), new TransformationContext(), parameters, "0001");
+
+		// then
+		assertEquals(expected, rowMetadata.getColumns());
+	}
+
+	@Test
+	public void shouldAcceptColumn() {
+		assertTrue(action.acceptColumn(getColumn(Type.STRING)));
+	}
+
+	@Test
+	public void shouldNotAcceptColumn() {
+		assertFalse(action.acceptColumn(getColumn(Type.NUMERIC)));
+		assertFalse(action.acceptColumn(getColumn(Type.FLOAT)));
+		assertFalse(action.acceptColumn(getColumn(Type.DATE)));
+		assertFalse(action.acceptColumn(getColumn(Type.BOOLEAN)));
+	}
+
+	private ColumnMetadata createMetadata(String id, String name) {
+		return createMetadata(id, name, Type.BOOLEAN);
+	}
+
+	private ColumnMetadata createMetadata(String id, String name, Type type) {
+		return ColumnMetadata.Builder.column().computedId(id).name(name).type(type).headerSize(12).empty(0).invalid(2)
+				.valid(5).build();
+	}
+
+}
