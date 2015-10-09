ddc2f5e698b7c867fd676d609184e35ca371f4a4
fhuaulme <fhuaulme@talend.com>
2015-10-09 10:48:07 +0200

    TDP-393: Updates all transformation unit test following API & behavior changes.

--- dataprep-transformation/src/test/java/org/talend/dataprep/transformation/service/ActionTests.java
+++ dataprep-transformation/src/test/java/org/talend/dataprep/transformation/service/ActionTests.java
@@ -1,338 +1,47 @@
 package org.talend.dataprep.transformation.service;
 
 import static com.jayway.restassured.RestAssured.given;
-import static org.hamcrest.core.Is.is;
-import static org.skyscreamer.jsonassert.JSONAssert.assertEquals;
-import static org.springframework.http.HttpStatus.OK;
+import static com.jayway.restassured.http.ContentType.JSON;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.talend.dataprep.test.SameJSONFile.sameJSONAsFile;
 
 import org.apache.commons.io.IOUtils;
 import org.junit.Test;
-import org.talend.dataprep.transformation.Application;
 
 /**
- * Integration tests on actions.
+ * Integration tests on column actions.
  */
 public class ActionTests extends TransformationServiceBaseTests {
 
     @Test
-    public void emptyTransformation() {
-        given()//
-                .multiPart("content", "")//
-                .when()//
-                .post("/transform/JSON")//
-                .then()//
-                .statusCode(OK.value());
-    }
-
-    @Test
-    public void noAction() throws Exception {
-        // given
-        final String initialContent = IOUtils.toString(Application.class.getResourceAsStream("actions/input_case.json"));
-
+    public void noColumnActions() throws Exception {
         // when
-        final String transformedContent = given()//
-                .multiPart("actions", "")//
-                .multiPart("content", initialContent)//
-                .when().post("/transform/JSON")//
-                .asString();
-
-        // then
-        assertEquals(initialContent, transformedContent, false);
-    }
-
-    @Test
-    public void noActionWithCarrierReturn() throws Exception {
-        // given
-        final String initialContent = IOUtils
-                .toString(Application.class.getResourceAsStream("actions/input_with_carrier_return.json"));
-
-        // when
-        final String transformedContent = given()//
-                .multiPart("actions", "")//
-                .multiPart("content", initialContent)//
-                .when()//
-                .post("/transform/JSON")//
-                .asString();
-
-        // then
-        assertEquals(initialContent, transformedContent, false);
-    }
-
-    @Test
-    public void testInvalidJSONInput() throws Exception {
-        given()//
-                .multiPart("content", "invalid content on purpose.")//
-                .when()//
-                .post("/transform/JSON")//
-                .then()//
-                .statusCode(400)//
-                .content("code", is("TDP_ALL_UNABLE_TO_PARSE_JSON"));
-    }
-
-    // ------------------------------------------------------------------------------------------------------------------
-    // --------------------------------------------------------Actions---------------------------------------------------
-    // ------------------------------------------------------------------------------------------------------------------
-
-    @Test
-    public void uppercaseAction() throws Exception {
-        // given
-        final String actions = IOUtils.toString(Application.class.getResourceAsStream("actions/uppercaseAction.json"));
-        final String initialContent = IOUtils.toString(Application.class.getResourceAsStream("actions/input_case.json"));
-        final String expectedContent = IOUtils
-                .toString(Application.class.getResourceAsStream("actions/uppercaseAction_expected.json"));
-
-        // when
-        final String transformedContent = given() //
-                .multiPart("actions", actions) //
-                .multiPart("content", initialContent) //
+        final String response = given() //
+                .contentType(JSON) //
+                .body("") //
                 .when() //
-                .post("/transform/JSON") //
+                .post("/actions/column") //
                 .asString();
 
         // then
-        assertEquals(expectedContent, transformedContent, false);
+        assertThat(response, sameJSONAsFile(ActionTests.class.getResourceAsStream("all_actions.json")));
     }
 
     @Test
-    public void lowercaseAction() throws Exception {
+    public void columnActions() throws Exception {
         // given
-        final String actions = IOUtils.toString(Application.class.getResourceAsStream("actions/lowercaseAction.json"));
-        final String initialContent = IOUtils.toString(Application.class.getResourceAsStream("actions/input_case.json"));
-        final String expectedContent = IOUtils
-                .toString(Application.class.getResourceAsStream("actions/lowercaseAction_expected.json"));
+        final String columnMetadata = IOUtils.toString(ActionTests.class.getResourceAsStream("../suggestions/string_column.json"));
 
         // when
-        final String transformedContent = given() //
-                .multiPart("actions", actions) //
-                .multiPart("content", initialContent) //
+        final String response = given() //
+                .contentType(JSON) //
+                .body(columnMetadata) //
                 .when() //
-                .post("/transform/JSON") //
+                .post("/actions/column") //
                 .asString();
 
         // then
-        assertEquals(expectedContent, transformedContent, false);
+        assertThat(response, sameJSONAsFile(ActionTests.class.getResourceAsStream("all_actions.json")));
     }
 
-    @Test
-    public void fillEmptyWithDefaultAction() throws Exception {
-        // given
-        final String actions = IOUtils.toString(Application.class.getResourceAsStream("actions/fillEmptyWithDefaultAction.json"));
-        final String initialContent = IOUtils.toString(Application.class.getResourceAsStream("actions/input.json"));
-        final String expectedContent = IOUtils
-                .toString(Application.class.getResourceAsStream("actions/fillEmptyWithDefaultAction_expected.json"));
-
-        // when
-        final String transformedContent = given() //
-                .multiPart("actions", actions) //
-                .multiPart("content", initialContent) //
-                .when() //
-                .post("/transform/JSON") //
-                .asString();
-
-        // then
-        assertEquals(expectedContent, transformedContent, false);
-    }
-
-    @Test
-    public void fillEmptyWithDefaultActionBoolean() throws Exception {
-        // given
-        final String actions = IOUtils
-                .toString(Application.class.getResourceAsStream("actions/fillEmptyWithDefaultBooleanAction.json"));
-        final String initialContent = IOUtils.toString(Application.class.getResourceAsStream("actions/input.json"));
-        final String expectedContent = IOUtils
-                .toString(Application.class.getResourceAsStream("actions/fillEmptyWithDefaultBooleanAction_expected.json"));
-
-        // when
-        final String transformedContent = given() //
-                .multiPart("actions", actions) //
-                .multiPart("content", initialContent) //
-                .when() //
-                .post("/transform/JSON") //
-                .asString();
-
-        // then
-        assertEquals(expectedContent, transformedContent, false);
-    }
-
-    @Test
-    public void fillEmptyWithDefaultActionInteger() throws Exception {
-        // given
-        final String actions = IOUtils
-                .toString(Application.class.getResourceAsStream("actions/fillEmptyWithDefaultIntegerAction.json"));
-        final String initialContent = IOUtils.toString(Application.class.getResourceAsStream("actions/input.json"));
-        final String expectedContent = IOUtils
-                .toString(Application.class.getResourceAsStream("actions/fillEmptyWithDefaultIntegerAction_expected.json"));
-
-        // when
-        final String transformedContent = given() //
-                .multiPart("actions", actions) //
-                .multiPart("content", initialContent) //
-                .when() //
-                .post("/transform/JSON") //
-                .asString();
-
-        // then
-        assertEquals(expectedContent, transformedContent, false);
-    }
-
-    @Test
-    public void negateActionBoolean() throws Exception {
-        // given
-        final String actions = IOUtils.toString(Application.class.getResourceAsStream("actions/negateAction.json"));
-        final String initialContent = IOUtils.toString(Application.class.getResourceAsStream("actions/input.json"));
-        final String expectedContent = IOUtils
-                .toString(Application.class.getResourceAsStream("actions/negateAction_expected.json"));
-
-        // when
-        final String transformedContent = given() //
-                .multiPart("actions", actions) //
-                .multiPart("content", initialContent) //
-                .when() //
-                .post("/transform/JSON") //
-                .asString();
-
-        // then
-        assertEquals(expectedContent, transformedContent, false);
-    }
-
-    @Test
-    public void cutAction() throws Exception {
-        // given
-        final String actions = IOUtils.toString(Application.class.getResourceAsStream("actions/cutAction.json"));
-        final String initialContent = IOUtils.toString(Application.class.getResourceAsStream("actions/input_cut.json"));
-        final String expectedContent = IOUtils.toString(Application.class.getResourceAsStream("actions/cutAction_expected.json"));
-
-        // when
-        final String transformedContent = given() //
-                .multiPart("actions", actions) //
-                .multiPart("content", initialContent) //
-                .when() //
-                .post("/transform/JSON") //
-                .asString();
-
-        // then
-        assertEquals(expectedContent, transformedContent, false);
-    }
-
-    @Test
-    public void duplicateAction() throws Exception {
-        // given
-        final String actions = IOUtils.toString(Application.class.getResourceAsStream("actions/duplicateAction.json"));
-        final String initialContent = IOUtils.toString(Application.class.getResourceAsStream("actions/input_duplicate.json"));
-        final String expectedContent = IOUtils
-                .toString(Application.class.getResourceAsStream("actions/duplicateAction_expected.json"));
-
-        // when
-        final String transformedContent = given() //
-                .multiPart("actions", actions) //
-                .multiPart("content", initialContent) //
-                .when() //
-                .post("/transform/JSON") //
-                .asString();
-
-        // then
-        assertEquals(expectedContent, transformedContent, false);
-    }
-
-    @Test
-    public void deleteEmptyActionString() throws Exception {
-        // given
-        final String actions = IOUtils.toString(Application.class.getResourceAsStream("actions/deleteEmptyAction.json"));
-        final String initialContent = IOUtils.toString(Application.class.getResourceAsStream("actions/input.json"));
-        final String expectedContent = IOUtils
-                .toString(Application.class.getResourceAsStream("actions/deleteEmptyAction_expected.json"));
-
-        // when
-        final String transformedContent = given() //
-                .multiPart("actions", actions) //
-                .multiPart("content", initialContent) //
-                .when() //
-                .post("/transform/JSON") //
-                .asString();
-
-        // then
-        assertEquals(expectedContent, transformedContent, false);
-    }
-
-    @Test
-    public void absoluteIntAction() throws Exception {
-        // given
-        final String actions = IOUtils.toString(Application.class.getResourceAsStream("actions/absoluteIntAction.json"));
-        final String initialContent = IOUtils
-                .toString(Application.class.getResourceAsStream("actions/input_absoluteAction.json"));
-        final String expectedContent = IOUtils
-                .toString(Application.class.getResourceAsStream("actions/absoluteIntAction_expected.json"));
-
-        // when
-        final String transformedContent = given() //
-                .multiPart("actions", actions) //
-                .multiPart("content", initialContent) //
-                .when() //
-                .post("/transform/JSON") //
-                .asString();
-
-        // then
-        assertEquals(expectedContent, transformedContent, false);
-    }
-
-    @Test
-    public void absoluteFloatAction() throws Exception {
-        // given
-        final String actions = IOUtils.toString(Application.class.getResourceAsStream("actions/absoluteFloatAction.json"));
-        final String initialContent = IOUtils
-                .toString(Application.class.getResourceAsStream("actions/input_absoluteAction.json"));
-        final String expectedContent = IOUtils
-                .toString(Application.class.getResourceAsStream("actions/absoluteFloatAction_expected.json"));
-
-        // when
-        final String transformedContent = given() //
-                .multiPart("actions", actions) //
-                .multiPart("content", initialContent) //
-                .when() //
-                .post("/transform/JSON") //
-                .asString();
-
-        // then
-        assertEquals(expectedContent, transformedContent, false);
-    }
-
-    @Test
-    public void replaceOnValueAction() throws Exception {
-        // given
-        final String actions = IOUtils.toString(Application.class.getResourceAsStream("actions/replaceOnValueAction.json"));
-        final String initialContent = IOUtils.toString(Application.class.getResourceAsStream("actions/input.json"));
-        final String expectedContent = IOUtils
-                .toString(Application.class.getResourceAsStream("actions/replaceOnValueAction_expected.json"));
-
-        // when
-        final String transformedContent = given() //
-                .multiPart("actions", actions) //
-                .multiPart("content", initialContent) //
-                .when() //
-                .post("/transform/JSON") //
-                .asString();
-
-        // then
-        assertEquals(expectedContent, transformedContent, false);
-    }
-
-    @Test
-    public void transformStateColumn() throws Exception {
-        // given
-        final String actions = IOUtils.toString(Application.class.getResourceAsStream("actions/lowercaseAction_state.json"));
-        final String initialContent = IOUtils.toString(Application.class.getResourceAsStream("actions/input_state.json"));
-        final String expectedContent = IOUtils
-                .toString(Application.class.getResourceAsStream("actions/lowercaseAction_state_expected.json"));
-
-        // when
-        final String transformedContent = given() //
-                .multiPart("actions", actions) //
-                .multiPart("content", initialContent) //
-                .when() //
-                .post("/transform/JSON") //
-                .asString();
-
-        // then
-        assertEquals(expectedContent, transformedContent, false);
-    }
 }
