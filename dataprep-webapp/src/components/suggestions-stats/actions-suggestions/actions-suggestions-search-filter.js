95f22d2ed6544b45ece3110fc930920bc4592c53
cnguyen <cnguyen@talend.com>
2015-10-09 11:53:36 +0200

    TDP-551: Add input search action

---
+++ dataprep-webapp/src/components/suggestions-stats/actions-suggestions/actions-suggestions-search-filter.js
@@ -0,0 +1,22 @@
+(function() {
+    'use strict';
+
+    function ActionsSuggestionsSearchFilter() {
+        return function(arr, searchActionString){
+            if(!searchActionString){
+                return arr;
+            }
+            var result = [];
+            searchActionString = searchActionString.toLowerCase();
+            angular.forEach(arr, function(item){
+                if(item.label.toLowerCase().indexOf(searchActionString) !== -1){
+                    result.push(item);
+                }
+            });
+            return result;
+        };
+    }
+
+    angular.module('data-prep.actions-suggestions')
+        .filter('actionsSuggestionsSearchFilter', ActionsSuggestionsSearchFilter);
+})();
