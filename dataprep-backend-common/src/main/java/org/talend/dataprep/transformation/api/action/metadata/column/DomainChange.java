d9c9114bb659eb5a5e150c0358a76444db2816df
fhuaulme <fhuaulme@talend.com>
2015-10-15 14:23:16 +0200

    TDP-702: Review domain change action
    * Review incorrect behavior for domain change action.
    * Adds tests for change domain transformation.

--- dataprep-backend-common/src/main/java/org/talend/dataprep/transformation/api/action/metadata/column/DomainChange.java
+++ dataprep-backend-common/src/main/java/org/talend/dataprep/transformation/api/action/metadata/column/DomainChange.java
@@ -3,14 +3,12 @@ package org.talend.dataprep.transformation.api.action.metadata.column;
 import java.util.Map;
 
 import org.apache.commons.lang.StringUtils;
-import org.apache.commons.lang.math.NumberUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.talend.dataprep.api.dataset.ColumnMetadata;
 import org.talend.dataprep.api.dataset.DataSetRow;
 import org.talend.dataprep.transformation.api.action.context.TransformationContext;
-import org.talend.dataprep.transformation.api.action.metadata.SchemaChangeAction;
 import org.talend.dataprep.transformation.api.action.metadata.category.ActionCategory;
 import org.talend.dataprep.transformation.api.action.metadata.common.AbstractActionMetadata;
 import org.talend.dataprep.transformation.api.action.metadata.common.ActionMetadata;
@@ -21,9 +19,7 @@ import org.talend.dataprep.transformation.api.action.metadata.common.ColumnActio
  * It's available from column headers</b>
  */
 @Component(DomainChange.ACTION_BEAN_PREFIX + DomainChange.DOMAIN_CHANGE_ACTION_NAME)
-public class DomainChange extends AbstractActionMetadata implements ColumnAction, SchemaChangeAction {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(DomainChange.class);
+public class DomainChange extends AbstractActionMetadata implements ColumnAction {
 
     /**
      * The action name.
@@ -32,9 +28,7 @@ public class DomainChange extends AbstractActionMetadata implements ColumnAction
 
     public static final String NEW_DOMAIN_ID_PARAMETER_KEY = "new_domain_id";
 
-    public static final String NEW_DOMAIN_LABEL_PARAMETER_KEY = "new_domain_label";
-
-    public static final String NEW_DOMAIN_FREQUENCY_PARAMETER_KEY = "new_domain_frequency";
+    private static final Logger LOGGER = LoggerFactory.getLogger(DomainChange.class);
 
     /**
      * @see ActionMetadata#getName()
@@ -49,7 +43,7 @@ public class DomainChange extends AbstractActionMetadata implements ColumnAction
      */
     @Override
     public boolean acceptColumn(ColumnMetadata column) {
-        return false;
+        return true;
     }
 
     /**
@@ -65,26 +59,22 @@ public class DomainChange extends AbstractActionMetadata implements ColumnAction
      */
     @Override
     public void applyOnColumn(DataSetRow row, TransformationContext context, Map<String, String> parameters, String columnId) {
-
         LOGGER.debug("DomainChange for columnId {} with parameters {} ", columnId, parameters);
-
         final ColumnMetadata columnMetadata = row.getRowMetadata().getById(columnId);
         if (columnMetadata == null) {
             // FIXME exception?
             return;
         }
         final String newDomainId = parameters.get(NEW_DOMAIN_ID_PARAMETER_KEY);
-        final String newDomainLabel = parameters.get(NEW_DOMAIN_LABEL_PARAMETER_KEY);
-        final String newDomainFrequency = parameters.get(NEW_DOMAIN_FREQUENCY_PARAMETER_KEY);
-
         if (StringUtils.isNotEmpty(newDomainId)) {
             columnMetadata.setDomain(newDomainId);
-            columnMetadata.setDomainLabel(newDomainLabel);
-            columnMetadata.setDomainFrequency(NumberUtils.toFloat(newDomainFrequency, 0));
+            columnMetadata.setDomainFrequency(0);
             columnMetadata.setDomainForced(true);
-            forceColumn(context, columnId);
         }
-
     }
 
+    @Override
+    public ActionMetadata adapt(ColumnMetadata column) {
+        return this;
+    }
 }
