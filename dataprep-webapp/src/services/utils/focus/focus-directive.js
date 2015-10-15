a560d1e9db15e991937be8006d7cf98e6ab7bfba
Olivier Lamy <olamy@apache.org>
2015-10-15 22:20:06 +1100

    [TDP-87] do not select text per default add a directive attribute.

--- dataprep-webapp/src/services/utils/focus/focus-directive.js
+++ dataprep-webapp/src/services/utils/focus/focus-directive.js
@@ -7,8 +7,9 @@
      * @description This directive set focus on a element depending on a model value
      * @restrict A
      * @usage
-     * <input type='text' enable-focus='ctrl.beerTime'/>
+     * <input type='text' enable-focus='beerTime' select-on-focus='true'/>
      * in case of beerTime === true the input text will have focus.
+     * @param select-on-focus true to select the text or not when focus
      */
     function EnableFocus() {
         return {
@@ -17,7 +18,9 @@
                 scope.$watch(attrs.enableFocus, function(newValue, oldValue) {
                     if (newValue) {
                         iElement[0].focus();
-                        iElement[0].select();
+                        if (attrs.selectOnFocus && attrs.selectOnFocus==='true') {
+                            iElement[0].select();
+                        }
                     }
                 });
             }
