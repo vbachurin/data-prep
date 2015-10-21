69c908c1a0b84dd9867d601589cc64a0244e126e
Olivier Lamy <olamy@apache.org>
2015-10-21 15:08:22 +1100

    [TDP-401] file storage implementation

--- dataprep-backend-common/src/main/java/org/talend/dataprep/folder/store/file/FileSystemFolderRepository.java
+++ dataprep-backend-common/src/main/java/org/talend/dataprep/folder/store/file/FileSystemFolderRepository.java
@@ -4,10 +4,12 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.attribute.FileAttribute;
 import java.nio.file.attribute.PosixFilePermission;
 import java.nio.file.attribute.PosixFilePermissions;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Stream;
 
 import javax.annotation.PostConstruct;
@@ -24,7 +26,11 @@ import org.talend.dataprep.api.folder.FolderEntry;
 import org.talend.dataprep.folder.store.FolderRepository;
 import org.talend.dataprep.folder.store.FolderRepositoryAdapter;
 
-@Component
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import com.google.common.collect.Lists;
+
+@Component("folderRepository#file")
 @ConditionalOnProperty(name = "folder.store", havingValue = "file")
 public class FileSystemFolderRepository extends FolderRepositoryAdapter implements FolderRepository {
 
@@ -34,17 +40,18 @@ public class FileSystemFolderRepository extends FolderRepositoryAdapter implemen
     @Value("${folder.store.file.location}")
     private String foldersLocation;
 
+    private FileAttribute<Set<PosixFilePermission>> defaultFilePermissions = PosixFilePermissions.asFileAttribute( //
+            Sets.newHashSet(PosixFilePermission.GROUP_READ,
+                    //
+                    PosixFilePermission.GROUP_WRITE));
+
     /**
      * Make sure the root folder is there.
      */
     @PostConstruct
     private void init() {
         try {
-            Files.createDirectories(getRootFolder(),
-                    PosixFilePermissions.asFileAttribute( //
-                            Sets.newHashSet(PosixFilePermission.GROUP_READ,
-                                    //
-                                    PosixFilePermission.GROUP_WRITE)));
+            Files.createDirectories(getRootFolder(), defaultFilePermissions);
         } catch (IOException e) {
             throw new RuntimeException(e.getMessage(), e);
         }
@@ -62,41 +69,82 @@ public class FileSystemFolderRepository extends FolderRepositoryAdapter implemen
 
     @Override
     public Iterable<Folder> childs(Folder folder) {
-        List<String> pathParts = folder.getPathParts();
-        Path folderPath = Paths.get(getRootFolder().toString(), pathParts.toArray(new String[pathParts.size()]));
-        Stream<Path> childStream = Files.list(folderPath);
-        List<Folder> childs = new ArrayList<>();
-        childStream.forEach(path -> {
-            childs.add(Folder.Builder.folder().name(path.getFileName().toString()).pathParts(path.iterator()).build());
-        });
-        return childs;
+        try {
+            List<String> pathParts = folder.getPathParts();
+            Path folderPath = Paths.get(getRootFolder().toString(), pathParts.toArray(new String[pathParts.size()]));
+            Stream<Path> childStream = Files.list(folderPath);
+            List<Folder> childs = new ArrayList<>();
+            childStream.forEach(path -> {
+                childs.add(Folder.Builder.folder().name(path.getFileName().toString()).pathParts(pathParts(path)).build());
+            });
+            return childs;
+        } catch (IOException e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
     }
 
-    protected List<>
+    protected List<String> pathParts(Path path) {
+
+        Path relativePath = path.subpath(getRootFolder().getNameCount(), path.getNameCount());
+        final List<String> parts = new ArrayList<>();
+        relativePath.iterator().forEachRemaining(thePath -> parts.add(thePath.toString()));
+        return parts;
+    }
 
     @Override
     public Folder addFolder(Folder parent, Folder child) {
-        return null;
+        try {
+            List<String> pathParts = Lists.newArrayList(parent.getPathParts());
+            pathParts.add(child.getName());
+            Path path = Paths.get(getRootFolder().toString(), pathParts.toArray(new String[pathParts.size()]));
+
+            child.setPathParts(pathParts);
+            child.setId(child.getName());
+
+            if (!Files.exists(path)) {
+                Files.createDirectories(path, defaultFilePermissions);
+            }
+
+            return child;
+        } catch (IOException e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
+
     }
 
     @Override
     public void addFolderEntry(Folder parent, FolderEntry folderEntry) {
 
+        throw new NotImplementedException();
+
     }
 
     @Override
     public void removeFolderEntry(Folder parent, FolderEntry folderEntry) {
-
+        throw new NotImplementedException();
     }
 
     @Override
     public void removeFolder(Folder folder) {
+        List<String> pathParts = Lists.newArrayList(folder.getPathParts());
+
+        Path path = Paths.get(getRootFolder().toString(), pathParts.toArray(new String[pathParts.size()]));
+
+        try {
+            FileUtils.deleteDirectory(path.toFile());
+        } catch (IOException e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
 
     }
 
     @Override
     public Folder rootFolder() {
-        return null;
+
+        List<String> parts = new ArrayList<>();
+        getRootFolder().iterator().forEachRemaining(thePath -> parts.add(thePath.toString()));
+        String name = getRootFolder().getFileName().toString();
+        return Folder.Builder.folder().pathParts(parts).name(name).build();
     }
 
     @Override
--- dataprep-backend-common/src/test/java/org/talend/dataprep/folder/file/FileFolderStoreTest.java
+++ dataprep-backend-common/src/test/java/org/talend/dataprep/folder/file/FileFolderStoreTest.java
@@ -1,11 +1,15 @@
 package org.talend.dataprep.folder.file;
 
+import javax.inject.Inject;
+
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.talend.dataprep.folder.AbstractFolderTest;
+import org.talend.dataprep.folder.store.FolderRepository;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = FileFolderStoreTest.class)
@@ -13,4 +17,12 @@ import org.talend.dataprep.folder.AbstractFolderTest;
 @TestPropertySource(inheritLocations = false, inheritProperties = false, properties = { "folder.store=file",
         "folder.store.file.location=target/test/store/folders" })
 public class FileFolderStoreTest extends AbstractFolderTest {
+
+    @Inject
+    private ApplicationContext applicationContext;
+
+    @Override
+    protected FolderRepository getFolderRepository() {
+        return applicationContext.getBean("folderRepository#file", FolderRepository.class);
+    }
 }
