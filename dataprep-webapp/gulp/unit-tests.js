74dd372e667a8f6f50946cffbd88272b6dd88117
jsomsanith <jsomsanith@talend.com>
2015-10-22 12:01:27 +0200

    Run tests in 2 parts

--- dataprep-webapp/gulp/unit-tests.js
+++ dataprep-webapp/gulp/unit-tests.js
@@ -1,12 +1,26 @@
 'use strict';
 
 var gulp = require('gulp');
-
 var $ = require('gulp-load-plugins')();
-
 var wiredep = require('wiredep');
 
-function runTests(singleRun, done, karmaConfPath) {
+var fullTestsFiles = [
+    'src/*.js',
+    'src/{services,components}/**/*-module.js',
+    'src/{services,components,mocks}/**/*.js',
+    'src/components/**/*.html'
+];
+
+var filesToExclude = {
+    services: [
+        'src/components/**/*.spec.js'
+    ],
+    components: [
+        'src/services/**/*.spec.js'
+    ]
+};
+
+function runTests(singleRun, done, karmaConfPath, type) {
     var bowerDeps = wiredep({
         directory: 'bower_components',
         exclude: ['bootstrap-sass-official'],
@@ -14,19 +28,26 @@ function runTests(singleRun, done, karmaConfPath) {
         devDependencies: true
     });
 
-    var testFiles = bowerDeps.js.concat([
-        'src/*.js',
-        'src/{services,components}/**/*-module.js',
-        'src/{services,components,mocks}/**/*.js',
-        'src/components/**/*.html'
-    ]);
+    var testFiles = bowerDeps.js.concat(fullTestsFiles);
+
+    var preprocessors = {
+        'src/**/*.html':['ng-html2js']
+    };
+    var toCover = type ?
+        'src/' + type + '/**/!(*spec|*mock).js' :
+        'src/**/!(*spec|*mock).js';
+    preprocessors[toCover] = ['coverage'];
 
     return gulp.src([])
         .pipe($.karma({
             configFile: karmaConfPath,
             action: (singleRun) ? 'run' : 'watch',
-            files: testFiles
-
+            files: testFiles,
+            exclude: filesToExclude[type],
+            coverageReporter: {
+                dir: 'coverage/' + (type || '')
+            },
+            preprocessors: preprocessors
         }))
         .on('error', function (err) {
             // Make sure failed tests cause gulp to exit non-zero
@@ -34,9 +55,22 @@ function runTests(singleRun, done, karmaConfPath) {
         });
 }
 
+gulp.task('test:components', function (done) {
+    console.log('test:components');
+    runTests(true /* singleRun */, done, 'karma.conf.js', 'components')
+});
+
+gulp.task('test:services', function (done) {
+    console.log('test:services');
+    runTests(true /* singleRun */, done, 'karma.conf.js', 'services')
+});
+
+gulp.task('test:parts', ['test:components', 'test:services']);
+
 gulp.task('test', function (done) {
     runTests(true /* singleRun */, done, 'karma.conf.js')
 });
+
 gulp.task('test:auto', function (done) {
     runTests(false /* singleRun */, done, 'karma.conf.js')
 });
