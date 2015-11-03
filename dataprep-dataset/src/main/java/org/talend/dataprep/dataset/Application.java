a24470a5cb8268317afc97e03161da425127702a
fhuaulme <fhuaulme@talend.com>
2015-11-03 14:03:25 +0100

    TDP-737: Removes remaining mongo db presence in OS code
    * Moves MongoDB code to EE code repository.
    * Review Hazelcast setup for consitent Hazelcast shutdown.
    * Fix Log4j2 warning on shutdown (allow proper logging during shutdown).

--- dataprep-dataset/src/main/java/org/talend/dataprep/dataset/Application.java
+++ dataprep-dataset/src/main/java/org/talend/dataprep/dataset/Application.java
@@ -1,44 +1,17 @@
 package org.talend.dataprep.dataset;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Profile;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-
-import com.hazelcast.core.Hazelcast;
-import com.hazelcast.core.HazelcastInstance;
 
 @SpringBootApplication
 @Profile("standalone")
-@EnableMongoRepositories(basePackages = "org.talend.dataprep")
 @ComponentScan(basePackages = "org.talend.dataprep")
-public class Application implements DisposableBean {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
-
-    @Autowired(required = false)
-    private HazelcastInstance hazelcastInstance;
+public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
-    @Override
-    public void destroy() throws Exception {
-        // Hazelcast shutdown
-        if (hazelcastInstance != null) {
-            LOGGER.info("Stopping Hazelcast...");
-            hazelcastInstance.shutdown();
-            Hazelcast.shutdownAll();
-            LOGGER.info("Stopped Hazelcast.");
-        } else {
-            LOGGER.info("No Hazelcast to stop.");
-        }
-
-    }
 }
