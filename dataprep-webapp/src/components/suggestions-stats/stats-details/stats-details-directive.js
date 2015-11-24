ddbd46d7aea9c9de3bdeeef2503abe578b29e5b5
jsomsanith <jsomsanith@talend.com>
2015-11-24 11:28:10 +0100

    TDP-927 : remove suggestions/stats panel size management in js, replace it with css and flexbox for the win ! in the suggestions part

--- dataprep-webapp/src/components/suggestions-stats/stats-details/stats-details-directive.js
+++ dataprep-webapp/src/components/suggestions-stats/stats-details/stats-details-directive.js
@@ -1,23 +1,13 @@
 (function() {
     'use strict';
 
-    function StatsDetails($timeout) {
+    function StatsDetails() {
         return {
             restrict: 'E',
             templateUrl: 'components/suggestions-stats/stats-details/stats-details.html',
             bindToController: true,
             controllerAs: 'statsDetailsCtrl',
-            controller: 'StatsDetailsCtrl',
-            link: function (scope, iElement, iAttrs, ctrl) {
-                ctrl.resizePanels = function resizePanels() {
-                    $timeout(function () {
-                        //Force to resize tabs containers
-                        var panel2 = angular.element('.split-pane2').eq(0);
-                        var statHeaderPanelsSizeMargin = 45;
-                        iElement.find('.stat-detail-tab-items').css('height', panel2.height()- statHeaderPanelsSizeMargin + 'px');
-                    }, 200, false);
-                };
-            }
+            controller: 'StatsDetailsCtrl'
         };
     }
 
--- dataprep-webapp/src/components/suggestions-stats/suggestions-stats-directive.js
+++ dataprep-webapp/src/components/suggestions-stats/suggestions-stats-directive.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
 
-    function SuggestionsStats($timeout, $window) {
+    function SuggestionsStats($timeout) {
         return {
             restrict: 'E',
             templateUrl: 'components/suggestions-stats/suggestions-stats.html',
@@ -14,55 +14,15 @@
             link: function (scope, iElement) {
 
                 $timeout(function () {
-                    var windowElement = angular.element($window);
-                    var documentElement = angular.element(document);
                     var handler = iElement.find('.split-handler').eq(0);
                     var panel1 = iElement.find('.split-pane1').eq(0);
                     var panel2 = iElement.find('.split-pane2').eq(0);
-
-                    var drag = false;
-                    var actionHeaderPanelsSizeMargin = 160;
-                    var statHeaderPanelsSizeMargin = 45;
-
-                    function resize() {
-                        iElement.find('.action-suggestion-tab-items').css('height', panel1.height() - actionHeaderPanelsSizeMargin + 'px');
-                        iElement.find('.stat-detail-tab-items').css('height', panel2.height() - statHeaderPanelsSizeMargin + 'px');
-                    }
-
-                    //drag event : resize the panels
-                    function startDrag() {
-                        drag = true;
-                    }
-                    function finishDrag() {
-                        drag = false;
-                    }
-
-                    handler.bind('mousedown', startDrag);
-                    documentElement.bind('mouseup',finishDrag);
-                    iElement.bind('mousemove', function () {
-                        if (drag) {
-                            resize();
-                        }
-                    });
-
-                    //Resize panel when window is resized
-                    windowElement.on('resize', resize);
 
                     //Initialization of the right panel
                     // 325px : to have at least 5 actions in the top panel
                     panel1.css('height', '310px');
                     handler.css('top', '310px');
                     panel2.css('top', '310px');
-                    resize();
-
-                    //on remove/destroy, clean events
-                    iElement.on('$destroy', function () {
-                        scope.$destroy();
-                    });
-                    scope.$on('$destroy', function () {
-                        documentElement.off('mouseup', finishDrag);
-                        windowElement.off('resize', resize);
-                    });
                 }, 0, false);
             }
         };
