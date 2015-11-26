6013aba0ff5d611c56d921b5006f3bc5e4e99776
fhuaulme <fhuaulme@talend.com>
2015-11-26 17:15:18 +0100

    TDP-724: Adapts sample data with folder
    * Move us state to folder.
    * Fix configuration (missing folder store).

--- dataprep-api/src/main/java/org/talend/dataprep/api/service/command/folder/FolderDataSetList.java
+++ dataprep-api/src/main/java/org/talend/dataprep/api/service/command/folder/FolderDataSetList.java
@@ -26,37 +26,26 @@ import org.talend.dataprep.api.service.command.common.GenericCommand;
 import org.talend.dataprep.exception.TDPException;
 import org.talend.dataprep.exception.error.APIErrorCodes;
 import org.talend.dataprep.exception.error.CommonErrorCodes;
+import org.talend.dataprep.exception.error.DataSetErrorCodes;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.netflix.hystrix.HystrixCommand;
-import org.talend.dataprep.exception.error.DataSetErrorCodes;
 
 @Component
 @Scope("request")
 public class FolderDataSetList extends GenericCommand<FolderContent> {
 
-    private FolderDataSetList(HttpClient client, String sort, String order) {
-        this(client, sort, order, null);
-    }
-
     private FolderDataSetList(HttpClient client, String sort, String order, String folder) {
         super(PreparationAPI.TRANSFORM_GROUP, client);
-
-        try {
-            execute(() -> onExecute(sort, order, folder));
-            onError(e -> new TDPException(APIErrorCodes.UNABLE_TO_LIST_DATASETS, e));
-            on(HttpStatus.NO_CONTENT, HttpStatus.ACCEPTED).then(emptyFolderContent());
-            on(HttpStatus.OK).then(buildContent(folder, order, sort));
-
-        } catch (Exception e) {
-            throw new TDPException(CommonErrorCodes.UNEXPECTED_EXCEPTION, e);
-        }
+        execute(() -> onExecute(sort, order, folder));
+        onError(e -> new TDPException(APIErrorCodes.UNABLE_TO_LIST_DATASETS, e));
+        on(HttpStatus.NO_CONTENT, HttpStatus.ACCEPTED).then(emptyFolderContent());
+        on(HttpStatus.OK).then(buildContent(folder, order, sort));
     }
 
     private HttpRequestBase onExecute(String sort, String order, String folder) {
         try {
-
             URIBuilder uriBuilder = new URIBuilder(datasetServiceUrl + "/datasets");
             uriBuilder.addParameter("sort", sort);
             uriBuilder.addParameter("order", order);
@@ -76,7 +65,8 @@ public class FolderDataSetList extends GenericCommand<FolderContent> {
         };
     }
 
-    public BiFunction<HttpRequestBase, HttpResponse, FolderContent> buildContent(final String searchFolder, final String order, final String sort) {
+    public BiFunction<HttpRequestBase, HttpResponse, FolderContent> buildContent(final String searchFolder, final String order,
+            final String sort) {
         return (request, response) -> {
             try {
                 ObjectMapper mapper = builder.build();
@@ -93,32 +83,32 @@ public class FolderDataSetList extends GenericCommand<FolderContent> {
                         new TypeReference<List<Folder>>() {
                 });
 
-
                 final Comparator<String> comparisonOrder;
                 switch (order.toUpperCase()) {
-                    case "ASC":
-                        comparisonOrder = Comparator.naturalOrder();
-                        break;
-                    case "DESC":
-                        comparisonOrder = Comparator.reverseOrder();
-                        break;
-                    default:
-                        throw new TDPException( DataSetErrorCodes.ILLEGAL_ORDER_FOR_LIST, ExceptionContext.build().put( "order", order));
+                case "ASC":
+                    comparisonOrder = Comparator.naturalOrder();
+                    break;
+                case "DESC":
+                    comparisonOrder = Comparator.reverseOrder();
+                    break;
+                default:
+                    throw new TDPException(DataSetErrorCodes.ILLEGAL_ORDER_FOR_LIST,
+                            ExceptionContext.build().put("order", order));
                 }
                 // Select comparator for sort (either by name or date)
                 final Comparator<Folder> comparator;
                 switch (sort.toUpperCase()) {
-                    case "NAME":
-                        comparator = Comparator.comparing(folder -> folder.getName().toUpperCase(), comparisonOrder);
-                        break;
-                    case "DATE":
-                        comparator = Comparator.comparing(folder -> String.valueOf(folder.getCreationDate()), comparisonOrder);
-                        break;
-                    default:
-                        throw new TDPException(DataSetErrorCodes.ILLEGAL_SORT_FOR_LIST, ExceptionContext.build().put("sort", order));
+                case "NAME":
+                    comparator = Comparator.comparing(folder -> folder.getName().toUpperCase(), comparisonOrder);
+                    break;
+                case "DATE":
+                    comparator = Comparator.comparing(folder -> String.valueOf(folder.getCreationDate()), comparisonOrder);
+                    break;
+                default:
+                    throw new TDPException(DataSetErrorCodes.ILLEGAL_SORT_FOR_LIST, ExceptionContext.build().put("sort", order));
                 }
 
-                folders.sort( comparator );
+                folders.sort(comparator);
 
                 return new FolderContent(datasets, folders);
             } catch (IOException e) {
--- dataprep-api/src/main/java/org/talend/dataprep/api/service/command/folder/FoldersList.java
+++ dataprep-api/src/main/java/org/talend/dataprep/api/service/command/folder/FoldersList.java
@@ -1,33 +1,27 @@
 package org.talend.dataprep.api.service.command.folder;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
+import static org.talend.dataprep.api.service.command.common.Defaults.pipeStream;
+import static org.talend.dataprep.exception.error.APIErrorCodes.UNABLE_TO_LIST_FOLDERS;
+
+import java.io.InputStream;
+import java.net.URISyntaxException;
+
 import org.apache.commons.lang.StringUtils;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpRequestBase;
 import org.apache.http.client.utils.URIBuilder;
-import org.apache.http.entity.ByteArrayEntity;
 import org.springframework.context.annotation.Scope;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.talend.daikon.exception.ExceptionContext;
-import org.talend.dataprep.api.folder.Folder;
 import org.talend.dataprep.api.service.APIService;
 import org.talend.dataprep.api.service.command.common.GenericCommand;
 import org.talend.dataprep.exception.TDPException;
 import org.talend.dataprep.exception.error.CommonErrorCodes;
 
-import java.io.InputStream;
-import java.net.URISyntaxException;
-
-import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
-import static org.talend.dataprep.api.service.command.common.Defaults.pipeStream;
-import static org.talend.dataprep.exception.error.APIErrorCodes.UNABLE_TO_LIST_FOLDERS;
-
 @Component
-@Scope("request")
+@Scope("prototype")
 public class FoldersList extends GenericCommand<InputStream> {
 
     private FoldersList(HttpClient client, String path) {
