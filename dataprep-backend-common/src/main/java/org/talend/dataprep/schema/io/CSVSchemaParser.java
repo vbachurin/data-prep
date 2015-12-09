9ab47b88b744b871026611e0e32d4542fbe28c2c
Boubacar Diouf <bdiouf@talend.com>
2015-12-09 19:32:46 +0100

    TDP-937: Avoid throwing exception when a row have more columns than header when performing naive guessing

--- dataprep-backend-common/src/main/java/org/talend/dataprep/schema/io/CSVSchemaParser.java
+++ dataprep-backend-common/src/main/java/org/talend/dataprep/schema/io/CSVSchemaParser.java
@@ -30,6 +30,11 @@ public class CSVSchemaParser implements SchemaParser {
 
     private static final String META_KEY = "key";
 
+    /**
+     *
+     * @param request container with information needed to parse the raw data.
+     * @return
+     */
     @Override
     public SchemaParserResult parse(Request request) {
         List<SchemaParserResult.SheetContent> sheetContents = new ArrayList<>();
@@ -39,7 +44,7 @@ public class CSVSchemaParser implements SchemaParser {
             final Map<String, String> parameters = metadata.getContent().getParameters();
             final char separator = parameters.get(CSVFormatGuess.SEPARATOR_PARAMETER).charAt(0);
             CSVReader reader = new CSVReader(new InputStreamReader(request.getContent(), metadata.getEncoding()), separator);
-            // First line has column names
+            // First line as column names
             String[] columns = reader.readNext();
             if (columns == null) { // Empty content?
                 reader.close();
@@ -54,8 +59,8 @@ public class CSVSchemaParser implements SchemaParser {
                         .get().getColumnMetadatas() //
                         .add(column().id(i).name(columns[i]).type(Type.STRING).build());
             }
-            // Best guess (and naive) on data types
-            bestGuess(reader, sheetContents);
+            // naively guess data types of columns
+            guessNaivelyColumnDataType(reader, sheetContents);
             LOGGER.debug("Default types for columns set.");
         } catch (IOException e) {
             throw new TDPException(CommonErrorCodes.UNABLE_TO_READ_CONTENT, e);
@@ -66,13 +71,15 @@ public class CSVSchemaParser implements SchemaParser {
     }
 
     /**
-     * Best (and naive) data type guess.
+     * Parses the ten first lines and naively guess the data type of columns.
+     * TODO: it guesses according to the last line read instead of using all ten lines.
      *
      * @param reader the csv reader.
      * @param sheetContents the sheet content.
      * @throws IOException if an error occurs.
      */
-    private void bestGuess(CSVReader reader, List<SchemaParserResult.SheetContent> sheetContents) throws IOException {
+    private void guessNaivelyColumnDataType(CSVReader reader, List<SchemaParserResult.SheetContent> sheetContents)
+            throws IOException {
         // Best guess (and naive) on data types
         String[] line;
         int lineNumber = 0;
@@ -80,26 +87,27 @@ public class CSVSchemaParser implements SchemaParser {
         while ((line = reader.readNext()) != null && lineNumber < 10) {
             for (int i = 0; i < line.length; i++) {
                 String columnValue = line[i];
+                Optional<SchemaParserResult.SheetContent> content = sheetContents.stream() //
+                        .filter(sheetContent -> META_KEY.equals(sheetContent.getName())) //
+                        .findFirst();
+
+                List<ColumnMetadata> columns = content.get().getColumnMetadatas();
                 try {
                     Integer.parseInt(columnValue);
-                    Optional<SchemaParserResult.SheetContent> content = sheetContents.stream() //
-                            .filter(sheetContent -> META_KEY.equals(sheetContent.getName())) //
-                            .findFirst();
 
                     if (content.isPresent()) {
-                        List<ColumnMetadata> columns = content.get().getColumnMetadatas();
                         // in case there are more columns that in the header
                         if (columns.size() > i) {
                             columns.get(i).setType(Type.INTEGER.getName());
                         }
                     }
-
                 } catch (NumberFormatException e) {
-                    // Not an number
+                    // Not a number
                 }
                 if ("true".equalsIgnoreCase(columnValue.trim()) || "false".equalsIgnoreCase(columnValue.trim())) {
-                    sheetContents.stream().filter(sheetContent -> META_KEY.equals(sheetContent.getName())).findFirst() //
-                            .get().getColumnMetadatas().get(i).setType(Type.BOOLEAN.getName());
+                    if (columns.size() > i) {
+                        columns.get(i).setType(Type.BOOLEAN.getName());
+                    }
                 }
             }
             lineNumber++;
