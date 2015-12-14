72dad7beab148ebf04bafef7dda0ac7006b0d79e
Vincent Jaouën <vjaouen@talend.com>
2015-12-14 22:21:32 +0100

    TDP-983: add JavaDoc

--- dataprep-backend-common/src/main/java/org/talend/dataprep/api/preparation/PreparationDetails.java
+++ dataprep-backend-common/src/main/java/org/talend/dataprep/api/preparation/PreparationDetails.java
@@ -2,11 +2,19 @@ package org.talend.dataprep.api.preparation;
 
 /**
  * Bean that extends Preparation used for json serialization towards frontend.
+ * 
+ * @see org.talend.dataprep.api.preparation.json.PreparationDetailsJsonSerializer
  */
 public class PreparationDetails {
 
+    /** The wrapped Preparation. */
     private Preparation preparation;
 
+    /**
+     * Default constructor.
+     *
+     * @param source the source preparation.
+     */
     public PreparationDetails(Preparation source) {
         this.preparation = source;
     }
