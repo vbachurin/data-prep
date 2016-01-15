cb42c93e0054e773b9426488c1518b98f68b32ac
Boubacar Diouf <bdiouf@talend.com>
2016-01-15 10:56:58 +0100

    TDP-1240: Add unit test for TDP-1240

--- dataprep-backend-common/src/test/java/org/talend/dataprep/schema/csv/CSVFormatGuesserTest.java
+++ dataprep-backend-common/src/test/java/org/talend/dataprep/schema/csv/CSVFormatGuesserTest.java
@@ -9,6 +9,7 @@ import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.tools.ant.filters.StringInputStream;
 import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,7 +22,7 @@ import org.talend.dataprep.schema.unsupported.UnsupportedFormatGuess;
  * 
  * @see CSVFormatGuesser
  */
-public class CSVGuesserTest extends AbstractSchemaTestUtils {
+public class CSVFormatGuesserTest extends AbstractSchemaTestUtils {
 
     /** The format guesser to test. */
     @Autowired
@@ -140,7 +141,6 @@ public class CSVGuesserTest extends AbstractSchemaTestUtils {
         assertTrue(separatorMap.isEmpty());
     }
 
-
     /**
      * Have a look at https://jira.talendforge.org/browse/TDP-1060
      */
@@ -168,6 +168,82 @@ public class CSVGuesserTest extends AbstractSchemaTestUtils {
             assertTrue(actual.getFormatGuess() instanceof CSVFormatGuess);
             char separator = actual.getParameters().get(CSVFormatGuess.SEPARATOR_PARAMETER).charAt(0);
             assertEquals(',', separator);
+        }
+    }
+
+    /**
+     * Have a look at https://jira.talendforge.org/browse/TDP-1240
+     * Randomly separators were not detected for files with wrong characters.
+     *
+     */
+    @Test
+    public void TDP_1240_should_detect_separator_if_wrong_characters_do_not_exceed_the_threshold() throws IOException {
+
+        char[] chars = new char[50];
+        for (int i= 0; i < 48; i++){
+            chars[i] = 0;
+        }
+        chars[48] = ';';
+        chars[49] = '\n';
+        try ( InputStream inputStream = new StringInputStream(new String(chars))) {
+            FormatGuesser.Result actual = guesser.guess(getRequest(inputStream, "#10"), "UTF-8");
+
+            Assert.assertNotNull(actual);
+            assertTrue(actual.getFormatGuess() instanceof CSVFormatGuess);
+            char separator = actual.getParameters().get(CSVFormatGuess.SEPARATOR_PARAMETER).charAt(0);
+            assertEquals(';', separator);
+        }
+
+        chars = new char[50];
+        for (int i= 0; i < 48; i++){
+            chars[i] = 65533;
+        }
+        chars[48] = ';';
+        chars[49] = '\n';
+        try ( InputStream inputStream = new StringInputStream(new String(chars))) {
+            FormatGuesser.Result actual = guesser.guess(getRequest(inputStream, "#10"), "UTF-8");
+
+            Assert.assertNotNull(actual);
+            assertTrue(actual.getFormatGuess() instanceof CSVFormatGuess);
+            char separator = actual.getParameters().get(CSVFormatGuess.SEPARATOR_PARAMETER).charAt(0);
+            assertEquals(';', separator);
+        }
+    }
+
+    /**
+     * Have a look at https://jira.talendforge.org/browse/TDP-1240
+     * Randomly separators were not detected for files with wrong characters.
+     *
+     */
+    @Test
+    public void TDP_1240_should_detect_separator_if_wrong_characters_exceed_threshold() throws IOException {
+
+        // null character
+        char[] chars = new char[51];
+        for (int i= 0; i < 49; i++){
+            chars[i] = 0;
+        }
+        chars[49] = ';';
+        chars[50] = '\n';
+        try ( InputStream inputStream = new StringInputStream(new String(chars))) {
+            FormatGuesser.Result actual = guesser.guess(getRequest(inputStream, "#10"), "UTF-8");
+
+            Assert.assertNotNull(actual);
+            assertTrue(actual.getFormatGuess() instanceof UnsupportedFormatGuess);
+        }
+
+        // wrong character
+        chars = new char[51];
+        for (int i= 0; i < 49; i++){
+            chars[i] = 65533;
+        }
+        chars[49] = ';';
+        chars[50] = '\n';
+        try ( InputStream inputStream = new StringInputStream(new String(chars))) {
+            FormatGuesser.Result actual = guesser.guess(getRequest(inputStream, "#10"), "UTF-8");
+
+            Assert.assertNotNull(actual);
+            assertTrue(actual.getFormatGuess() instanceof UnsupportedFormatGuess);
         }
     }
 
