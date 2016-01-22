ab034b6ea413f691a6eb0400aea9d883c96c1811
cnguyen <cnguyen@talend.com>
2016-01-22 14:50:55 +0100

    TDP-1198: add "gulp test:unit --folder " task to run all tests of a folder

--- dataprep-webapp/gulp/unit-tests.js
+++ dataprep-webapp/gulp/unit-tests.js
@@ -7,6 +7,13 @@ var wiredep = require('wiredep');
 var runSequence = require('run-sequence');
 var path = require('path');
 
+
+var knownOptions = {
+    string: 'folder'
+};
+
+var options = require('minimist')(process.argv.slice(2), knownOptions);
+
 var fullTestsFiles = [
     'src/*.js',
     'src/{services,components}/**/*-module.js',
@@ -37,7 +44,7 @@ var filesToCover = {
     widgets: 'src/components/widgets/**/!(*spec|*mock).js'
 };
 
-function runTests(singleRun, done, karmaConfPath, type) {
+function runTests(singleRun, done, karmaConfPath, type, args) {
     var bowerDeps = wiredep({
         directory: 'bower_components',
         dependencies: true,
@@ -50,24 +57,50 @@ function runTests(singleRun, done, karmaConfPath, type) {
     pathSrcHtml.forEach(function (path) {
         preprocessors[path] = ['ng-html2js'];
     });
-    var srcJs = type ? filesToCover [type] : 'src/**/!(*spec|*mock).js';
-    preprocessors[srcJs] = ['coverage'];
+    var srcJs, localConfig;
 
-    var localConfig = {
-        configFile: path.join(__dirname, '/../', karmaConfPath),
-        singleRun: singleRun,
-        autoWatch: !singleRun,
-        reporters: reporters,
-        preprocessors: preprocessors,
-        files: bowerDeps.js.concat(fullTestsFiles),
-        exclude: type ? filesToExclude[type] : []
-    };
+    if(type === 'folder') {
+        srcJs = 'src/**/' + args + '/!(*spec|*mock).js';
+        preprocessors[srcJs] = ['coverage'];
+
+        localConfig = {
+            configFile: path.join(__dirname, '/../', karmaConfPath),
+            singleRun: singleRun,
+            autoWatch: !singleRun,
+            reporters: reporters,
+            preprocessors: preprocessors,
+            files: bowerDeps.js.concat(fullTestsFiles),
+            exclude: ['src/**/!(' + args + ')/*.spec.js']
+        };
+
+    } else {
+        srcJs = type ? filesToCover [type] : 'src/**/!(*spec|*mock).js';
+        preprocessors[srcJs] = ['coverage'];
+
+        localConfig = {
+            configFile: path.join(__dirname, '/../', karmaConfPath),
+            singleRun: singleRun,
+            autoWatch: !singleRun,
+            reporters: reporters,
+            preprocessors: preprocessors,
+            files: bowerDeps.js.concat(fullTestsFiles),
+            exclude: type ? filesToExclude[type] : []
+        };
+    }
+
 
     var server = new karma.Server(localConfig, function (failCount) {
         done(failCount ? new Error("Failed " + failCount + " tests.") : null);
     });
     server.start();
 }
+
+gulp.task('test:unit',function (done) {
+    if(options.folder){
+        return runTests(true /* singleRun */, done, 'karma.conf.js', 'folder', options.folder);
+    }
+    return runTests(true /* singleRun */, done, 'karma.conf.js');
+});
 
 gulp.task('test:components', function (done) {
     return runTests(true /* singleRun */, done, 'karma.conf.js', 'components')
