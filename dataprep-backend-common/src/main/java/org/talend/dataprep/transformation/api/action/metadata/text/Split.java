7c124a4543dfa22be8d49c331448a442d37b999a
jsomsanith <jsomsanith@talend.com>
2016-02-01 09:55:40 +0100

    TDP-1378 : fix canBeBlank flag on split that can accept blank separators (tabs, space, ...)

--- dataprep-backend-common/src/main/java/org/talend/dataprep/transformation/api/action/metadata/text/Split.java
+++ dataprep-backend-common/src/main/java/org/talend/dataprep/transformation/api/action/metadata/text/Split.java
@@ -89,6 +89,7 @@ public class Split extends ActionMetadata implements ColumnAction {
         //@formatter:off
         parameters.add(SelectParameter.Builder.builder()
                         .name(SEPARATOR_PARAMETER)
+                        .canBeBlank(true)
                         .item(":")
                         .item(";")
                         .item(",")
