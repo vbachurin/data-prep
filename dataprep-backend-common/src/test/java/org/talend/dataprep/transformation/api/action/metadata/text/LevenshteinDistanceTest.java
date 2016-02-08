04f348d039c0c2b0cb0f612c1e17d81946e1336d
olivier lamy <olamy@apache.org>
2016-02-08 15:32:03 +1100

    [TDP-172] fix unit tests and add new to test other column mode
    Signed-off-by: olivier lamy <olamy@apache.org>

--- dataprep-backend-common/src/test/java/org/talend/dataprep/transformation/api/action/metadata/text/LevenshteinDistanceTest.java
+++ dataprep-backend-common/src/test/java/org/talend/dataprep/transformation/api/action/metadata/text/LevenshteinDistanceTest.java
@@ -29,6 +29,7 @@ import org.talend.dataprep.transformation.api.action.ActionTestWorkbench;
 import org.talend.dataprep.transformation.api.action.metadata.AbstractMetadataBaseTest;
 import org.talend.dataprep.transformation.api.action.metadata.ActionMetadataTestUtils;
 import org.talend.dataprep.transformation.api.action.metadata.category.ActionCategory;
+import org.talend.dataprep.transformation.api.action.metadata.common.OtherColumnParameters;
 
 /**
  * Test class for LevenshteinDistance action. Creates one consumer, and test it.
@@ -57,7 +58,7 @@ public class LevenshteinDistanceTest extends AbstractMetadataBaseTest {
     }
 
     @Test
-    public void should_be_true_as_less_than_distance() {
+    public void should_be_true_as_less_than_distance_with_constant() {
         // given
         final Map<String, String> values = new HashMap<>();
         values.put("0000", "pale ale");
@@ -65,6 +66,7 @@ public class LevenshteinDistanceTest extends AbstractMetadataBaseTest {
 
         parameters.put(LevenshteinDistance.VALUE_PARAMETER, "pale zle");
         parameters.put(LevenshteinDistance.DISTANCE_PARAMETER, "5");
+        parameters.put(OtherColumnParameters.MODE_PARAMETER, OtherColumnParameters.CONSTANT_MODE);
 
         // when
         ActionTestWorkbench.test(row, action.create(parameters).getRowAction());
@@ -78,7 +80,7 @@ public class LevenshteinDistanceTest extends AbstractMetadataBaseTest {
     }
 
     @Test
-    public void should_be_true_as_equals_distance() {
+    public void should_be_true_as_equals_distance_with_constant() {
         // given
         final Map<String, String> values = new HashMap<>();
         values.put("0000", "pale ale");
@@ -86,6 +88,7 @@ public class LevenshteinDistanceTest extends AbstractMetadataBaseTest {
 
         parameters.put(LevenshteinDistance.VALUE_PARAMETER, "zale zne");
         parameters.put(LevenshteinDistance.DISTANCE_PARAMETER, "3");
+        parameters.put(OtherColumnParameters.MODE_PARAMETER, OtherColumnParameters.CONSTANT_MODE);
 
         // when
         ActionTestWorkbench.test(row, action.create(parameters).getRowAction());
@@ -99,7 +102,7 @@ public class LevenshteinDistanceTest extends AbstractMetadataBaseTest {
     }
 
     @Test
-    public void should_be_false_as_not_less_equals_than_distance() {
+    public void should_be_false_as_not_less_equals_than_distance_with_constant() {
         // given
         final Map<String, String> values = new HashMap<>();
         values.put("0000", "pale ale");
@@ -107,15 +110,88 @@ public class LevenshteinDistanceTest extends AbstractMetadataBaseTest {
 
         parameters.put(LevenshteinDistance.VALUE_PARAMETER, "zale zSQ");
         parameters.put(LevenshteinDistance.DISTANCE_PARAMETER, "3");
+        parameters.put(OtherColumnParameters.MODE_PARAMETER, OtherColumnParameters.CONSTANT_MODE);
 
         // when
         ActionTestWorkbench.test(row, action.create(parameters).getRowAction());
 
         // then
         Assertions.assertThat(row.values()) //
-            .hasSize(2) //
-            .containsExactly(MapEntry.entry("0000", "pale ale"), //
-                             MapEntry.entry("0001", "false"));
+                .hasSize(2) //
+                .containsExactly(MapEntry.entry("0000", "pale ale"), //
+                        MapEntry.entry("0001", "false"));
+
+    }
+
+    @Test
+    public void should_be_true_as_less_than_distance_with_column() {
+        // given
+        final Map<String, String> values = new HashMap<>();
+        values.put("0000", "pale ale");
+        values.put("0001", "pale zle");
+        final DataSetRow row = new DataSetRow(values);
+
+        parameters.put(LevenshteinDistance.DISTANCE_PARAMETER, "5");
+        parameters.put(OtherColumnParameters.SELECTED_COLUMN_PARAMETER, "0001");
+        parameters.put(OtherColumnParameters.MODE_PARAMETER, OtherColumnParameters.OTHER_COLUMN_MODE);
+
+        // when
+        ActionTestWorkbench.test(row, action.create(parameters).getRowAction());
+
+        // then
+        Assertions.assertThat(row.values()) //
+                .hasSize(3) //
+                .containsExactly(MapEntry.entry("0000", "pale ale"), //
+                        MapEntry.entry("0001", "pale zle"), //
+                        MapEntry.entry("0002", "true"));
+
+    }
+
+    @Test
+    public void should_be_true_as_equals_distance_with_column() {
+        // given
+        final Map<String, String> values = new HashMap<>();
+        values.put("0000", "pale ale");
+        values.put("0001", "zale zne");
+        final DataSetRow row = new DataSetRow(values);
+
+        parameters.put(LevenshteinDistance.DISTANCE_PARAMETER, "3");
+        parameters.put(OtherColumnParameters.SELECTED_COLUMN_PARAMETER, "0001");
+        parameters.put(OtherColumnParameters.MODE_PARAMETER, OtherColumnParameters.OTHER_COLUMN_MODE);
+
+        // when
+        ActionTestWorkbench.test(row, action.create(parameters).getRowAction());
+
+        // then
+        Assertions.assertThat(row.values()) //
+                .hasSize(3) //
+                .containsExactly(MapEntry.entry("0000", "pale ale"), //
+                        MapEntry.entry("0001", "zale zne"), //
+                        MapEntry.entry("0002", "true"));
+
+    }
+
+    @Test
+    public void should_be_false_as_not_less_equals_than_distance_with_column() {
+        // given
+        final Map<String, String> values = new HashMap<>();
+        values.put("0000", "pale ale");
+        values.put("0001", "zale zSQ");
+        final DataSetRow row = new DataSetRow(values);
+
+        parameters.put(LevenshteinDistance.DISTANCE_PARAMETER, "3");
+        parameters.put(OtherColumnParameters.SELECTED_COLUMN_PARAMETER, "0001");
+        parameters.put(OtherColumnParameters.MODE_PARAMETER, OtherColumnParameters.OTHER_COLUMN_MODE);
+
+        // when
+        ActionTestWorkbench.test(row, action.create(parameters).getRowAction());
+
+        // then
+        Assertions.assertThat(row.values()) //
+                .hasSize(3) //
+                .containsExactly(MapEntry.entry("0000", "pale ale"), //
+                        MapEntry.entry("0001", "zale zSQ"), //
+                        MapEntry.entry("0002", "false"));
 
     }
 
