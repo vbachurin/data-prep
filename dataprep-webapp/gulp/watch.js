b4e7f9b1564764b97812636d1c670789a5631ce1
jsomsanith <jsomsanith@talend.com>
2016-02-11 16:14:54 +0100

    TDP-674 : watch config.mine.json to replace and reload on change

--- dataprep-webapp/gulp/watch.js
+++ dataprep-webapp/gulp/watch.js
@@ -7,26 +7,31 @@ var conf = require('./conf');
 var browserSync = require('browser-sync');
 
 function isOnlyChange(event) {
-  return event.type === 'changed';
+    return event.type === 'changed';
 }
 
 gulp.task('watch', ['scripts:watch', 'inject'], function () {
 
-  gulp.watch([path.join(conf.paths.src, '/*.html'), 'bower.json'], ['inject-reload']);
+    gulp.watch([path.join(conf.paths.src, '/*.html'), 'bower.json'], ['inject-reload']);
 
-  gulp.watch([
-    path.join(conf.paths.src, '/app/**/*.css'),
-    path.join(conf.paths.src, '/app/**/*.scss')
-  ], function(event) {
-    if(isOnlyChange(event)) {
-      gulp.start('styles-reload');
-    } else {
-      gulp.start('inject-reload');
-    }
-  });
+    gulp.watch([
+        path.join(conf.paths.src, '/app/**/*.css'),
+        path.join(conf.paths.src, '/app/**/*.scss')
+    ], function (event) {
+        if (isOnlyChange(event)) {
+            gulp.start('styles-reload');
+        } else {
+            gulp.start('inject-reload');
+        }
+    });
 
 
-  gulp.watch(path.join(conf.paths.src, '/app/**/*.html'), function(event) {
-    browserSync.reload(event.path);
-  });
+    gulp.watch(path.join(conf.paths.src, '/app/**/*.html'), function (event) {
+        browserSync.reload(event.path);
+    });
+
+    gulp.watch(path.join(conf.paths.src, '/assets/config/config.mine.json'), function (event) {
+        gulp.start('replace-mine');
+        browserSync.reload(event.path);
+    });
 });
