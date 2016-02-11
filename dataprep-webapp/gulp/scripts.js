ba11bfb69c3992b029db6b76baaa0d6ad24716f0
jsomsanith <jsomsanith@talend.com>
2016-02-11 16:01:06 +0100

    TDP-674 : allow to run only unit tests in a specific folder

--- dataprep-webapp/gulp/scripts.js
+++ dataprep-webapp/gulp/scripts.js
@@ -3,6 +3,7 @@
 var path = require('path');
 var gulp = require('gulp');
 var conf = require('./conf');
+var argv = require('yargs').argv;
 
 var browserSync = require('browser-sync');
 var webpack = require('webpack-stream');
@@ -11,58 +12,59 @@ var $ = require('gulp-load-plugins')();
 
 
 function webpackWrapper(watch, test, callback) {
-  var webpackOptions = {
-    watch: watch,
-    module: {
-      preLoaders: [{ test: /\.js$/, exclude: /node_modules/, loader: 'eslint-loader'}],
-      loaders: [{ test: /\.js$/, exclude: /node_modules/, loaders: ['ng-annotate', 'babel-loader']}]
-    },
-    output: { filename: 'index.module.js' }
-  };
+    var webpackOptions = {
+        watch: watch,
+        module: {
+            preLoaders: [{test: /\.js$/, exclude: /node_modules/, loader: 'eslint-loader'}],
+            loaders: [{test: /\.js$/, exclude: /node_modules/, loaders: ['ng-annotate', 'babel-loader']}]
+        },
+        output: {filename: 'index.module.js'}
+    };
 
-  if(watch) {
-    webpackOptions.devtool = 'inline-source-map';
-  }
-
-  var webpackChangeHandler = function(err, stats) {
-    if(err) {
-      conf.errorHandler('Webpack')(err);
+    if (watch) {
+        webpackOptions.devtool = 'inline-source-map';
     }
-    $.util.log(stats.toString({
-      colors: $.util.colors.supportsColor,
-      chunks: false,
-      hash: false,
-      version: false
-    }));
-    browserSync.reload();
-    if(watch) {
-      watch = false;
-      callback();
+
+    var webpackChangeHandler = function (err, stats) {
+        if (err) {
+            conf.errorHandler('Webpack')(err);
+        }
+        $.util.log(stats.toString({
+            colors: $.util.colors.supportsColor,
+            chunks: false,
+            hash: false,
+            version: false
+        }));
+        browserSync.reload();
+        if (watch) {
+            watch = false;
+            callback();
+        }
+    };
+
+    var sources = [path.join(conf.paths.src, '/app/**/*module.js')];
+    if (test) {
+        var specFiles = argv.folder ? '/app/**/' + argv.folder + '/*.spec.js' : '/app/**/*.spec.js';
+        sources.push(path.join(conf.paths.src, specFiles));
     }
-  };
 
-  var sources = [ path.join(conf.paths.src, '/app/**/*module.js') ];
-  if (test) {
-    sources.push(path.join(conf.paths.src, '/app/**/*.spec.js'));
-  }
-
-  return gulp.src(sources)
-    .pipe(webpack(webpackOptions, null, webpackChangeHandler))
-    .pipe(gulp.dest(path.join(conf.paths.tmp, '/serve/app')));
+    return gulp.src(sources)
+        .pipe(webpack(webpackOptions, null, webpackChangeHandler))
+        .pipe(gulp.dest(path.join(conf.paths.tmp, '/serve/app')));
 }
 
 gulp.task('scripts', function () {
-  return webpackWrapper(false, false);
+    return webpackWrapper(false, false);
 });
 
 gulp.task('scripts:watch', ['scripts'], function (callback) {
-  return webpackWrapper(true, false, callback);
+    return webpackWrapper(true, false, callback);
 });
 
 gulp.task('scripts:test', function () {
-  return webpackWrapper(false, true);
+    return webpackWrapper(false, true);
 });
 
 gulp.task('scripts:test-watch', ['scripts'], function (callback) {
-  return webpackWrapper(true, true, callback);
+    return webpackWrapper(true, true, callback);
 });
