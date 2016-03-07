15e44dbc8aeaa396228b4cd0326718dc760d0d2b
olivier lamy <olamy@apache.org>
2016-03-07 13:55:27 +1100

    [TDP-490] extract number action implementation
    Signed-off-by: olivier lamy <olamy@apache.org>

---
+++ dataprep-backend-common/src/main/java/org/talend/dataprep/transformation/api/action/metadata/math/ExtractNumber.java
@@ -0,0 +1,235 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2016 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// https://github.com/Talend/data-prep/blob/master/LICENSE
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.dataprep.transformation.api.action.metadata.math;
+
+import java.math.BigDecimal;
+import java.text.CharacterIterator;
+import java.text.DecimalFormat;
+import java.text.MessageFormat;
+import java.text.NumberFormat;
+import java.text.StringCharacterIterator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Nonnull;
+import javax.annotation.PostConstruct;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.math.NumberUtils;
+import org.springframework.stereotype.Component;
+import org.talend.dataprep.api.dataset.ColumnMetadata;
+import org.talend.dataprep.api.dataset.DataSetRow;
+import org.talend.dataprep.api.dataset.RowMetadata;
+import org.talend.dataprep.api.type.Type;
+import org.talend.dataprep.transformation.api.action.context.ActionContext;
+import org.talend.dataprep.transformation.api.action.metadata.category.ActionCategory;
+import org.talend.dataprep.transformation.api.action.metadata.common.ActionMetadata;
+import org.talend.dataprep.transformation.api.action.metadata.common.ColumnAction;
+import org.talend.dataprep.transformation.api.action.parameters.Parameter;
+import org.talend.dataprep.transformation.api.action.parameters.SelectParameter;
+
+/**
+ * This will extract the numeric part
+ */
+@Component(ActionMetadata.ACTION_BEAN_PREFIX + ExtractNumber.EXTRACT_NUMBER_ACTION_NAME)
+public class ExtractNumber extends ActionMetadata implements ColumnAction {
+
+    public static final String EXTRACT_NUMBER_ACTION_NAME = "extract_number"; //$NON-NLS-1$
+
+    public static String DECIMAL_SEPARATOR = "compare_mode"; //$NON-NLS-1$
+
+    public static String DOT = ".";
+
+    public static String COMMA = ",";
+
+    public static final String DEFAULT_RESULT = "0";
+
+    /**
+     * the maximum fraction digits displayed in the output
+     */
+    public static final int MAX_FRACTION_DIGITS_DISPLAY = 30;
+
+    /**
+     * K: the prefix, V: the value
+     */
+    private Map<String, MetricPrefix> metricPrefixes = new HashMap<>(13);
+
+    public static class MetricPrefix {
+
+        private final String name, sign;
+
+        private final BigDecimal multiply;
+
+        public MetricPrefix(BigDecimal multiply, String name, String sign) {
+            this.multiply = multiply;
+            this.name = name;
+            this.sign = sign;
+        }
+
+        public BigDecimal getMultiply() {
+            return multiply;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getSign() {
+            return sign;
+        }
+    }
+
+    // we use metric prefix from https://en.wikipedia.org/wiki/Metric_prefix
+
+    /*
+     * 
+     * tera T 1000000000000 giga G 1000000000 mega M 1000000 kilo k 1000 hecto h 100 deca da 10 (none) (none) 1 deci d
+     * 0.1 centi c 0.01 milli m 0.001 micro μ 0.000001 nano n 0.000000001 pico p 0.000000000001
+     * 
+     */
+
+    @PostConstruct
+    public void initialize() {
+        // initialize all standard Metric prefix
+        metricPrefixes.put("T", new MetricPrefix(new BigDecimal("1000000000000"), "tera", "T"));
+        metricPrefixes.put("G", new MetricPrefix(new BigDecimal("1000000000"), "giga", "G"));
+        metricPrefixes.put("M", new MetricPrefix(new BigDecimal("1000000"), "mega", "M"));
+        metricPrefixes.put("k", new MetricPrefix(new BigDecimal("1000"), "kilo", "k"));
+        metricPrefixes.put("h", new MetricPrefix(new BigDecimal("100"), "hecto", "h"));
+        metricPrefixes.put("da", new MetricPrefix(new BigDecimal("10"), "deca", "da"));
+        metricPrefixes.put("d", new MetricPrefix(new BigDecimal("0.1"), "deci", "d"));
+        metricPrefixes.put("c", new MetricPrefix(new BigDecimal("0.01"), "centi", "c"));
+        metricPrefixes.put("m", new MetricPrefix(new BigDecimal("0.001"), "milli", "m"));
+        metricPrefixes.put("μ", new MetricPrefix(new BigDecimal("0.000001"), "micro", "μ"));
+        metricPrefixes.put("n", new MetricPrefix(new BigDecimal("0.000000001"), "nano", "n"));
+        metricPrefixes.put("p", new MetricPrefix(new BigDecimal("0.000000000001"), "pico", "p"));
+    }
+
+    @Override
+    public String getName() {
+        return EXTRACT_NUMBER_ACTION_NAME;
+    }
+
+    @Override
+    public String getCategory() {
+        return ActionCategory.SPLIT.getDisplayName();
+    }
+
+    @Override
+    public boolean acceptColumn(ColumnMetadata column) {
+        return true;
+    }
+
+    @Override
+    @Nonnull
+    public List<Parameter> getParameters() {
+        final List<Parameter> parameters = super.getParameters();
+
+        //@formatter:off
+        parameters.add(
+        SelectParameter.Builder.builder() //
+            .name(DECIMAL_SEPARATOR) //
+            .item( COMMA ) //
+            .item( DOT ) //
+            .defaultValue(DOT) //
+            .build()
+        );
+        //@formatter:on
+
+        return parameters;
+    }
+
+    @Override
+    public void applyOnColumn(DataSetRow row, ActionContext context) {
+
+        final String columnId = context.getColumnId();
+        final RowMetadata rowMetadata = row.getRowMetadata();
+
+        final Map<String, String> parameters = context.getParameters();
+
+        String decimalSeparator = parameters.get(DECIMAL_SEPARATOR);
+
+        if (StringUtils.isEmpty(decimalSeparator)) {
+            decimalSeparator = DOT;
+        }
+
+        // create new column and append it after current column
+        final ColumnMetadata column = rowMetadata.getById(columnId);
+        final String newColumnId = context.column("result", (r) -> {
+            final ColumnMetadata c = ColumnMetadata.Builder //
+                    .column() //
+                    .name(column.getName() + "_number") //
+                    .type(Type.NUMERIC) //
+                    .build();
+            rowMetadata.insertAfter(columnId, c);
+            return c;
+        });
+
+        row.set(newColumnId, extractNumber(row.get(columnId), decimalSeparator));
+
+    }
+
+    protected String extractNumber(String value, String decimalSeparator) {
+
+        if (StringUtils.isEmpty( value )) {
+            return DEFAULT_RESULT;
+        }
+
+        StringCharacterIterator iter = new StringCharacterIterator(value);
+
+        StringBuilder firstPart = new StringBuilder();
+        StringBuilder secondPart = new StringBuilder();
+        boolean decimalSeparatorFound = false;
+        MetricPrefix metricPrefix = null;
+
+        for (char c = iter.first(); c != CharacterIterator.DONE; c = iter.next()) {
+            if (StringUtils.equals(decimalSeparator, String.valueOf(c))) {
+                decimalSeparatorFound = true;
+                continue;
+            }
+            if (NumberUtils.isNumber(String.valueOf(c))) {
+                if (decimalSeparatorFound) {
+                    secondPart.append(c);
+                } else {
+                    firstPart.append(c);
+                }
+                continue;
+            }
+            // we take the last metric prefix after digit found
+            MetricPrefix found = metricPrefixes.get(String.valueOf(c));
+            if (found != null) {
+                metricPrefix = found;
+            }
+        }
+
+        if (firstPart.length()<1 && secondPart.length()<1){
+            return DEFAULT_RESULT;
+        }
+        if (firstPart.length()<1){
+            firstPart.append( '0' );
+        }
+
+        BigDecimal bigDecimal = new BigDecimal(firstPart.append(DOT).append(secondPart).toString());
+
+        if (metricPrefix != null) {
+            bigDecimal = bigDecimal.multiply(metricPrefix.getMultiply());
+        }
+
+        DecimalFormat decimalFormat = new DecimalFormat( "0.#" );
+        decimalFormat.setMaximumFractionDigits( MAX_FRACTION_DIGITS_DISPLAY );
+        return decimalFormat.format( bigDecimal.stripTrailingZeros() );
+
+    }
+
+}
---
+++ dataprep-backend-common/src/test/java/org/talend/dataprep/transformation/api/action/metadata/math/ExtractNumberTest.java
@@ -0,0 +1,268 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2016 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// https://github.com/Talend/data-prep/blob/master/LICENSE
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+
+package org.talend.dataprep.transformation.api.action.metadata.math;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.talend.dataprep.api.dataset.ColumnMetadata.Builder.column;
+import static org.talend.dataprep.transformation.api.action.metadata.ActionMetadataTestUtils.getRow;
+
+import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.talend.dataprep.api.dataset.ColumnMetadata;
+import org.talend.dataprep.api.dataset.DataSetRow;
+import org.talend.dataprep.api.type.Type;
+import org.talend.dataprep.transformation.api.action.ActionTestWorkbench;
+import org.talend.dataprep.transformation.api.action.metadata.AbstractMetadataBaseTest;
+import org.talend.dataprep.transformation.api.action.metadata.ActionMetadataTestUtils;
+import org.talend.dataprep.transformation.api.action.metadata.category.ActionCategory;
+import org.talend.dataprep.transformation.api.action.parameters.Parameter;
+
+/**
+ * Unit test for the ExtractNumber action.
+ *
+ * @see ExtractNumber
+ */
+public class ExtractNumberTest extends AbstractMetadataBaseTest {
+
+    /** The action to test. */
+    @Autowired
+    private ExtractNumber action;
+
+    /** The action parameters. */
+    private Map<String, String> parameters;
+
+    @Before
+    public void setUp() throws Exception {
+        final InputStream parametersSource = ExtractNumberTest.class.getResourceAsStream("extractNumberAction.json");
+        parameters = ActionMetadataTestUtils.parseParameters(parametersSource);
+    }
+
+    @Test
+    public void testActionName() throws Exception {
+        assertEquals("extract_number", action.getName());
+    }
+
+    @Test
+    public void testActionParameters() throws Exception {
+        final List<Parameter> parameters = action.getParameters();
+        Assertions.assertThat(parameters).isNotNull().isNotEmpty().hasSize(5);
+    }
+
+    @Test
+    public void testAdapt() throws Exception {
+        assertThat(action.adapt((ColumnMetadata) null), is(action));
+        ColumnMetadata column = column().name("myColumn").id(0).type(Type.STRING).build();
+        assertThat(action.adapt(column), is(action));
+    }
+
+    @Test
+    public void testCategory() throws Exception {
+        assertThat(action.getCategory(), is(ActionCategory.SPLIT.getDisplayName()));
+    }
+
+    @Test
+    public void extract_simple() throws Exception {
+        // given
+        DataSetRow row = getRow("5000");
+        Assertions.assertThat(row.getRowMetadata().getColumns()).isNotEmpty().hasSize(1);
+
+        // when
+        ActionTestWorkbench.test(row, action.create(parameters).getRowAction());
+
+
+        // then
+        Assertions.assertThat(row.getRowMetadata().getColumns()) //
+                .isNotEmpty().hasSize(2)
+                .contains(ColumnMetadata.Builder //
+                        .column() //
+                        .name("0000" + "_number") //
+                        .type(Type.NUMERIC) //
+                        .computedId("0001") //
+                        .build());
+
+        Assertions.assertThat(row.values()).isNotEmpty().hasSize(2);
+
+        Assertions.assertThat( row.get( "0001" ) ).isEqualTo( "5000" );
+
+    }
+
+    @Test
+    public void extract_simple_with_digit() throws Exception {
+        // given
+        DataSetRow row = getRow("5000.231");
+        Assertions.assertThat(row.getRowMetadata().getColumns()).isNotEmpty().hasSize(1);
+        parameters.put( ExtractNumber.DECIMAL_SEPARATOR, ExtractNumber.DOT );
+
+        // when
+        ActionTestWorkbench.test(row, action.create(parameters).getRowAction());
+
+        // then
+        Assertions.assertThat(row.getRowMetadata().getColumns()) //
+            .isNotEmpty().hasSize(2)
+            .contains(ColumnMetadata.Builder //
+                          .column() //
+                          .name("0000" + "_number") //
+                          .type(Type.NUMERIC) //
+                          .computedId("0001") //
+                          .build());
+
+        Assertions.assertThat(row.values()).isNotEmpty().hasSize(2);
+
+        Assertions.assertThat( row.get( "0001" ) ).isEqualTo( "5000.231" );
+
+    }
+
+    @Test
+    public void extract_simple_with_k() throws Exception {
+        // given
+        DataSetRow row = getRow("5k");
+        Assertions.assertThat(row.getRowMetadata().getColumns()).isNotEmpty().hasSize(1);
+
+        // when
+        ActionTestWorkbench.test(row, action.create(parameters).getRowAction());
+
+
+        // then
+        Assertions.assertThat(row.getRowMetadata().getColumns()) //
+            .isNotEmpty().hasSize(2)
+            .contains(ColumnMetadata.Builder //
+                          .column() //
+                          .name("0000" + "_number") //
+                          .type(Type.NUMERIC) //
+                          .computedId("0001") //
+                          .build());
+
+        Assertions.assertThat(row.values()).isNotEmpty().hasSize(2);
+
+        Assertions.assertThat( row.get( "0001" ) ).isEqualTo( "5000" );
+
+    }
+
+    @Test
+    public void extract_simple_with_k_and_digit() throws Exception {
+        // given
+        DataSetRow row = getRow("5.5k");
+        Assertions.assertThat(row.getRowMetadata().getColumns()).isNotEmpty().hasSize(1);
+        parameters.put( ExtractNumber.DECIMAL_SEPARATOR, ExtractNumber.DOT );
+
+        // when
+        ActionTestWorkbench.test(row, action.create(parameters).getRowAction());
+
+
+        // then
+        Assertions.assertThat(row.getRowMetadata().getColumns()) //
+            .isNotEmpty().hasSize(2)
+            .contains(ColumnMetadata.Builder //
+                          .column() //
+                          .name("0000" + "_number") //
+                          .type(Type.NUMERIC) //
+                          .computedId("0001") //
+                          .build());
+
+        Assertions.assertThat(row.values()).isNotEmpty().hasSize(2);
+
+        Assertions.assertThat( row.get( "0001" ) ).isEqualTo( "5500" );
+
+    }
+
+    @Test
+    public void extract_empty() throws Exception {
+        // given
+        DataSetRow row = getRow("");
+        Assertions.assertThat(row.getRowMetadata().getColumns()).isNotEmpty().hasSize(1);
+        parameters.put( ExtractNumber.DECIMAL_SEPARATOR, ExtractNumber.DOT );
+
+        // when
+        ActionTestWorkbench.test(row, action.create(parameters).getRowAction());
+
+
+        // then
+        Assertions.assertThat(row.getRowMetadata().getColumns()) //
+            .isNotEmpty().hasSize(2)
+            .contains(ColumnMetadata.Builder //
+                          .column() //
+                          .name("0000" + "_number") //
+                          .type(Type.NUMERIC) //
+                          .computedId("0001") //
+                          .build());
+
+        Assertions.assertThat(row.values()).isNotEmpty().hasSize(2);
+
+        Assertions.assertThat( row.get( "0001" ) ).isEqualTo( "0" );
+
+    }
+
+    @Test
+    public void extract_nan() throws Exception {
+        // given
+        DataSetRow row = getRow("beer");
+        Assertions.assertThat(row.getRowMetadata().getColumns()).isNotEmpty().hasSize(1);
+        parameters.put( ExtractNumber.DECIMAL_SEPARATOR, ExtractNumber.DOT );
+
+        // when
+        ActionTestWorkbench.test(row, action.create(parameters).getRowAction());
+
+
+        // then
+        Assertions.assertThat(row.getRowMetadata().getColumns()) //
+            .isNotEmpty().hasSize(2)
+            .contains(ColumnMetadata.Builder //
+                          .column() //
+                          .name("0000" + "_number") //
+                          .type(Type.NUMERIC) //
+                          .computedId("0001") //
+                          .build());
+
+        Assertions.assertThat(row.values()).isNotEmpty().hasSize(2);
+
+        Assertions.assertThat( row.get( "0001" ) ).isEqualTo( "0" );
+
+    }
+
+    @Test
+    public void extract_with_only_digits_and_k() throws Exception {
+        // given
+        DataSetRow row = getRow(".01k");
+        Assertions.assertThat(row.getRowMetadata().getColumns()).isNotEmpty().hasSize(1);
+        parameters.put( ExtractNumber.DECIMAL_SEPARATOR, ExtractNumber.DOT );
+
+        // when
+        ActionTestWorkbench.test(row, action.create(parameters).getRowAction());
+
+
+        // then
+        Assertions.assertThat(row.getRowMetadata().getColumns()) //
+            .isNotEmpty().hasSize(2)
+            .contains(ColumnMetadata.Builder //
+                          .column() //
+                          .name("0000" + "_number") //
+                          .type(Type.NUMERIC) //
+                          .computedId("0001") //
+                          .build());
+
+        Assertions.assertThat(row.values()).isNotEmpty().hasSize(2);
+
+        Assertions.assertThat( row.get( "0001" ) ).isEqualTo( "10" );
+
+    }
+
+}
