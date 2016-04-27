0757492b300f122cadb2d95a2ad0c19380778d96
olivier lamy <olamy@apache.org>
2016-04-27 14:18:12 +1000

    [TDP-1710] Square function
    Signed-off-by: olivier lamy <olamy@apache.org>

---
+++ dataprep-backend-common/src/main/java/org/talend/dataprep/transformation/api/action/metadata/math/Square.java
@@ -0,0 +1,55 @@
+//  ============================================================================
+//
+//  Copyright (C) 2006-2016 Talend Inc. - www.talend.com
+//
+//  This source code is available under agreement available at
+//  https://github.com/Talend/data-prep/blob/master/LICENSE
+//
+//  You should have received a copy of the agreement
+//  along with this program; if not, write to Talend SA
+//  9 rue Pages 92150 Suresnes, France
+//
+//  ============================================================================
+package org.talend.dataprep.transformation.api.action.metadata.math;
+
+import static org.talend.dataprep.transformation.api.action.metadata.math.Square.SQRT_NAME;
+
+import org.apache.commons.lang.math.NumberUtils;
+import org.apache.commons.math3.util.FastMath;
+import org.springframework.stereotype.Component;
+import org.talend.dataprep.api.dataset.ColumnMetadata;
+import org.talend.dataprep.transformation.api.action.metadata.common.ActionMetadata;
+
+/**
+ * Create a new column with square root value
+ */
+@Component(ActionMetadata.ACTION_BEAN_PREFIX + SQRT_NAME)
+public class Square
+    extends AbstractMathNoParameterAction {
+
+    protected static final String SQRT_NAME = "square_numbers";
+
+    @Override
+    protected String calculateResult(String columnValue) {
+        double value = NumberUtils.toDouble(columnValue);
+
+        return value < 0 ? ERROR_RESULT : Double.toString(FastMath.sqrt(value));
+    }
+
+    @Override
+    protected String getColumnNameSuffix() {
+        return "square";
+    }
+
+    @Override
+    public boolean acceptColumn(ColumnMetadata column) {
+        return false;
+    }
+
+    @Override
+    public String getName() {
+        return SQRT_NAME;
+    }
+
+
+}
