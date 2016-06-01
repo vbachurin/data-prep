10ba91d6a3216f439de72f436e3fad4e3873f674
olivier lamy <olamy@apache.org>
2016-06-01 21:30:49 +1000

    [TDP-2024] huge excel import
    Signed-off-by: olivier lamy <olamy@apache.org>

---
+++ dataprep-backend-common/src/main/java/org/talend/dataprep/schema/xls/serialization/XlsxStreamRunnable.java
@@ -0,0 +1,161 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2016 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// https://github.com/Talend/data-prep/blob/master/LICENSE
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+
+package org.talend.dataprep.schema.xls.serialization;
+
+import static org.apache.commons.lang.StringUtils.EMPTY;
+import static org.slf4j.LoggerFactory.getLogger;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.slf4j.Logger;
+import org.talend.dataprep.api.dataset.ColumnMetadata;
+import org.talend.dataprep.api.dataset.DataSetMetadata;
+import org.talend.dataprep.schema.xls.XlsSerializer;
+import org.talend.dataprep.schema.xls.streaming.StreamingReader;
+
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerator;
+
+/**
+ * Serialize XLSX file using stream parsing.
+ */
+public class XlsxStreamRunnable
+    implements Runnable {
+
+    /** This class' logger. */
+    private static final Logger LOG = getLogger(XlsxStreamRunnable.class);
+
+    /** Where to serialize the json. */
+    private final OutputStream jsonOutput;
+
+    /** The xlsx raw input. */
+    private final InputStream rawContent;
+
+    /** The dataset metadata to serialize. */
+    private final DataSetMetadata metadata;
+
+    /** The jackson factory to use for the serialization. */
+    private final JsonFactory jsonFactory;
+
+    /**
+     * Constructor.
+     *
+     * @param jsonOutput Where to serialize the json.
+     * @param rawContent The xlsx raw input.
+     * @param metadata The dataset metadata to serialize.
+     * @param factory The jackson factory to use for the serialization.
+     */
+    public XlsxStreamRunnable( OutputStream jsonOutput, InputStream rawContent, DataSetMetadata metadata, JsonFactory factory) {
+        this.jsonOutput = jsonOutput;
+        this.rawContent = rawContent;
+        this.metadata = metadata;
+        this.jsonFactory = factory;
+    }
+
+    /**
+     * @see Runnable#run()
+     */
+    @Override
+    public void run() {
+        try {
+            JsonGenerator generator = jsonFactory.createGenerator(jsonOutput);
+
+            int columnsNumber = metadata.getRowMetadata().getColumns().size();
+
+
+            Workbook workbook = StreamingReader.builder() //
+                .bufferSize(4096) //
+                .rowCacheSize( 1 ) //
+                .open(rawContent);
+
+            Sheet sheet = StringUtils.isEmpty( metadata.getSheetName() ) ? //
+                workbook.getSheetAt( 0 ) : workbook.getSheet( metadata.getSheetName() );
+
+
+            generator.writeStartArray();
+
+            int rowNumber = 1;
+            for ( Row row : sheet){
+                // we need to track empty rows and generate those
+                //((StreamingSheet) sheet).getReader().getFirstRowIndex()
+                //row.getRowNum()
+
+                if (!XlsSerializer.isHeaderLine(rowNumber, metadata.getRowMetadata().getColumns())) {
+                    
+                    generator.writeStartObject();
+                    for (Cell cell : row) {
+                        System.out.print( "|" + cell.getStringCellValue() + "|" );
+                        /*
+                        for (int j = 0; j < metadata.getRowMetadata().getColumns().size(); j++) {
+                            ColumnMetadata columnMetadata = metadata.getRowMetadata().getColumns().get(j);
+                            String cellValue = cell.getStringCellValue();
+                            generator.writeFieldName(columnMetadata.getId());
+                            if (cellValue != null) {
+                                generator.writeString(cellValue);
+                            } else {
+                                generator.writeNull();
+                            }
+                        }
+                        */
+
+                    }
+                    System.out.println( "" );
+                    generator.writeEndObject();
+                }
+                rowNumber++;
+            }
+
+
+            generator.writeEndArray();
+
+            generator.flush();
+
+        } catch (Exception e) {
+            // Consumer may very well interrupt consumption of stream (in case of limit(n) use for sampling).
+            // This is not an issue as consumer is allowed to partially consumes results, it's up to the
+            // consumer to ensure data it consumed is consistent.
+            LOG.debug("Unable to continue serialization for {}. Skipping remaining content.", metadata.getId(), e);
+        } finally {
+            try {
+                jsonOutput.close();
+            } catch (IOException e) {
+                LOG.error("Unable to close output", e);
+            }
+        }
+    }
+
+
+    /**
+     * Return a list of empty string from the given size.
+     * @param size the wanted list size.
+     * @return a list of empty string from the given size.
+     */
+    private List<String> createListWithEmpty(int size) {
+        List<String> list = new ArrayList<>( size);
+        for (int i = 0; i < size; i++) {
+            list.add(EMPTY);
+        }
+        return list;
+    }
+
+}
---
+++ dataprep-backend-common/src/main/java/org/talend/dataprep/schema/xls/streaming/StreamingReader.java
@@ -0,0 +1,351 @@
+package org.talend.dataprep.schema.xls.streaming;
+
+import static com.monitorjbl.xlsx.XmlUtils.document;
+import static com.monitorjbl.xlsx.XmlUtils.searchForNodeList;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.security.GeneralSecurityException;
+import java.util.Iterator;
+import java.util.Objects;
+
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.openxml4j.exceptions.OpenXML4JException;
+import org.apache.poi.openxml4j.opc.OPCPackage;
+import org.apache.poi.poifs.crypt.Decryptor;
+import org.apache.poi.poifs.crypt.EncryptionInfo;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.eventusermodel.XSSFReader;
+import org.apache.poi.xssf.model.SharedStringsTable;
+import org.apache.poi.xssf.model.StylesTable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.w3c.dom.NodeList;
+
+import com.monitorjbl.xlsx.exceptions.MissingSheetException;
+import com.monitorjbl.xlsx.exceptions.OpenException;
+import com.monitorjbl.xlsx.exceptions.ReadException;
+
+/**
+ * Streaming Excel workbook implementation. Most advanced features of POI are not supported. Use this only if your
+ * application can handle iterating through an entire workbook, row by row.
+ */
+public class StreamingReader implements Iterable<Row>, AutoCloseable {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger( StreamingReader.class);
+
+    private File tmp;
+
+    private final StreamingWorkbookReader workbook;
+
+    public StreamingReader(StreamingWorkbookReader workbook) {
+        this.workbook = workbook;
+    }
+
+    /**
+     * Returns a new streaming iterator to loop through rows. This iterator is not guaranteed to have all rows in
+     * memory, and any particular iteration may trigger a load from disk to read in new data.
+     *
+     * @return the streaming iterator
+     * @deprecated StreamingReader is equivalent to the POI Workbook object rather than the Sheet object. This method
+     * will be removed in a future release.
+     */
+    @Override
+    public Iterator<Row> iterator() {
+        return workbook.first().iterator();
+    }
+
+    /**
+     * Closes the streaming resource, attempting to clean up any temporary files created.
+     *
+     * @throws com.monitorjbl.xlsx.exceptions.CloseException if there is an issue closing the stream
+     */
+    @Override
+    public void close() {
+        try {
+            workbook.close();
+        } finally {
+            if (tmp != null) {
+                LOGGER.debug( "Deleting tmp file [" + tmp.getAbsolutePath() + "]");
+                tmp.delete();
+            }
+        }
+    }
+
+    static File writeInputStreamToFile(InputStream is, int bufferSize) throws IOException {
+        File f = Files.createTempFile("tmp-", ".xlsx").toFile();
+        try (FileOutputStream fos = new FileOutputStream(f)) {
+            int read;
+            byte[] bytes = new byte[bufferSize];
+            while ((read = is.read(bytes)) != -1) {
+                fos.write(bytes, 0, read);
+            }
+            is.close();
+            fos.close();
+            return f;
+        }
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+
+        private int rowCacheSize = 10;
+
+        private int bufferSize = 1024;
+
+        private int sheetIndex = 0;
+
+        private String sheetName;
+
+        private String password;
+
+        public int getRowCacheSize() {
+            return rowCacheSize;
+        }
+
+        public int getBufferSize() {
+            return bufferSize;
+        }
+
+        /**
+         * @return The sheet index
+         * @deprecated This method will be removed in a future release.
+         */
+        public int getSheetIndex() {
+            return sheetIndex;
+        }
+
+        /**
+         * @return The sheet name
+         * @deprecated This method will be removed in a future release.
+         */
+        public String getSheetName() {
+            return sheetName;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        /**
+         * The number of rows to keep in memory at any given point.
+         * <p>
+         * Defaults to 10
+         * </p>
+         *
+         * @param rowCacheSize number of rows
+         * @return reference to current {@code Builder}
+         */
+        public Builder rowCacheSize(int rowCacheSize) {
+            this.rowCacheSize = rowCacheSize;
+            return this;
+        }
+
+        /**
+         * The number of bytes to read into memory from the input resource.
+         * <p>
+         * Defaults to 1024
+         * </p>
+         *
+         * @param bufferSize buffer size in bytes
+         * @return reference to current {@code Builder}
+         */
+        public Builder bufferSize(int bufferSize) {
+            this.bufferSize = bufferSize;
+            return this;
+        }
+
+        /**
+         * Which sheet to open. There can only be one sheet open for a single instance of {@code StreamingReader}. If
+         * more sheets need to be read, a new instance must be created.
+         * <p>
+         * Defaults to 0
+         * </p>
+         *
+         * @param sheetIndex index of sheet
+         * @return reference to current {@code Builder}
+         * @deprecated This method will be removed in a future release. Use {@link StreamingWorkbook#getSheetAt(int)}
+         * instead.
+         */
+        public Builder sheetIndex(int sheetIndex) {
+            this.sheetIndex = sheetIndex;
+            return this;
+        }
+
+        /**
+         * Which sheet to open. There can only be one sheet open for a single instance of {@code StreamingReader}. If
+         * more sheets need to be read, a new instance must be created.
+         *
+         * @param sheetName name of sheet
+         * @return reference to current {@code Builder}
+         * @deprecated This method will be removed in a future release. Use {@link StreamingWorkbook#getSheet(String)}
+         * instead.
+         */
+        public Builder sheetName(String sheetName) {
+            this.sheetName = sheetName;
+            return this;
+        }
+
+        /**
+         * For password protected files specify password to open file. If the password is incorrect a
+         * {@code ReadException} is thrown on {@code read}.
+         * <p>
+         * NULL indicates that no password should be used, this is the default value.
+         * </p>
+         *
+         * @param password to use when opening file
+         * @return reference to current {@code Builder}
+         */
+        public Builder password(String password) {
+            this.password = password;
+            return this;
+        }
+
+        /**
+         * Reads a given {@code InputStream} and returns a new instance of {@code Workbook}. Due to Apache POI
+         * limitations, a temporary file must be written in order to create a streaming iterator. This process will use
+         * the same buffer size as specified in {@link #bufferSize(int)}.
+         *
+         * @param is input stream to read in
+         * @return A {@link Workbook} that can be read from
+         * @throws ReadException if there is an issue reading the stream
+         */
+        public Workbook open(InputStream is) {
+            StreamingWorkbookReader workbook = new StreamingWorkbookReader(this);
+            workbook.init(is);
+            return new StreamingWorkbook(workbook);
+        }
+
+        /**
+         * Reads a given {@code File} and returns a new instance of {@code Workbook}.
+         *
+         * @param file file to read in
+         * @return built streaming reader instance
+         * @throws OpenException if there is an issue opening the file
+         * @throws ReadException if there is an issue reading the file
+         */
+        public Workbook open(File file) {
+            StreamingWorkbookReader workbook = new StreamingWorkbookReader(this);
+            workbook.init(file);
+            return new StreamingWorkbook(workbook);
+        }
+
+        /**
+         * Reads a given {@code InputStream} and returns a new instance of {@code StreamingReader}. Due to Apache POI
+         * limitations, a temporary file must be written in order to create a streaming iterator. This process will use
+         * the same buffer size as specified in {@link #bufferSize(int)}.
+         *
+         * @param is input stream to read in
+         * @return built streaming reader instance
+         * @throws ReadException if there is an issue reading the stream
+         * @deprecated This method will be removed in a future release. Use {@link Builder#open(InputStream)} instead
+         */
+        public StreamingReader read(InputStream is) {
+            File f = null;
+            try {
+                f = writeInputStreamToFile(is, bufferSize);
+                LOGGER.debug( "Created temp file [" + f.getAbsolutePath() + "]");
+
+                StreamingReader r = read(f);
+                r.tmp = f;
+                return r;
+            } catch (IOException e) {
+                throw new ReadException("Unable to read input stream", e);
+            } catch (RuntimeException e) {
+                f.delete();
+                throw e;
+            }
+        }
+
+        /**
+         * Reads a given {@code File} and returns a new instance of {@code StreamingReader}.
+         *
+         * @param f file to read in
+         * @return built streaming reader instance
+         * @throws OpenException if there is an issue opening the file
+         * @throws ReadException if there is an issue reading the file
+         * @deprecated This method will be removed in a future release. Use {@link Builder#open(File)} instead
+         */
+        public StreamingReader read(File f) {
+            try {
+                OPCPackage pkg;
+                if (password != null) {
+                    // Based on: https://poi.apache.org/encryption.html
+                    POIFSFileSystem poifs = new POIFSFileSystem(f);
+                    EncryptionInfo info = new EncryptionInfo(poifs);
+                    Decryptor d = Decryptor.getInstance(info);
+                    d.verifyPassword(password);
+                    pkg = OPCPackage.open(d.getDataStream(poifs));
+                } else {
+                    pkg = OPCPackage.open(f);
+                }
+
+                XSSFReader reader = new XSSFReader(pkg);
+                SharedStringsTable sst = reader.getSharedStringsTable();
+                StylesTable styles = reader.getStylesTable();
+
+                InputStream sheet = findSheet(reader);
+                if (sheet == null) {
+                    throw new MissingSheetException("Unable to find sheet at index [" + sheetIndex + "]");
+                }
+
+                XMLEventReader parser = XMLInputFactory.newInstance().createXMLEventReader(sheet);
+
+                return new StreamingReader(
+                        new StreamingWorkbookReader(pkg, new StreamingSheetReader(sst, styles, parser, rowCacheSize), this));
+            } catch (IOException e) {
+                throw new OpenException("Failed to open file", e);
+            } catch (OpenXML4JException | XMLStreamException e) {
+                throw new ReadException("Unable to read workbook", e);
+            } catch (GeneralSecurityException e) {
+                throw new ReadException("Unable to read workbook - Decryption failed", e);
+            }
+        }
+
+        /**
+         * @deprecated This will be removed when the transition to the 1.x API is complete
+         */
+        private InputStream findSheet(XSSFReader reader) throws IOException, InvalidFormatException {
+            int index = sheetIndex;
+            if (sheetName != null) {
+                index = -1;
+                // This file is separate from the worksheet data, and should be fairly small
+                NodeList nl = searchForNodeList(document(reader.getWorkbookData()), "/workbook/sheets/sheet");
+                for (int i = 0; i < nl.getLength(); i++) {
+                    if (Objects.equals(nl.item(i).getAttributes().getNamedItem("name").getTextContent(), sheetName)) {
+                        index = i;
+                    }
+                }
+                if (index < 0) {
+                    return null;
+                }
+            }
+            Iterator<InputStream> iter = reader.getSheetsData();
+            InputStream sheet = null;
+
+            int i = 0;
+            while (iter.hasNext()) {
+                InputStream is = iter.next();
+                if (i++ == index) {
+                    sheet = is;
+                    LOGGER.debug( "Found sheet at index [" + sheetIndex + "]");
+                    break;
+                }
+            }
+            return sheet;
+        }
+    }
+
+}
