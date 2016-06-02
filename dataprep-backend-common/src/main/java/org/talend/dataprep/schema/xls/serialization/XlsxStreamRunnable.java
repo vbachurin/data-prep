07a836fa07fdde60722a642cf2f5da5fb62b6778
olivier lamy <olamy@apache.org>
2016-06-02 16:58:18 +1000

    [TDP-2024] do some intermediate flush
    Signed-off-by: olivier lamy <olamy@apache.org>

--- dataprep-backend-common/src/main/java/org/talend/dataprep/schema/xls/serialization/XlsxStreamRunnable.java
+++ dataprep-backend-common/src/main/java/org/talend/dataprep/schema/xls/serialization/XlsxStreamRunnable.java
@@ -112,6 +112,8 @@ public class XlsxStreamRunnable
                     }
                     generator.writeEndObject();
                 }
+
+                generator.flush();
             }
 
 
