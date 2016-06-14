a1a0146fa798e8c687879b4dca7649147c4718d8
amaalej <amaalej@talend.com>
2016-06-14 17:56:41 +0200

    TDP-1748 code formatting, documentation

--- dataprep-webapp/src/app/components/inventory/item/inventory-item-controller.js
+++ dataprep-webapp/src/app/components/inventory/item/inventory-item-controller.js
@@ -1,15 +1,15 @@
 /*  ============================================================================
 
-  Copyright (C) 2006-2016 Talend Inc. - www.talend.com
+ Copyright (C) 2006-2016 Talend Inc. - www.talend.com
 
-  This source code is available under agreement available at
-  https://github.com/Talend/data-prep/blob/master/LICENSE
+ This source code is available under agreement available at
+ https://github.com/Talend/data-prep/blob/master/LICENSE
 
-  You should have received a copy of the agreement
-  along with this program; if not, write to Talend SA
-  9 rue Pages 92150 Suresnes, France
+ You should have received a copy of the agreement
+ along with this program; if not, write to Talend SA
+ 9 rue Pages 92150 Suresnes, France
 
-  ============================================================================*/
+ ============================================================================*/
 
 /**
  * @ngdoc controller
@@ -23,10 +23,10 @@ export default function InventoryItemCtrl() {
      * @ngdoc method
      * @name openRelatedInventoryItem
      * @methodOf data-prep.inventory-item:InventoryItemCtrl
-     * @params relatedInventory the related inventory item
+     * @params {Object} relatedInventory the related inventory item
      * @description opens the inventory related to the current inventory item
      */
-    vm.openRelatedInventoryItem = function openRelatedInventoryItem (relatedInventory) {
+    vm.openRelatedInventoryItem = function openRelatedInventoryItem(relatedInventory) {
         if (vm.openRelatedInventory) {
             vm.openRelatedInventory(relatedInventory);
         }
@@ -37,11 +37,11 @@ export default function InventoryItemCtrl() {
      * @name getTooltipContent
      * @methodOf data-prep.inventory-item:InventoryItemCtrl
      * @description creates the object used to construct the tooltip
-     * @params isRelatedInventory if the related inventory item
+     * @params {Object} isRelatedInventory if the related inventory item
      * @returns {Object} the object to construct the tooltip with
      */
-    vm.getTooltipContent = function getTooltipContent (isRelatedInventory) {
-        return  isRelatedInventory && vm.relatedInventories && vm.relatedInventories.length?
+    vm.getTooltipContent = function getTooltipContent(isRelatedInventory) {
+        return isRelatedInventory && vm.relatedInventories && vm.relatedInventories.length ?
         {
             type: vm.relatedInventoriesType,
             name: vm.relatedInventories[0].name
