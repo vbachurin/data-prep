82362fe3d5f7899f4f69a9ed578538ce0519760e
Boubacar Diouf <bdiouf@talend.com>
2016-07-11 15:28:08 +0200

    TDP-1527: Configure the ObjectMapper bean instead of the Jackson2ObjectMapperBuilder

--- dataprep-backend-common/src/main/java/org/talend/dataprep/configuration/Serialization.java
+++ dataprep-backend-common/src/main/java/org/talend/dataprep/configuration/Serialization.java
@@ -1,15 +1,15 @@
-//  ============================================================================
+// ============================================================================
 //
-//  Copyright (C) 2006-2016 Talend Inc. - www.talend.com
+// Copyright (C) 2006-2016 Talend Inc. - www.talend.com
 //
-//  This source code is available under agreement available at
-//  https://github.com/Talend/data-prep/blob/master/LICENSE
+// This source code is available under agreement available at
+// https://github.com/Talend/data-prep/blob/master/LICENSE
 //
-//  You should have received a copy of the agreement
-//  along with this program; if not, write to Talend SA
-//  9 rue Pages 92150 Suresnes, France
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
 //
-//  ============================================================================
+// ============================================================================
 
 package org.talend.dataprep.configuration;
 
@@ -18,10 +18,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.Module;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 
 @Configuration
@@ -32,12 +34,13 @@ public class Serialization {
     List<Module> modules;
 
     @Bean
-    public Jackson2ObjectMapperBuilder jacksonBuilder() {
+    @Primary
+    public ObjectMapper jacksonBuilder() {
         Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
         builder.featuresToDisable(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES);
         builder.indentOutput(false);
         modules.add(new Jdk8Module()); // needed to [de]serialize java8 Optional (among other things)
         builder.modules(modules);
-        return builder;
+        return builder.build();
     }
 }
