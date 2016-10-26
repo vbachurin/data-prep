0fc488438d35b379a6792a357ff1b12f08046c1d
Jimmy Somsanith <jsomsanith@talend.com>
2016-10-26 10:10:26 +0200

    bugfix(TDP-2784): lib fonts packaging (#304)
    * TDP-2784: resolve url + load fonts + some style fixes du to bootstrap integration
    
    * TDP-2784: fix recipe font size to fit actions font size

--- dataprep-webapp/config/webpack.config.js
+++ dataprep-webapp/config/webpack.config.js
@@ -18,146 +18,154 @@ const VENDOR_PATH = path.resolve(__dirname, '../src/vendor.js');
 const BUILD_PATH = path.resolve(__dirname, '../build');
 
 function getDefaultConfig(options) {
-    return {
-        entry: {
-            app: INDEX_PATH,
-            vendor: VENDOR_PATH,
-            style: STYLE_PATH,
-        },
-        output: {
-            path: BUILD_PATH,
-            filename: '[name]-[hash].js',
-        },
-        module: {
-            preLoaders: [],
-            loaders: [
-                { test: /\.js$/, loaders: ['ng-annotate', 'babel'], exclude: /node_modules/ },
-                { test: /\.(css|scss)$/, loader: extractCSS.extract(['css', 'sass']) },
-                { test: /\.(png|jpg|jpeg|gif)$/, loader: 'url-loader', query: { mimetype: 'image/png' } },
-                { test: /\.html$/, loaders: ['ngtemplate', 'html'], exclude: INDEX_TEMPLATE_PATH },
-            ]
-        },
-        plugins: [
-            extractCSS,
-            new webpack.ProvidePlugin({
-                $: 'jquery',
-                jQuery: 'jquery',
-                'window.jQuery': 'jquery'
-            })
-        ],
-        cache: true,
-        devtool: options.devtool,
-        debug: options.debug
-    };
+	return {
+		entry: {
+			app: INDEX_PATH,
+			vendor: VENDOR_PATH,
+			style: STYLE_PATH,
+		},
+		output: {
+			path: BUILD_PATH,
+			filename: '[name]-[hash].js',
+		},
+		module: {
+			preLoaders: [],
+			loaders: [
+				{ test: /\.js$/, loaders: ['ng-annotate', 'babel'], exclude: /node_modules/ },
+				{ test: /\.(css|scss)$/, loader: extractCSS.extract(['css', 'resolve-url', 'sass?sourceMap']) },
+				{ test: /\.(png|jpg|jpeg|gif)$/, loader: 'url-loader', query: { mimetype: 'image/png' } },
+				{ test: /\.html$/, loaders: ['ngtemplate', 'html'], exclude: INDEX_TEMPLATE_PATH },
+
+				{ test: /\.woff(2)?(\?v=\d+\.\d+\.\d+)?$/,  loader: "url?name=/assets/fonts/[name].[ext]&limit=10000&mimetype=application/font-woff" },
+				{ test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,       loader: "url?name=/assets/fonts/[name].[ext]&limit=10000&mimetype=application/octet-stream" },
+				{ test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,       loader: "file?name=/assets/fonts/[name].[ext]" },
+				{ test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,       loader: "url?name=/assets/fonts/[name].[ext]&limit=10000&mimetype=image/svg+xml" }
+			]
+		},
+		plugins: [
+			extractCSS,
+			new webpack.ProvidePlugin({
+				$: 'jquery',
+				jQuery: 'jquery',
+				'window.jQuery': 'jquery'
+			})
+		],
+		cache: true,
+		devtool: options.devtool,
+		debug: options.debug
+	};
 }
 
 function addDevServerConfig(config) {
-    config.devServer = {
-        port: appConf.port,
-        host: appConf.host,
-        watchOptions: {
-            aggregateTimeout: 300,
-            poll: 1000
-        },
-        inline: true,
-        progress: true,
-        contentBase: BUILD_PATH,
-        outputPath: BUILD_PATH
-    };
+	config.devServer = {
+		port: appConf.port,
+		host: appConf.host,
+		watchOptions: {
+			aggregateTimeout: 300,
+			poll: 1000
+		},
+		inline: true,
+		progress: true,
+		contentBase: BUILD_PATH,
+		outputPath: BUILD_PATH
+	};
 }
 
 function addMinifyConfig(config) {
-    config.plugins.push(new webpack.optimize.UglifyJsPlugin({
-        compress: {
-            warnings: false
-        }
-    }));
+	config.plugins.push(new webpack.optimize.UglifyJsPlugin({
+		compress: {
+			warnings: false
+		}
+	}));
 }
 
 function addStripCommentsConfig(config) {
-    config.module.preLoaders.push({ test: /\.js$/, loader: 'stripcomment', exclude: [/node_modules/, /\.spec\.js$/] });
+	config.module.preLoaders.push({ test: /\.js$/, loader: 'stripcomment', exclude: [/node_modules/, /\.spec\.js$/] });
 }
 
 function addTestConfig(config) {
-    config.isparta = {
-        embedSource: true,
-        noAutoWrap: true,
-        babel: {
-            presets: ['es2015']
-        }
-    };
-    config.module.preLoaders.push({ test: /\.js$/, loader: 'isparta', exclude: [/node_modules/, /\.spec\.js$/] });
-    config.externals = {
-        angular: 'angular'
-    };
-    config.plugins.push(new webpack.ProvidePlugin({
-        angular: 'angular'
-    }));
+	config.isparta = {
+		embedSource: true,
+		noAutoWrap: true,
+		babel: {
+			presets: ['es2015']
+		}
+	};
+	config.module.preLoaders.push({ test: /\.js$/, loader: 'isparta', exclude: [/node_modules/, /\.spec\.js$/] });
+	config.externals = {
+		angular: 'angular'
+	};
+	config.plugins.push(new webpack.ProvidePlugin({
+		angular: 'angular'
+	}));
 }
 
 function addPlugins(config, options) {
-    config.plugins.push(
+	config.plugins.push(
+		/*
+		 * Plugin: CopyWebpackPlugin
+		 * Description: Copy files and directories in webpack.
+		 * Copies project static assets.
+		 *
+		 * See: https://www.npmjs.com/package/copy-webpack-plugin
+		 */
+		new CopyWebpackPlugin([
+			{ from: 'src/assets', to: 'assets' },
+			{
+				from: 'src/assets/config/config.mine.json',
+				to: 'assets/config/config.json',
+				force: (options.env === 'dev')
+			},
+			{ from: 'src/i18n', to: 'i18n' },
+		]),
 
-        /*
-         * Plugin: CopyWebpackPlugin
-         * Description: Copy files and directories in webpack.
-         * Copies project static assets.
-         *
-         * See: https://www.npmjs.com/package/copy-webpack-plugin
-         */
-        new CopyWebpackPlugin([
-            { from: 'src/assets', to: 'assets' },
-            { from: 'src/assets/config/config.mine.json', to: 'assets/config/config.json', force: (options.env === 'dev') },
-            { from: 'src/i18n', to: 'i18n' },
-        ]),
+		/*
+		 * Plugin: HtmlWebpackPlugin
+		 * Description: Simplifies creation of HTML files to serve your webpack bundles.
+		 * This is especially useful for webpack bundles that include a hash in the filename
+		 * which changes every compilation.
+		 *
+		 * See: https://github.com/ampedandwired/html-webpack-plugin
+		 */
+		new HtmlWebpackPlugin({
+			title: appConf.title,
+			rootElement: appConf.rootElement,
+			rootModule: appConf.rootModule,
+			env: options.env,
+			template: INDEX_TEMPLATE_PATH,
+			inject: 'head'
+		}),
 
-        /*
-         * Plugin: HtmlWebpackPlugin
-         * Description: Simplifies creation of HTML files to serve your webpack bundles.
-         * This is especially useful for webpack bundles that include a hash in the filename
-         * which changes every compilation.
-         *
-         * See: https://github.com/ampedandwired/html-webpack-plugin
-         */
-        new HtmlWebpackPlugin({
-            title: appConf.title,
-            rootElement: appConf.rootElement,
-            rootModule: appConf.rootModule,
-            env: options.env,
-            template: INDEX_TEMPLATE_PATH,
-            inject: 'head'
-        }),
+		/*
+		 * Plugin: BannerPlugin
+		 * Description: Inject a banner on top of the output file
+		 * This is used to inject the licence.
+		 *
+		 * See: https://webpack.github.io/docs/list-of-plugins.html#bannerplugin
+		 */
+		new webpack.BannerPlugin(getLicense()),
 
-        /*
-         * Plugin: BannerPlugin
-         * Description: Inject a banner on top of the output file
-         * This is used to inject the licence.
-         *
-         * See: https://webpack.github.io/docs/list-of-plugins.html#bannerplugin
-         */
-        new webpack.BannerPlugin(getLicense()),
-
-        /*
-         * Plugin: webpack.optimize.CommonsChunkPlugin
-         * Description: Identifies common modules and put them into a commons chunk
-         *
-         * See: https://github.com/webpack/docs/wiki/optimization
-         */
-        new webpack.optimize.CommonsChunkPlugin({ name: 'vendor' })
-    );
+		/*
+		 * Plugin: webpack.optimize.CommonsChunkPlugin
+		 * Description: Identifies common modules and put them into a commons chunk
+		 *
+		 * See: https://github.com/webpack/docs/wiki/optimization
+		 */
+		new webpack.optimize.CommonsChunkPlugin({ name: 'vendor' })
+	);
 }
 
 function addLinterConfig(config) {
-    config.eslint = { configFile: path.resolve(__dirname, '../.eslintrc') };
-    config.module.preLoaders.push({
-        test: /src\/.*\.js$/,
-        exclude: /node_modules/,
-        loader: 'eslint-loader',
-    });
+	config.eslint = { configFile: path.resolve(__dirname, '../.eslintrc') };
+	config.module.preLoaders.push({
+		test: /src\/.*\.js$/,
+		exclude: /node_modules/,
+		loader: 'eslint-loader',
+	});
 
-    // config.plugins.push(new SassLintPlugin({
-    //     glob: 'src/app/**/*.s?(a|c)ss',
-    // }));
+	// config.plugins.push(new SassLintPlugin({
+	//     glob: 'src/app/**/*.s?(a|c)ss',
+	// }));
 }
 
 /*
@@ -173,28 +181,28 @@ function addLinterConfig(config) {
  }
  */
 module.exports = (options) => {
-    const config = getDefaultConfig(options);
+	const config = getDefaultConfig(options);
 
-    if (options.devServer) {
-        addDevServerConfig(config);
-        addPlugins(config, options);
-    }
+	if (options.devServer) {
+		addDevServerConfig(config);
+		addPlugins(config, options);
+	}
 
-    if (options.minify) {
-        addMinifyConfig(config);
-    }
+	if (options.minify) {
+		addMinifyConfig(config);
+	}
 
-    if (options.stripComments) {
-        addStripCommentsConfig(config);
-    }
+	if (options.stripComments) {
+		addStripCommentsConfig(config);
+	}
 
-    if (options.test) {
-        addTestConfig(config);
-    }
+	if (options.test) {
+		addTestConfig(config);
+	}
 
-    if (options.linter) {
-        addLinterConfig(config);
-    }
+	if (options.linter) {
+		addLinterConfig(config);
+	}
 
-    return config;
+	return config;
 };
