83e90673e88186892eeb019d07d0c536e126ff49
Sizhao Liu <sizhaoliu@talend.com>
2016-11-09 18:34:10 +0100

    [for dev only] configure DQ dict service on DataPrep OS

---
+++ dataprep-dataset/src/main/java/org/talend/dataprep/dataset/DataQualityConfiguration.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (C) 2006-2016 Talend Inc. - www.talend.com
+ *
+ * This source code is available under agreement available at
+ * https://github.com/Talend/data-prep/blob/master/LICENSE
+ *
+ * You should have received a copy of the agreement
+ * along with this program; if not, write to Talend SA
+ * 9 rue Pages 92150 Suresnes, France
+ */
+
+package org.talend.dataprep.dataset;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("org.talend.dataquality")
+@ConditionalOnProperty(name = "dataquality.semantic.update.enable", havingValue = "true")
+public class DataQualityConfiguration {
+
+}
