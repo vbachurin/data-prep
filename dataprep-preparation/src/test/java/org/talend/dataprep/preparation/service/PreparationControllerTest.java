26fc4b273fc765ab7fee6f3a4fd1e4749ad23aa8
hmdebenque <hmdebenque@talend.com>
2016-12-30 15:08:35 +0100

    fix TDP-2562: ignoring flawed test

--- dataprep-preparation/src/test/java/org/talend/dataprep/preparation/service/PreparationControllerTest.java
+++ dataprep-preparation/src/test/java/org/talend/dataprep/preparation/service/PreparationControllerTest.java
@@ -13,19 +13,6 @@
 
 package org.talend.dataprep.preparation.service;
 
-import static com.jayway.restassured.RestAssured.given;
-import static com.jayway.restassured.RestAssured.when;
-import static java.util.Arrays.asList;
-import static java.util.Collections.singletonList;
-import static junit.framework.TestCase.assertTrue;
-import static org.hamcrest.Matchers.*;
-import static org.junit.Assert.*;
-import static org.talend.dataprep.api.folder.FolderContentType.PREPARATION;
-import static org.talend.dataprep.preparation.service.EntityBuilder.*;
-import static org.talend.dataprep.preparation.service.PreparationControllerTestClient.appendStepsToPrep;
-import static org.talend.dataprep.test.SameJSONFile.sameJSONAsFile;
-import static uk.co.datumedge.hamcrest.json.SameJSONAs.sameJSONAs;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.*;
@@ -40,6 +27,7 @@ import com.jayway.restassured.path.json.JsonPath;
 import com.jayway.restassured.response.Response;
 import org.apache.commons.io.IOUtils;
 import org.hamcrest.MatcherAssert;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -1621,6 +1609,7 @@ public class PreparationControllerTest extends BasePreparationTest {
     }
 
     @Test
+    @Ignore("This test is flawed, it may be corrected but this behavior is already tested through PreparationAPI.")
     // TODO: this test is not testing what it says it is testing:
     // The 204 comes from the use of the dataset as preparation base, not in lookup
     public void shouldReturnHTTP204WhenPreparationHasLookupOnDataset() throws Exception {
