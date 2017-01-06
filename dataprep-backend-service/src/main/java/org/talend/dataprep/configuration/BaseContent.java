1339655cd474675964f4b3386e5925493d9d9d5c
fhuaulme <fhuaulme@talend.com>
2017-01-06 14:24:18 +0100

    TDP-2953: Fix converter initialization.

--- dataprep-backend-service/src/main/java/org/talend/dataprep/configuration/BaseContent.java
+++ dataprep-backend-service/src/main/java/org/talend/dataprep/configuration/BaseContent.java
@@ -1,15 +1,14 @@
-//  ============================================================================
+// ============================================================================
+// Copyright (C) 2006-2016 Talend Inc. - www.talend.com
 //
-//  Copyright (C) 2006-2016 Talend Inc. - www.talend.com
+// This source code is available under agreement available at
+// https://github.com/Talend/data-prep/blob/master/LICENSE
 //
-//  This source code is available under agreement available at
-//  https://github.com/Talend/data-prep/blob/master/LICENSE
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
 //
-//  You should have received a copy of the agreement
-//  along with this program; if not, write to Talend SA
-//  9 rue Pages 92150 Suresnes, France
-//
-//  ============================================================================
+// ============================================================================
 
 package org.talend.dataprep.configuration;
 
@@ -44,12 +43,16 @@ public class BaseContent {
 
     @Bean
     public Converter<String, JsonNode> jsonNodeConverter() {
-        return source -> {
-            ObjectMapper mapper = new ObjectMapper();
-            try {
-                return mapper.readTree(source);
-            } catch (IOException e) {
-                throw new TDPException(CommonErrorCodes.UNEXPECTED_EXCEPTION, e);
+        return new Converter<String, JsonNode>() {
+
+            @Override
+            public JsonNode convert(String source) {
+                ObjectMapper mapper = new ObjectMapper();
+                try {
+                    return mapper.readTree(source);
+                } catch (IOException e) {
+                    throw new TDPException(CommonErrorCodes.UNEXPECTED_EXCEPTION, e);
+                }
             }
         };
     }
