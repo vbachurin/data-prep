30547f5cc2a3531f1ad421b16536bb365cc23c23
Matthieu Debarre <mdebarre@users.noreply.github.com>
2017-02-03 15:26:27 +0100

    Update webapp container to be able to reach API container in HTTPS (#467)

--- dataprep-webapp/docker/init_docker.sh
+++ dataprep-webapp/docker/init_docker.sh
@@ -8,6 +8,26 @@ PORT_REGEX='[0-9]*'
 echo 'new values to set are:'
 echo ' - api host=<'$TDP_API_HOST'>'
 echo ' - api port=<'$TDP_API_PORT'>'
+echo ' - api protocol=<'$TDP_API_PROTOCOL'>'
+
+PROTOCOL="http"
+
+if [ $TDP_API_PORT == "443" ]; then
+  PORT_NUMBER="";
+elif [ $TDP_API_PORT == "80" ]; then
+  PORT_NUMBER="";
+else
+  PORT_NUMBER=":"$TDP_API_PORT;
+fi
+
+if [ $TDP_API_PROTOCOL == "http" ]; then
+  PROTOCOL="http";
+elif [ $TDP_API_PROTOCOL == "https" ]; then
+  PROTOCOL="https";
+fi
+
+URL_SERVER=${PROTOCOL}"://"${TDP_API_HOST}${PORT_NUMBER}
+echo $URL_SERVER
 
 # this is the file to patch
 CONFIG_FILE=`ls "$CONFIG_LOCATION"/config.json`
@@ -17,7 +37,7 @@ echo 'file to patch=<'$CONFIG_FILE'> to <'$CONFIG_FILE_BACKUP'>'
 # replace default api service host:port by those taken from api container
 cd $CONFIG_LOCATION
 cp $CONFIG_FILE    $CONFIG_FILE_BACKUP
-sed "s|[\"]serverUrl[\"]:[ ]*[\"]http://"$IP_ADDRESS_REGEX":"$PORT_REGEX"['\"]|\"serverUrl\": \"http://"$TDP_API_HOST":"$TDP_API_PORT"\"|g" $CONFIG_FILE_BACKUP > $CONFIG_FILE
+sed "s|[\"]serverUrl[\"]:[ ]*[\"]http://"$IP_ADDRESS_REGEX":"$PORT_REGEX"['\"]|\"serverUrl\": \"$URL_SERVER\"|g" $CONFIG_FILE_BACKUP > $CONFIG_FILE
 
 echo 'before: '
 cat $CONFIG_FILE_BACKUP
